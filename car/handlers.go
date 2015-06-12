3d85157af614fce44d545378179b92e16e6ec3e9
Asim <asim@chuhnk.me>
2015-06-12 19:53:14 +0100

    No longer requires context

--- car/handlers.go
+++ car/handlers.go
@@ -151,7 +151,7 @@ func (c *conn) readLoop() {
 		if err != nil {
 			return
 		}
-		broker.Publish(context.Background(), c.topic, message)
+		broker.Publish(c.topic, &broker.Message{Body:message})
 	}
 }
 
@@ -163,7 +163,7 @@ func (c *conn) write(mType int, data []byte) error {
 func (c *conn) writeLoop() {
 	ticker := time.NewTicker(pingTime)
 
-	subscriber, err := broker.Subscribe(c.topic, func(ctx context.Context, msg *broker.Message) {
+	subscriber, err := broker.Subscribe(c.topic, func(msg *broker.Message) {
 		b, err := json.Marshal(msg)
 		if err != nil {
 			return
