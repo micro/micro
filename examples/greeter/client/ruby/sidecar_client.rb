30538bf776a366a6c5c597684e7b607f8af2cf39
Asim <asim@chuhnk.me>
2015-10-15 23:45:42 +0100

    fixup the request for ruby client

--- examples/greeter/client/ruby/sidecar_client.rb
+++ examples/greeter/client/ruby/sidecar_client.rb
@@ -7,15 +7,17 @@ require 'json'
 # via the sidecar application HTTP interface
 
 uri = URI("http://localhost:8081/rpc")
-service = "go.micro.srv.greeter"
-method = "Say.Hello"
-request = {"name" => "John"}
+
+req = {
+  "service" => "ruby.micro.srv.greeter",
+  "method"  => "Say.Hello",
+  "request" => {"name" => "John"}
+}
 
 # do request
-rsp = Net::HTTP.post_form(uri, {
-	"service" => service,
-	"method" => method,
-	"request" => request
-})
+http = Net::HTTP.new(uri.host, uri.port)
+request = Net::HTTP::Post.new(uri.request_uri)
+request.content_type = 'application/json'
+request.body = req.to_json
 
-puts JSON.parse(rsp.body)["msg"]
+puts JSON.parse(http.request(request).body)
