13da6bb64f625c5ffc3cdf55eaca0d7dff141758
Asim <asim@chuhnk.me>
2016-02-16 11:33:37 +0000

    need web sort file

---
+++ web/sort.go
@@ -0,0 +1,21 @@
+package web
+
+import (
+	"github.com/micro/go-micro/registry"
+)
+
+type sortedServices struct {
+	services []*registry.Service
+}
+
+func (s sortedServices) Len() int {
+	return len(s.services)
+}
+
+func (s sortedServices) Less(i, j int) bool {
+	return s.services[i].Name < s.services[j].Name
+}
+
+func (s sortedServices) Swap(i, j int) {
+	s.services[i], s.services[j] = s.services[j], s.services[i]
+}
