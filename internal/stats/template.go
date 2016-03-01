e9110462e4e558dd8c02bfa099231340cd0965e3
Asim <asim@chuhnk.me>
2016-03-01 20:03:10 +0000

    round seconds

--- internal/stats/template.go
+++ internal/stats/template.go
@@ -228,7 +228,7 @@ var (
               var time = uptime;
 	      var hours   = Math.floor(time / 3600);
 	      var minutes = Math.floor((time - (hours * 3600)) / 60);
-	      var seconds = time - (hours * 3600) - (minutes * 60);
+	      var seconds = Math.floor(time - (hours * 3600) - (minutes * 60));
 
 	      if (hours   < 10) {hours   = "0"+hours;}
 	      if (minutes < 10) {minutes = "0"+minutes;}
