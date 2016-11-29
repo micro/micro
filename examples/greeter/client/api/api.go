416ab98c161c9187774e419534917e7ee23f8f87
Asim <asim@chuhnk.me>
2016-11-29 14:34:27 +0000

    Add proto content-type to api

---
+++ examples/greeter/client/api/api.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"github.com/golang/protobuf/proto"
+	hello "github.com/micro/micro/examples/greeter/api/rpc/proto/hello"
+	"io/ioutil"
+	"net/http"
+)
+
+func main() {
+	req, err := proto.Marshal(&hello.Request{Name: "John"})
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	r, err := http.Post("http://localhost:8080/greeter/hello", "application/proto", bytes.NewReader(req))
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer r.Body.Close()
+
+	b, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	rsp := &hello.Response{}
+	if err := proto.Unmarshal(b, rsp); err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(rsp.Msg)
+}
