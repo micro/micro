7c16100fb6f611cd6c71b3f738c6ae4677071d19
Asim <asim@chuhnk.me>
2016-12-01 14:05:17 +0000

    Cleanup api examples

--- examples/greeter/api/api.go
+++ examples/greeter/api/api.go
@@ -5,17 +5,17 @@ import (
 	"log"
 	"strings"
 
-	"github.com/micro/go-micro/client"
-	"github.com/micro/go-micro/cmd"
+	"github.com/micro/go-micro"
 	"github.com/micro/go-micro/errors"
-	"github.com/micro/go-micro/server"
 	api "github.com/micro/micro/api/proto"
 	hello "github.com/micro/micro/examples/greeter/server/proto/hello"
 
 	"golang.org/x/net/context"
 )
 
-type Say struct{}
+type Say struct {
+	Client hello.SayClient
+}
 
 func (s *Say) Hello(ctx context.Context, req *api.Request, rsp *api.Response) error {
 	log.Print("Received Say.Hello API request")
@@ -25,13 +25,10 @@ func (s *Say) Hello(ctx context.Context, req *api.Request, rsp *api.Response) er
 		return errors.BadRequest("go.micro.api.greeter", "Name cannot be blank")
 	}
 
-	request := client.NewRequest("go.micro.srv.greeter", "Say.Hello", &hello.Request{
+	response, err := s.Client.Hello(ctx, &hello.Request{
 		Name: strings.Join(name.Values, " "),
 	})
-
-	response := &hello.Response{}
-
-	if err := client.Call(ctx, request, response); err != nil {
+	if err != nil {
 		return err
 	}
 
@@ -45,23 +42,20 @@ func (s *Say) Hello(ctx context.Context, req *api.Request, rsp *api.Response) er
 }
 
 func main() {
-	// optionally setup command line usage
-	cmd.Init()
-
-	// Initialise Server
-	server.Init(
-		server.Name("go.micro.api.greeter"),
+	service := micro.NewService(
+		micro.Name("go.micro.api.greeter"),
 	)
 
-	// Register Handlers
-	server.Handle(
-		server.NewHandler(
-			new(Say),
+	// parse command line flags
+	service.Init()
+
+	service.Server().Handle(
+		service.Server().NewHandler(
+			&Say{Client: hello.NewSayClient("go.micro.srv.greeter", service.Client())},
 		),
 	)
 
-	// Run server
-	if err := server.Run(); err != nil {
+	if err := service.Run(); err != nil {
 		log.Fatal(err)
 	}
 }
--- examples/greeter/api/rpc/rpc.go
+++ examples/greeter/api/rpc/rpc.go
@@ -4,8 +4,6 @@ import (
 	"log"
 
 	"github.com/micro/go-micro"
-	"github.com/micro/go-micro/client"
-	"github.com/micro/go-micro/server"
 	proto "github.com/micro/micro/examples/greeter/api/rpc/proto/hello"
 	hello "github.com/micro/micro/examples/greeter/server/proto/hello"
 
@@ -13,17 +11,14 @@ import (
 )
 
 type Greeter struct {
-	Client client.Client
+	Client hello.SayClient
 }
 
 func (g *Greeter) Hello(ctx context.Context, req *proto.Request, rsp *proto.Response) error {
 	log.Print("Received Greeter.Hello API request")
 
-	// create client
-	say := hello.NewSayClient("go.micro.srv.greeter", g.Client)
-
 	// make the request
-	response, err := say.Hello(ctx, &hello.Request{Name: req.Name})
+	response, err := g.Client.Hello(ctx, &hello.Request{Name: req.Name})
 	if err != nil {
 		return err
 	}
@@ -43,12 +38,15 @@ func main() {
 	service.Init()
 
 	// Register Handlers
-	proto.RegisterGreeterHandler(service.Server(), &Greeter{service.Client()})
+	proto.RegisterGreeterHandler(service.Server(), &Greeter{
+		// Create Service Client
+		Client: hello.NewSayClient("go.micro.srv.greeter", service.Client()),
+	})
 
 	// for handler use
 
 	// Run server
-	if err := server.Run(); err != nil {
+	if err := service.Run(); err != nil {
 		log.Fatal(err)
 	}
 }
