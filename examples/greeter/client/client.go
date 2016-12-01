13908a4ed396eea0702291dbf7e6e739cfa553e0
Asim <asim@chuhnk.me>
2016-12-01 14:08:02 +0000

    Replace client example

--- examples/greeter/client/client.go
+++ examples/greeter/client/client.go
@@ -6,6 +6,7 @@ import (
 	"github.com/micro/go-micro/client"
 	"github.com/micro/go-micro/cmd"
 	"github.com/micro/go-micro/metadata"
+
 	hello "github.com/micro/micro/examples/greeter/server/proto/hello"
 
 	"golang.org/x/net/context"
@@ -14,10 +15,8 @@ import (
 func main() {
 	cmd.Init()
 
-	// Create new request to service go.micro.srv.greeter, method Say.Hello
-	req := client.NewRequest("go.micro.srv.greeter", "Say.Hello", &hello.Request{
-		Name: "John",
-	})
+	// Use the generated client stub
+	cl := hello.NewSayClient("go.micro.srv.greeter", client.DefaultClient)
 
 	// Set arbitrary headers in context
 	ctx := metadata.NewContext(context.Background(), map[string]string{
@@ -25,10 +24,11 @@ func main() {
 		"X-From-Id": "script",
 	})
 
-	rsp := &hello.Response{}
-
-	// Call service
-	if err := client.Call(ctx, req, rsp); err != nil {
+	// Make request
+	rsp, err := cl.Hello(ctx, &hello.Request{
+		Name: "John",
+	})
+	if err != nil {
 		fmt.Println(err)
 		return
 	}
