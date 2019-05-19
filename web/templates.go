7d79f12c6c60366cbb0fe19a95de6d86a28c56fa
Asim Aslam <asim@aslam.me>
2019-05-19 17:04:20 +0000

    add viewport

--- web/templates.go
+++ web/templates.go
@@ -6,6 +6,7 @@ var (
 <html>
 	<head>
 		<title>Micro Web</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 		<style>
 		{{ template "style" . }}
