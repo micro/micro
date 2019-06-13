7a02d2c7a3ff888aa614aed99697001c7b500eef
Milos Gajdos <milosgajdos83@gmail.com>
2019-06-13 11:00:44 +0100

    Added comments. Shuffled things around. No code change.

--- server/server.go
+++ server/server.go
@@ -16,23 +16,27 @@ import (
 )
 
 var (
-	// Name of the server
+	// Name of the server microservice
 	Name = "go.micro.server"
-	// Address to bind route microservices to
+	// Address is the router microservice bind address
 	Address = ":8083"
-	// Router address to bind to for router gossip
+	// Router is the router bind address a.k.a. gossip bind address
 	Router = ":9093"
-	// Network address to bind to
+	// Network is the micro network bind address
 	Network = ":9094"
 )
 
+// srv is micro server
 type srv struct {
-	router  router.Router
+	// router is micro router
+	router router.Router
+	// network is micro network server
 	network server.Server
 }
 
+// newServer creates new micro server and returns it
 func newServer(s micro.Service, r router.Router) *srv {
-	// NOTE: this will end up being QUIC transport
+	// NOTE: this will end up being QUIC transport once it gets stable
 	t := grpc.NewTransport(transport.Addrs(Network))
 	n := server.NewServer(server.Transport(t))
 
@@ -42,6 +46,7 @@ func newServer(s micro.Service, r router.Router) *srv {
 	}
 }
 
+// start starts the micro server.
 func (s *srv) start() error {
 	log.Log("[server] starting micro server")
 
@@ -56,6 +61,7 @@ func (s *srv) start() error {
 	return nil
 }
 
+// stop stops the micro server
 func (s *srv) stop() error {
 	log.Log("[server] attempting to stop server")
 
@@ -69,6 +75,7 @@ func (s *srv) stop() error {
 	return nil
 }
 
+// run runs the micro server
 func run(ctx *cli.Context, srvOpts ...micro.Option) {
 	// Init plugins
 	for _, p := range Plugins() {
@@ -81,11 +88,11 @@ func run(ctx *cli.Context, srvOpts ...micro.Option) {
 	if len(ctx.String("address")) > 0 {
 		Address = ctx.String("address")
 	}
-	if len(ctx.String("network")) > 0 {
-		Network = ctx.String("network")
-	}
 	if len(ctx.String("router")) > 0 {
 		Router = ctx.String("router")
+	}
+	if len(ctx.String("network")) > 0 {
+		Network = ctx.String("network")
 	}
 
 	// Initialise service
@@ -140,14 +147,14 @@ func Commands(options ...micro.Option) []cli.Command {
 				EnvVar: "MICRO_SERVER_ADDRESS",
 			},
 			cli.StringFlag{
-				Name:   "network",
-				Usage:  "Set the micro network address :9093",
-				EnvVar: "MICRO_NETWORK_ADDRESS",
+				Name:   "router",
+				Usage:  "Set the micro router address :9093",
+				EnvVar: "MICRO_ROUTER_ADDRESS",
 			},
 			cli.StringFlag{
-				Name:   "router",
-				Usage:  "Set the micro router address :9094",
-				EnvVar: "MICRO_ROUTER_ADDRESS",
+				Name:   "network",
+				Usage:  "Set the micro network address :9094",
+				EnvVar: "MICRO_NETWORK_ADDRESS",
 			},
 		},
 		Action: func(ctx *cli.Context) {
