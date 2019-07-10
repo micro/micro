f706bffeae3b6d1eafeffc2b5f54eca9ef7e2a00
Milos Gajdos <milosgajdos83@gmail.com>
2019-07-10 18:39:44 +0100

    Added List and Watch RPC endpoints

--- router/handler/handler.go
+++ router/handler/handler.go
@@ -40,3 +40,68 @@ func (r *Router) Lookup(ctx context.Context, req *pb.LookupRequest, resp *pb.Loo
 
 	return nil
 }
+
+func (r *Router) List(ctx context.Context, req *pb.ListRequest, resp *pb.ListResponse) error {
+	routes, err := r.Router.List()
+	if err != nil {
+		return fmt.Errorf("failed to list routes: %s", err)
+	}
+
+	var respRoutes []*pb.Route
+	for _, route := range routes {
+		respRoute := &pb.Route{
+			Service: route.Service,
+			Address: route.Address,
+			Gateway: route.Gateway,
+			Network: route.Network,
+			Link:    route.Link,
+			Metric:  int64(route.Metric),
+		}
+		respRoutes = append(respRoutes, respRoute)
+	}
+
+	resp.Routes = respRoutes
+
+	return nil
+}
+
+func (r *Router) Watch(ctx context.Context, req *pb.WatchRequest, stream pb.Router_WatchStream) error {
+	watcher, err := r.Router.Watch()
+	if err != nil {
+		return fmt.Errorf("failed creating routing table watcher: %v", err)
+	}
+
+	defer stream.Close()
+
+	for {
+		event, err := watcher.Next()
+		if err == table.ErrWatcherStopped {
+			break
+		}
+
+		if err != nil {
+			return fmt.Errorf("error watching table: %s", err)
+		}
+
+		route := &pb.Route{
+			Service: event.Route.Service,
+			Address: event.Route.Address,
+			Gateway: event.Route.Gateway,
+			Network: event.Route.Network,
+			Link:    event.Route.Link,
+			Metric:  int64(event.Route.Metric),
+		}
+
+		tableEvent := &pb.TableEvent{
+			Type:      pb.EventType(event.Type),
+			Timestamp: event.Timestamp.UnixNano(),
+			Route:     route,
+		}
+
+		if err := stream.Send(tableEvent); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
