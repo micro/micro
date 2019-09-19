e1e5be6298eae471c1ac58e3c842a9b44dc4102c
Asim Aslam <asim@aslam.me>
2019-09-19 14:55:15 +0100

    Add health check command

---
+++ health/health.go
@@ -0,0 +1,101 @@
+// Package health is a healthchecking sidecar
+package health
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/micro/cli"
+	"github.com/micro/go-micro"
+	"github.com/micro/go-micro/client"
+	proto "github.com/micro/go-micro/debug/proto"
+	"github.com/micro/go-micro/util/log"
+	mcli "github.com/micro/micro/cli"
+	"golang.org/x/net/context"
+)
+
+var (
+	healthAddress = "127.0.0.1:8088"
+	serverAddress string
+	serverName    string
+)
+
+func run(ctx *cli.Context) {
+	serverName = ctx.String("check_service")
+	serverAddress = ctx.String("check_address")
+
+	if addr := ctx.String("health_address"); len(addr) > 0 {
+		healthAddress = addr
+	}
+
+	if len(healthAddress) == 0 {
+		log.Fatal("health address not set")
+	}
+	if len(serverName) == 0 {
+		log.Fatal("service name not set")
+	}
+	if len(serverAddress) == 0 {
+		log.Fatal("service address not set")
+	}
+
+	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		req := client.NewRequest(serverName, "Debug.Health", &proto.HealthRequest{})
+		rsp := &proto.HealthResponse{}
+
+		err := client.Call(context.TODO(), req, rsp, client.WithAddress(serverAddress))
+		if err != nil || rsp.Status != "ok" {
+			w.WriteHeader(http.StatusInternalServerError)
+			fmt.Fprint(w, "NOT_HEALTHY")
+			return
+		}
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprint(w, "OK")
+	})
+
+	log.Infof("Health check running at %s/health", healthAddress)
+	log.Infof("Health check defined for %s at %s", serverName, serverAddress)
+
+	if err := http.ListenAndServe(healthAddress, nil); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func Commands(options ...micro.Option) []cli.Command {
+	command := cli.Command{
+		Name:  "health",
+		Usage: "Run the http healthchecking sidecar at /health",
+		Flags: []cli.Flag{
+			cli.StringFlag{
+				Name:   "address",
+				Usage:  "Set the address exposed for the http server e.g :8088",
+				EnvVar: "MICRO_HEALTH_ADDRESS",
+			},
+			cli.StringFlag{
+				Name:   "check_service",
+				Usage:  "Name of the service to query",
+				EnvVar: "MICRO_HEALTH_CHECK_SERVICE",
+			},
+			cli.StringFlag{
+				Name:   "check_address",
+				Usage:  "Set the service address to query",
+				EnvVar: "MICRO_HEALTH_CHECK_ADDRESS",
+			},
+		},
+		Subcommands: mcli.HealthCommands(),
+		Action: func(ctx *cli.Context) {
+			run(ctx)
+		},
+	}
+
+	for _, p := range Plugins() {
+		if cmds := p.Commands(); len(cmds) > 0 {
+			command.Subcommands = append(command.Subcommands, cmds...)
+		}
+
+		if flags := p.Flags(); len(flags) > 0 {
+			command.Flags = append(command.Flags, flags...)
+		}
+	}
+
+	return []cli.Command{command}
+}
