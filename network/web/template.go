93f5d6e5c069505949158d9a1025415a2b8ad0e6
Asim Aslam <asim@aslam.me>
2019-11-16 21:07:29 +0000

    add a network graph

--- network/web/template.go
+++ network/web/template.go
@@ -25,6 +25,7 @@ var (
 <body>
   <div id="head">
     <ul class="nav">
+      <li><a href="/network/graph">Graph</a></li>
       <li><a href="/network/nodes">Nodes</a></li>
       <li><a href="/network/routes">Routes</a></li>
       <li><a href="/network/services">Services</a></li>
@@ -33,6 +34,68 @@ var (
   <div id="content">
     {{.}}
   </div>
+</body>
+</html>
+`
+
+	graphTemplate = `
+<html>
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://fonts.googleapis.com/css?family=Source+Code+Pro&display=swap" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script>
+  <style>
+    html {
+      font-family: 'Source Code Pro', monospace;
+    }
+    .nav {
+      margin-bottom: 10px;
+      padding: 0;
+    }
+    .nav li {
+      display: inline-block; 
+    }
+    table td {
+      padding-right: 5px;
+    }
+    #graph {
+      margin-top: 25px;
+    }
+  </style>
+</head>
+<body>
+  <div id="head">
+    <ul class="nav">
+      <li><a href="/network/graph">Graph</a></li>
+      <li><a href="/network/nodes">Nodes</a></li>
+      <li><a href="/network/routes">Routes</a></li>
+      <li><a href="/network/services">Services</a></li>
+    </ul>
+  </div>
+  <div id="content">
+    <canvas id="graph"></canvas>
+  </div>
+  <script type="text/javascript">
+	var ctx = document.getElementById('graph').getContext('2d');
+	var chart = new Chart(ctx, {
+	    type: 'radar',
+	    data: {
+		labels: ['{{ Join .Nodes "', '" }}'],
+		datasets: [
+		{{ range $label, $values := .Data }}
+		{
+		    label: '{{$label}}', 
+		    borderColor: 'rgb({{ Color }})',
+		    borderWidth: 1,
+		    fill: false,
+		    lineTension: 0.3,
+		    data: [{{ Join $values ", " }}]
+		}{{if $label}},{{end}}{{end}}
+		]
+	    },
+	    options: {legend: {display: false}}
+	});
+  </script>
 </body>
 </html>
 `
--- network/web/web.go
+++ network/web/web.go
@@ -6,6 +6,7 @@ import (
 	"context"
 	"fmt"
 	"math"
+	"math/rand"
 	"net"
 	"net/http"
 	"sort"
@@ -19,6 +20,10 @@ import (
 	"github.com/micro/go-micro/web"
 )
 
+func color() string {
+	return fmt.Sprintf("%d, %d, %d", rand.Intn(255), rand.Intn(255), rand.Intn(255))
+}
+
 func toMap(peer *pb.Peer, peers map[string]string) map[string]string {
 	if peer == nil || peer.Node == nil {
 		return peers
@@ -30,6 +35,29 @@ func toMap(peer *pb.Peer, peers map[string]string) map[string]string {
 	for _, p := range peer.Peers {
 		toMap(p, peers)
 	}
+	return peers
+}
+
+func toGraph(peer *pb.Peer, peers map[string][]string) map[string][]string {
+	if peer == nil || peer.Node == nil {
+		return peers
+	}
+	if peers == nil {
+		peers = make(map[string][]string)
+	}
+
+	// get the current list
+	p := peers[peer.Node.Address]
+
+	// first append the peer
+	for _, pr := range peer.Peers {
+		p = append(p, pr.Node.Address)
+		// save the peer list
+		peers[peer.Node.Address] = p
+		// now walk the peer graph
+		peers = toGraph(pr, peers)
+	}
+
 	return peers
 }
 
@@ -51,6 +79,10 @@ func Run(ctx *cli.Context) {
 
 	// template
 	t := template.Must(template.New("layout").Parse(templateFile))
+	tg := template.New("layout")
+	tg = tg.Funcs(template.FuncMap{"Join": strings.Join})
+	tg = tg.Funcs(template.FuncMap{"Color": color})
+	tg = template.Must(tg.Parse(graphTemplate))
 
 	// return some data
 	service.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
@@ -101,6 +133,86 @@ func Run(ctx *cli.Context) {
 		b.Write([]byte(strings.Join(output, "<br>")))
 
 		t.Execute(w, string(b.Bytes()))
+	})
+
+	service.HandleFunc("/graph", func(w http.ResponseWriter, r *http.Request) {
+		// lookup the network
+		ips, _ := net.LookupHost("network.micro.mu")
+		coreMap := make(map[string]int)
+		coreNodes := []string{}
+
+		// save by index
+		for i, ip := range ips {
+			coreMap[ip] = i
+			coreNodes = append(coreNodes, ip)
+		}
+
+		// sort the nodes (in future based on region)
+		sort.Strings(coreNodes)
+
+		var graph *pb.Peer
+		// get the network graph
+		rsp, err := client.Graph(context.Background(), &pb.GraphRequest{})
+		if err != nil || rsp.Root == nil {
+			return
+		}
+
+		// set the root
+		graph = rsp.Root
+
+		type graphT struct {
+			Nodes []string
+			Data  map[string][]string
+		}
+
+		graphData := new(graphT)
+		// set core nodes aka labels
+		graphData.Nodes = coreNodes
+		graphData.Data = make(map[string][]string)
+
+		// get the graph per node
+		nodeGraph := toGraph(graph, nil)
+
+		// range over the graph and build the data set for each
+		for address, nodes := range nodeGraph {
+			data := make([]string, len(coreNodes))
+			address = strings.TrimSuffix(address, ":30038")
+
+			// set all zeros
+			for i := 0; i < len(coreNodes); i++ {
+				data[i] = "0"
+			}
+
+			// address is one of the core nodes
+			if v, ok := coreMap[address]; ok {
+				// set self to high value
+				data[v] = "100"
+			}
+
+			// walk all the
+			for _, node := range nodes {
+				node = strings.TrimSuffix(node, ":30038")
+
+				// skip self
+				if node == address {
+					continue
+				}
+				v, ok := coreMap[node]
+				if !ok {
+					continue
+				}
+				if v >= len(data) {
+					continue
+				}
+				// set relationship to 1
+				data[v] = "100"
+			}
+
+			// save to data
+			graphData.Data[address] = data
+		}
+
+		tg.Execute(w, graphData)
 	})
 
 	service.HandleFunc("/routes", func(w http.ResponseWriter, r *http.Request) {
