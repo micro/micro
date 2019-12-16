6ef92166a41d215df393b4cc89f5f76c735676f2
Asim Aslam <asim@aslam.me>
2019-12-16 12:33:54 +0000

    add store namespacing

--- store/handler/handler.go
+++ store/handler/handler.go
@@ -3,19 +3,75 @@ package handler
 import (
 	"context"
 	"io"
+	"sync"
 	"time"
 
 	"github.com/micro/go-micro/errors"
+	"github.com/micro/go-micro/metadata"
 	"github.com/micro/go-micro/store"
 	pb "github.com/micro/go-micro/store/service/proto"
 )
 
 type Store struct {
-	Store store.Store
+	// The default store
+	Default store.Store
+
+	// Store initialiser
+	New func(string, string) (store.Store, error)
+
+	// Store map
+	sync.RWMutex
+	Stores map[string]store.Store
+}
+
+func (s *Store) get(ctx context.Context) (store.Store, error) {
+	md, ok := metadata.FromContext(ctx)
+	if !ok {
+		return s.Default, nil
+	}
+
+	namespace := md["Micro-Namespace"]
+	prefix := md["Micro-Prefix"]
+
+	if len(namespace) == 0 && len(prefix) == 0 {
+		return s.Default, nil
+	}
+
+	s.RLock()
+	str, ok := s.Stores[namespace+prefix]
+	// got it
+	if ok {
+		s.RUnlock()
+		return str, nil
+	}
+	s.RUnlock()
+
+	// lock (might be a race)
+	s.Lock()
+	defer s.Unlock()
+
+	// create a new store
+
+	// either namespace is not blank or prefix is not blank
+	st, err := s.New(namespace, prefix)
+	if err != nil {
+		return nil, errors.InternalServerError("go.micro.store", err.Error())
+	}
+
+	// save store
+	s.Stores[namespace+prefix] = st
+
+	return st, nil
 }
 
 func (s *Store) Read(ctx context.Context, req *pb.ReadRequest, rsp *pb.ReadResponse) error {
-	vals, err := s.Store.Read(req.Keys...)
+	// get new store
+	st, err := s.get(ctx)
+	if err != nil {
+		return err
+	}
+
+	vals, err := st.Read(req.Keys...)
 	if err != nil {
 		return errors.InternalServerError("go.micro.store", err.Error())
 	}
@@ -30,6 +86,12 @@ func (s *Store) Read(ctx context.Context, req *pb.ReadRequest, rsp *pb.ReadRespo
 }
 
 func (s *Store) Write(ctx context.Context, req *pb.WriteRequest, rsp *pb.WriteResponse) error {
+	// get new store
+	st, err := s.get(ctx)
+	if err != nil {
+		return err
+	}
+
 	records := make([]*store.Record, 0, len(req.Records))
 
 	for _, record := range req.Records {
@@ -40,29 +102,37 @@ func (s *Store) Write(ctx context.Context, req *pb.WriteRequest, rsp *pb.WriteRe
 		})
 	}
 
-	err := s.Store.Write(records...)
-	if err != nil {
+	if err := st.Write(records...); err != nil {
 		return errors.InternalServerError("go.micro.store", err.Error())
 	}
 	return nil
 }
 
 func (s *Store) Delete(ctx context.Context, req *pb.DeleteRequest, rsp *pb.DeleteResponse) error {
-	err := s.Store.Delete(req.Keys...)
+	// get new store
+	st, err := s.get(ctx)
 	if err != nil {
+		return err
+	}
+	if err := st.Delete(req.Keys...); err != nil {
 		return errors.InternalServerError("go.micro.store", err.Error())
 	}
 	return nil
 }
 
 func (s *Store) List(ctx context.Context, req *pb.ListRequest, stream pb.Store_ListStream) error {
+	// get new store
+	st, err := s.get(ctx)
+	if err != nil {
+		return err
+	}
+
 	var vals []*store.Record
-	var err error
 
 	if len(req.Key) > 0 {
-		vals, err = s.Store.Read(req.Key)
+		vals, err = st.Read(req.Key)
 	} else {
-		vals, err = s.Store.List()
+		vals, err = st.List()
 	}
 	if err != nil {
 		return errors.InternalServerError("go.micro.store", err.Error())
