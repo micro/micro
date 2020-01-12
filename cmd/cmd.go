dc1db4777b1aeab2921028b60af81edf9b95f2d0
Asim Aslam <asim@aslam.me>
2020-01-12 21:34:19 +0000

    cleanup cmd/cmd.go

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -2,15 +2,10 @@ package cmd
 
 import (
 	"fmt"
-	"os"
-	"os/signal"
-	"syscall"
 
 	ccli "github.com/micro/cli"
 	"github.com/micro/go-micro"
 	"github.com/micro/go-micro/config/cmd"
-	gorun "github.com/micro/go-micro/runtime"
-	"github.com/micro/go-micro/util/log"
 	"github.com/micro/micro/api"
 	"github.com/micro/micro/bot"
 	"github.com/micro/micro/broker"
@@ -35,7 +30,7 @@ import (
 
 	// include usage
 
-	"github.com/micro/micro/internal/update"
+	"github.com/micro/micro/internal/platform"
 	_ "github.com/micro/micro/internal/usage"
 )
 
@@ -50,7 +45,11 @@ var (
 )
 
 func init() {
+	// setup the build plugin
 	plugin.Register(build.Flags())
+
+	// set platform build date
+	platform.Version = BuildDate
 }
 
 func setup(app *ccli.App) {
@@ -284,130 +283,13 @@ func Setup(app *ccli.App, options ...micro.Option) {
 		Name:  "init",
 		Usage: "Run the micro operator",
 		Action: func(c *ccli.Context) {
-			initCommand(c)
+			platform.Init(c)
 		},
 		Flags: []ccli.Flag{},
 	})
 
-	// boot micro
-	app.Action = func(context *ccli.Context) {
-		log.Name("micro")
-
-		if len(context.Args()) > 0 {
-			ccli.ShowSubcommandHelp(context)
-			os.Exit(1)
-		}
-
-		// get the network flag
-		network := context.GlobalString("network")
-		local := context.GlobalBool("local")
-
-		// pass through the environment
-		// TODO: perhaps don't do this
-		env := os.Environ()
-
-		if network == "local" || local {
-			// no op for now
-			log.Info("Setting local network")
-		} else {
-			log.Info("Setting global network")
-
-			if v := os.Getenv("MICRO_NETWORK_NODES"); len(v) == 0 {
-				// set the resolver to use https://micro.mu/network
-				env = append(env, "MICRO_NETWORK_NODES=network.micro.mu")
-				log.Log("Setting default network micro.mu")
-			}
-			if v := os.Getenv("MICRO_NETWORK_TOKEN"); len(v) == 0 {
-				// set the network token
-				env = append(env, "MICRO_NETWORK_TOKEN=micro.mu")
-				log.Log("Setting default network token")
-			}
-		}
-
-		log.Info("Loading core services")
-
-		services := []string{
-			"network",  // :8085
-			"runtime",  // :8088
-			"registry", // :8000
-			"broker",   // :8001
-			"store",    // :8002
-			"tunnel",   // :8083
-			"router",   // :8084
-			"monitor",  // :????
-			"debug",    // :????
-			"proxy",    // :8081
-			"api",      // :8080
-			"web",      // :8082
-			"bot",      // :????
-		}
-
-		// create new micro runtime
-		muRuntime := cmd.DefaultCmd.Options().Runtime
-
-		// Use default update notifier
-		if context.GlobalBool("auto_update") {
-			options := []gorun.Option{
-				gorun.WithNotifier(update.NewNotifier(BuildDate)),
-			}
-			(*muRuntime).Init(options...)
-		}
-
-		for _, service := range services {
-			name = service
-			if namespace := context.GlobalString("namespace"); len(namespace) > 0 {
-				name = fmt.Sprintf("%s.%s", namespace, service)
-			}
-
-			log.Infof("Registering %s", name)
-
-			// runtime based on environment we run the service in
-			args := []gorun.CreateOption{
-				gorun.WithCommand(os.Args[0], service),
-				gorun.WithEnv(env),
-				gorun.WithOutput(os.Stdout),
-			}
-
-			// NOTE: we use BuildDate right now to check for the latest release
-			muService := &gorun.Service{Name: name, Version: BuildDate}
-			if err := (*muRuntime).Create(muService, args...); err != nil {
-				log.Errorf("Failed to create runtime enviroment: %v", err)
-				return
-			}
-		}
-
-		shutdown := make(chan os.Signal, 1)
-		signal.Notify(shutdown, syscall.SIGTERM, syscall.SIGINT, syscall.SIGQUIT)
-
-		log.Info("Starting service runtime")
-
-		// start the runtime
-		if err := (*muRuntime).Start(); err != nil {
-			log.Fatal(err)
-		}
-
-		log.Info("Service runtime started")
-
-		// TODO: should we launch the console?
-		// start the console
-		// cli.Init(context)
-
-		select {
-		case <-shutdown:
-			log.Info("Shutdown signal received")
-			log.Info("Stopping service runtime")
-		}
-
-		// stop all the things
-		if err := (*muRuntime).Stop(); err != nil {
-			log.Fatal(err)
-		}
-
-		log.Info("Service runtime shutdown")
-
-		// exit success
-		os.Exit(0)
-	}
+	// boot micro runtime
+	app.Action = platform.Run
 
 	setup(app)
 }
