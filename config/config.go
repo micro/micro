171b986a135df196fd574520ecfea584c169bd18
Asim Aslam <asim@aslam.me>
2020-03-11 22:51:58 +0000

    fix barronous cruft config (#613)
    * fix barronous cruft config
    
    * working config

--- config/config.go
+++ config/config.go
@@ -1,16 +1,24 @@
 package config
 
 import (
+	"context"
+	"encoding/base64"
+	"fmt"
+	"time"
+
 	"github.com/micro/cli/v2"
 	"github.com/micro/go-micro/v2"
 	"github.com/micro/go-micro/v2/config/cmd"
 	proto "github.com/micro/go-micro/v2/config/source/service/proto"
 	log "github.com/micro/go-micro/v2/logger"
 	"github.com/micro/micro/v2/config/db"
+	"github.com/micro/micro/v2/config/handler"
+
+	// TODO: decruft to just use the store
 	_ "github.com/micro/micro/v2/config/db/cockroach"
 	_ "github.com/micro/micro/v2/config/db/etcd"
 	_ "github.com/micro/micro/v2/config/db/memory"
-	"github.com/micro/micro/v2/config/handler"
+	_ "github.com/micro/micro/v2/config/db/store"
 )
 
 var (
@@ -18,6 +26,8 @@ var (
 	Name = "go.micro.config"
 	// Default database store
 	Database = "store"
+	// Default key
+	Namespace = "global"
 )
 
 func Run(c *cli.Context, srvOpts ...micro.Option) {
@@ -36,27 +46,164 @@ func Run(c *cli.Context, srvOpts ...micro.Option) {
 	srvOpts = append(srvOpts, micro.Name(Name))
 
 	service := micro.NewService(srvOpts...)
-	proto.RegisterConfigHandler(service.Server(), new(handler.Handler))
 
-	_ = service.Server().Subscribe(service.Server().NewSubscriber(handler.WatchTopic, handler.Watcher))
+	proto.RegisterConfigHandler(service.Server(), new(handler.Handler))
+	micro.RegisterSubscriber(handler.WatchTopic, service.Server(), handler.Watcher)
 
 	if err := db.Init(
 		db.WithDatabase(Database),
 		db.WithUrl(c.String("database_url")),
 		db.WithStore(*cmd.DefaultCmd.Options().Store),
 	); err != nil {
-		log.Fatalf("micro config init database error: %s", err)
+		log.Fatalf("config init database error: %s", err)
 	}
 
 	if err := service.Run(); err != nil {
-		log.Fatalf("micro config Run the service error: ", err)
+		log.Fatalf("config Run the service error: ", err)
 	}
+}
+
+func setConfig(ctx *cli.Context) error {
+	pb := proto.NewConfigService("go.micro.config", *cmd.DefaultCmd.Options().Client)
+
+	args := ctx.Args()
+
+	if args.Len() == 0 {
+		log.Fatal("Required usage; micro config set key val")
+	}
+
+	// key val
+	key := args.Get(0)
+	val := args.Get(1)
+
+	// TODO: allow the specifiying of a config.Key. This will be service name
+	// The actuall key-val set is a path e.g micro/accounts/key
+
+	_, err := pb.Update(context.TODO(), &proto.UpdateRequest{
+		Change: &proto.Change{
+			// global key
+			Namespace: Namespace,
+			// actual key for the value
+			Path: key,
+			// The value
+			ChangeSet: &proto.ChangeSet{
+				Data:      []byte(val),
+				Format:    "json",
+				Source:    "cli",
+				Timestamp: time.Now().Unix(),
+			},
+		},
+	})
+	if err != nil {
+		log.Fatalf("Error setting key-val: %v", err)
+	}
+
+	return nil
+}
+
+func getConfig(ctx *cli.Context) error {
+	pb := proto.NewConfigService("go.micro.config", *cmd.DefaultCmd.Options().Client)
+
+	args := ctx.Args()
+
+	if args.Len() == 0 {
+		log.Fatal("Required usage; micro config get key")
+	}
+
+	// key val
+	key := args.Get(0)
+
+	if len(key) == 0 {
+		log.Fatal("key cannot be blank")
+	}
+
+	// TODO: allow the specifiying of a config.Key. This will be service name
+	// The actuall key-val set is a path e.g micro/accounts/key
+
+	rsp, err := pb.Read(context.TODO(), &proto.ReadRequest{
+		// The global key,
+		Namespace: Namespace,
+		// The actual key for the val
+		Path: key,
+	})
+	if err != nil {
+		log.Fatalf("Error reading key-val: %v", err)
+	}
+
+	if rsp.Change == nil || rsp.Change.ChangeSet == nil {
+		return nil
+	}
+
+	// don't do it
+	if len(rsp.Change.ChangeSet.Data) == 0 {
+		return nil
+	}
+
+	v, err := base64.StdEncoding.DecodeString(string(rsp.Change.ChangeSet.Data))
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println("Value:", string(v))
+
+	return nil
+}
+
+func delConfig(ctx *cli.Context) error {
+	pb := proto.NewConfigService("go.micro.config", *cmd.DefaultCmd.Options().Client)
+
+	args := ctx.Args()
+
+	if args.Len() == 0 {
+		log.Fatal("Required usage; micro config get key")
+	}
+
+	// key val
+	key := args.Get(0)
+
+	if len(key) == 0 {
+		log.Fatal("key cannot be blank")
+	}
+
+	// TODO: allow the specifiying of a config.Key. This will be service name
+	// The actuall key-val set is a path e.g micro/accounts/key
+
+	_, err := pb.Delete(context.TODO(), &proto.DeleteRequest{
+		Change: &proto.Change{
+			// The global key,
+			Namespace: Namespace,
+			// The actual key for the val
+			Path: key,
+		},
+	})
+	if err != nil {
+		log.Fatalf("Error deleting key-val: %v", err)
+	}
+
+	return nil
 }
 
 func Commands(options ...micro.Option) []*cli.Command {
 	command := &cli.Command{
 		Name:  "config",
 		Usage: "Run the config server",
+		Subcommands: []*cli.Command{
+			{
+				Name:   "set",
+				Usage:  "Set a key-val; micro config set key val",
+				Action: setConfig,
+			},
+			{
+				Name:   "get",
+				Usage:  "Get a value; micro config get key",
+				Action: getConfig,
+			},
+			{
+				Name:   "del",
+				Usage:  "Delete a value; micro config del key",
+				Action: delConfig,
+			},
+		},
 		Action: func(ctx *cli.Context) error {
 			Run(ctx, options...)
 			return nil
--- config/handler/handler.go
+++ config/handler/handler.go
@@ -1,21 +1,20 @@
 package handler
 
 import (
+	"context"
+	"encoding/json"
 	"strings"
 	"sync"
 	"time"
 
-	"github.com/golang/protobuf/proto"
 	"github.com/micro/go-micro/v2/client"
 	cr "github.com/micro/go-micro/v2/config/reader"
-	"github.com/micro/go-micro/v2/config/reader/json"
+	jr "github.com/micro/go-micro/v2/config/reader/json"
 	"github.com/micro/go-micro/v2/config/source"
-	mp "github.com/micro/go-micro/v2/config/source/service/proto"
+	pb "github.com/micro/go-micro/v2/config/source/service/proto"
 	"github.com/micro/go-micro/v2/errors"
-	log "github.com/micro/go-micro/v2/logger"
 	"github.com/micro/go-micro/v2/store"
 	"github.com/micro/micro/v2/config/db"
-	"golang.org/x/net/context"
 )
 
 var (
@@ -24,35 +23,27 @@ var (
 	watchers     = make(map[string][]*watcher)
 
 	// we now support json only
-	reader = json.NewReader()
+	reader = jr.NewReader()
 	mtx    sync.RWMutex
 )
 
 type Handler struct{}
 
-func (c *Handler) Read(ctx context.Context, req *mp.ReadRequest, rsp *mp.ReadResponse) (err error) {
-	defer func() {
-		if err != nil {
-			log.Error(err)
-		}
-	}()
-
-	if len(req.Key) == 0 {
-		err = errors.BadRequest("go.micro.config.Read", "invalid id")
-		return err
+func (c *Handler) Read(ctx context.Context, req *pb.ReadRequest, rsp *pb.ReadResponse) error {
+	if len(req.Namespace) == 0 {
+		return errors.BadRequest("go.micro.config.Read", "invalid id")
 	}
 
-	ch, err := db.Read(req.Key)
+	ch, err := db.Read(req.Namespace)
 	if err != nil {
-		err = errors.BadRequest("go.micro.config.Read", "read error: %v", err)
-		return err
+		return errors.BadRequest("go.micro.config.Read", "read error: %v: %v", err, req.Namespace)
 	}
-	rsp.Change = &mp.Change{}
+
+	rsp.Change = new(pb.Change)
+
 	// Unmarshal value
-	err = proto.Unmarshal(ch.Value, rsp.Change)
-	if err != nil {
-		err = errors.BadRequest("go.micro.config.Read", "unmarshal value error: %v", err)
-		return err
+	if err = json.Unmarshal(ch.Value, rsp.Change); err != nil {
+		return errors.BadRequest("go.micro.config.Read", "unmarshal value error: %v", err)
 	}
 
 	// if dont need path, we return all of the data
@@ -60,19 +51,21 @@ func (c *Handler) Read(ctx context.Context, req *mp.ReadRequest, rsp *mp.ReadRes
 		return nil
 	}
 
-	rcc := rsp.Change.ChangeSet
+	rc := rsp.Change.ChangeSet
+
+	// generate reader.Values from the changeset
 	values, err := values(&source.ChangeSet{
-		Timestamp: time.Unix(rcc.Timestamp, 0),
-		Data:      rcc.Data,
-		Checksum:  rcc.Checksum,
-		Format:    rcc.Format,
-		Source:    rcc.Source,
+		Timestamp: time.Unix(rc.Timestamp, 0),
+		Data:      rc.Data,
+		Checksum:  rc.Checksum,
+		Format:    rc.Format,
+		Source:    rc.Source,
 	})
 	if err != nil {
-		err = errors.InternalServerError("go.micro.config.Read", err.Error())
-		return err
+		return errors.InternalServerError("go.micro.config.Read", err.Error())
 	}
 
+	// peel apart the path
 	parts := strings.Split(req.Path, PathSplitter)
 
 	// we just want to pass back bytes
@@ -81,122 +74,119 @@ func (c *Handler) Read(ctx context.Context, req *mp.ReadRequest, rsp *mp.ReadRes
 	return nil
 }
 
-func (c *Handler) Create(ctx context.Context, req *mp.CreateRequest, rsp *mp.CreateResponse) (err error) {
-	defer func() {
-		if err != nil {
-			log.Error(err)
-		}
-	}()
-
+func (c *Handler) Create(ctx context.Context, req *pb.CreateRequest, rsp *pb.CreateResponse) error {
 	if req.Change == nil || req.Change.ChangeSet == nil {
-		err = errors.BadRequest("go.micro.config.Create", "invalid change")
-		return err
+		return errors.BadRequest("go.micro.config.Create", "invalid change")
 	}
 
-	if len(req.Change.Key) == 0 {
-		err = errors.BadRequest("go.micro.config.Create", "invalid id")
-		return err
+	if len(req.Change.Namespace) == 0 {
+		return errors.BadRequest("go.micro.config.Create", "invalid id")
+	}
+
+	if len(req.Change.Path) > 0 {
+		vals, err := values(&source.ChangeSet{
+			Format: "json",
+		})
+		if err != nil {
+			return errors.InternalServerError("go.micro.config.Create", err.Error())
+		}
+
+		// peel apart the path
+		parts := strings.Split(req.Change.Path, PathSplitter)
+		// set the values
+		vals.Set(req.Change.ChangeSet.Data, parts...)
+		// change the changeset value
+		req.Change.ChangeSet.Data = vals.Bytes()
 	}
 
 	req.Change.ChangeSet.Timestamp = time.Now().Unix()
 
-	record := &store.Record{}
-	record.Value, err = proto.Marshal(req.Change)
-	if err != nil {
-		err = errors.BadRequest("go.micro.config.Create", "marshal error: %v", err)
-		return err
+	record := &store.Record{
+		Key: req.Change.Namespace,
 	}
 
-	record.Key = req.Change.Key
+	var err error
+	record.Value, err = json.Marshal(req.Change)
+	if err != nil {
+		return errors.BadRequest("go.micro.config.Create", "marshal error: %v", err)
+	}
 
 	if err := db.Create(record); err != nil {
-		err = errors.BadRequest("go.micro.config.Create", "create new into db error: %v", err)
-		return err
+		return errors.BadRequest("go.micro.config.Create", "create new into db error: %v", err)
 	}
 
-	_ = publish(ctx, &mp.WatchResponse{Key: req.Change.Key, ChangeSet: req.Change.ChangeSet})
+	_ = publish(ctx, &pb.WatchResponse{Namespace: req.Change.Namespace, ChangeSet: req.Change.ChangeSet})
 
 	return nil
 }
 
-func (c *Handler) Update(ctx context.Context, req *mp.UpdateRequest, rsp *mp.UpdateResponse) (err error) {
-	defer func() {
-		if err != nil {
-			log.Error(err)
-		}
-	}()
-
+func (c *Handler) Update(ctx context.Context, req *pb.UpdateRequest, rsp *pb.UpdateResponse) error {
 	if req.Change == nil || req.Change.ChangeSet == nil {
-		err = errors.BadRequest("go.micro.config.Update", "invalid change")
-		return err
+		return errors.BadRequest("go.micro.config.Update", "invalid change")
 	}
 
-	if len(req.Change.Key) == 0 {
-		err = errors.BadRequest("go.micro.config.Update", "invalid id")
-		return err
+	if len(req.Change.Namespace) == 0 {
+		return errors.BadRequest("go.micro.config.Update", "invalid id")
 	}
 
+	// set the changeset timestamp
 	req.Change.ChangeSet.Timestamp = time.Now().Unix()
 
+	oldCh := &pb.Change{}
+
 	// Get the current change set
-	record, err := db.Read(req.Change.Key)
+	record, err := db.Read(req.Change.Namespace)
 	if err != nil {
-		err = errors.BadRequest("go.micro.config.Update", "read old value error: %v", err)
-		return err
+		if err.Error() != "not found" {
+			return errors.BadRequest("go.micro.config.Update", "read old value error: %v", err)
+		}
+		// create new record
+		record = new(store.Record)
+		record.Key = req.Change.Namespace
+	} else {
+		// Unmarshal value
+		if err := json.Unmarshal(record.Value, oldCh); err != nil {
+			return errors.BadRequest("go.micro.config.Read", "unmarshal value error: %v", err)
+		}
 	}
 
-	ch := &mp.Change{}
-	// Unmarshal value
-	err = proto.Unmarshal(record.Value, ch)
-	if err != nil {
-		err = errors.BadRequest("go.micro.config.Read", "unmarshal value error: %v", err)
-		return err
+	// generate a new base changeset
+	changeSet := &source.ChangeSet{
+		Format: "json",
+		Data:   []byte(`{}`),
 	}
 
-	chc := ch.ChangeSet
-	change := &source.ChangeSet{
-		Timestamp: time.Unix(ch.ChangeSet.Timestamp, 0),
-		Data:      chc.Data,
-		Checksum:  chc.Checksum,
-		Source:    chc.Source,
-		Format:    chc.Format,
+	if oldCh.ChangeSet != nil {
+		changeSet = &source.ChangeSet{
+			Timestamp: time.Unix(oldCh.ChangeSet.Timestamp, 0),
+			Data:      oldCh.ChangeSet.Data,
+			Checksum:  oldCh.ChangeSet.Checksum,
+			Source:    oldCh.ChangeSet.Source,
+			Format:    oldCh.ChangeSet.Format,
+		}
 	}
 
 	var newChange *source.ChangeSet
 
 	// Set the change at a particular path
 	if len(req.Change.Path) > 0 {
-		// Unpack the data as a go type
-		var data interface{}
-		vals, err := values(&source.ChangeSet{Data: req.Change.ChangeSet.Data, Format: ch.ChangeSet.Format})
-		if err != nil {
-			err = errors.InternalServerError("go.micro.config.Update", "values error: %v", err)
-			return err
-		}
-		if err := vals.Get().Scan(&data); err != nil {
-			err = errors.InternalServerError("go.micro.config.Update", "scan data error: %v", err)
-			return err
-		}
-
 		// Get values from existing change
-		values, err := values(change)
+		values, err := values(changeSet)
 		if err != nil {
-			err = errors.InternalServerError("go.micro.config.Update", "get values from existing change error: %v", err)
-			return err
+			return errors.InternalServerError("go.micro.config.Update", "error getting existing change: %v", err)
 		}
 
 		// Apply the data to the existing change
-		values.Set(data, strings.Split(req.Change.Path, PathSplitter)...)
+		values.Set(req.Change.ChangeSet.Data, strings.Split(req.Change.Path, PathSplitter)...)
 
 		// Create a new change
 		newChange, err = merge(&source.ChangeSet{Data: values.Bytes()})
 		if err != nil {
-			err = errors.InternalServerError("go.micro.config.Update", "create a new change error: %v", err)
-			return err
+			return errors.InternalServerError("go.micro.config.Update", "create a new change error: %v", err)
 		}
 	} else {
 		// No path specified, business as usual
-		newChange, err = merge(change, &source.ChangeSet{
+		newChange, err = merge(changeSet, &source.ChangeSet{
 			Timestamp: time.Unix(req.Change.ChangeSet.Timestamp, 0),
 			Data:      req.Change.ChangeSet.Data,
 			Checksum:  req.Change.ChangeSet.Checksum,
@@ -204,13 +194,12 @@ func (c *Handler) Update(ctx context.Context, req *mp.UpdateRequest, rsp *mp.Upd
 			Format:    req.Change.ChangeSet.Format,
 		})
 		if err != nil {
-			err = errors.BadRequest("go.micro.srv.config.Update", "merge all error: %v", err)
-			return err
+			return errors.BadRequest("go.micro.srv.config.Update", "merge all error: %v", err)
 		}
 	}
 
 	// update change set
-	req.Change.ChangeSet = &mp.ChangeSet{
+	req.Change.ChangeSet = &pb.ChangeSet{
 		Timestamp: newChange.Timestamp.Unix(),
 		Data:      newChange.Data,
 		Checksum:  newChange.Checksum,
@@ -218,51 +207,39 @@ func (c *Handler) Update(ctx context.Context, req *mp.UpdateRequest, rsp *mp.Upd
 		Format:    newChange.Format,
 	}
 
-	record.Value, err = proto.Marshal(req.Change)
+	record.Value, err = json.Marshal(req.Change)
 	if err != nil {
-		err = errors.BadRequest("go.micro.config.Update", "marshal error: %v", err)
-		return err
+		return errors.BadRequest("go.micro.config.Update", "marshal error: %v", err)
 	}
 
 	if err := db.Update(record); err != nil {
-		err = errors.BadRequest("go.micro.config.Update", "update into db error: %v", err)
-		return err
+		return errors.BadRequest("go.micro.config.Update", "update into db error: %v", err)
 	}
 
-	_ = publish(ctx, &mp.WatchResponse{Key: req.Change.Key, ChangeSet: req.Change.ChangeSet})
+	_ = publish(ctx, &pb.WatchResponse{Namespace: req.Change.Namespace, ChangeSet: req.Change.ChangeSet})
 
 	return nil
 }
 
-func (c *Handler) Delete(ctx context.Context, req *mp.DeleteRequest, rsp *mp.DeleteResponse) (err error) {
-	defer func() {
-		if err != nil {
-			log.Error(err)
-		}
-	}()
-
+func (c *Handler) Delete(ctx context.Context, req *pb.DeleteRequest, rsp *pb.DeleteResponse) error {
 	if req.Change == nil {
-		err = errors.BadRequest("go.micro.srv.Delete", "invalid change")
-		return err
+		return errors.BadRequest("go.micro.srv.Delete", "invalid change")
 	}
 
-	if len(req.Change.Key) == 0 {
-		err = errors.BadRequest("go.micro.srv.Delete", "invalid id")
-		return err
+	if len(req.Change.Namespace) == 0 {
+		return errors.BadRequest("go.micro.srv.Delete", "invalid id")
 	}
 
 	if req.Change.ChangeSet == nil {
-		req.Change.ChangeSet = &mp.ChangeSet{}
+		req.Change.ChangeSet = &pb.ChangeSet{}
 	}
 
 	req.Change.ChangeSet.Timestamp = time.Now().Unix()
 
 	// We're going to delete the record as we have no path and no data
 	if len(req.Change.Path) == 0 {
-		if err := db.Delete(req.Change.Key); err != nil {
-			err = errors.BadRequest("go.micro.srv.Delete", "delete from db error: %v", err)
-			log.Error(err)
-			return err
+		if err := db.Delete(req.Change.Namespace); err != nil {
+			return errors.BadRequest("go.micro.srv.Delete", "delete from db error: %v", err)
 		}
 		return nil
 	}
@@ -270,19 +247,15 @@ func (c *Handler) Delete(ctx context.Context, req *mp.DeleteRequest, rsp *mp.Del
 	// We've got a path. Let's update the required path
 
 	// Get the current change set
-	record, err := db.Read(req.Change.Key)
+	record, err := db.Read(req.Change.Namespace)
 	if err != nil {
-		err = errors.BadRequest("go.micro.config.Update", "read old value error: %v", err)
-		return err
+		return errors.BadRequest("go.micro.config.Update", "read old value error: %v", err)
 	}
 
-	ch := &mp.Change{}
+	ch := &pb.Change{}
 	// Unmarshal value
-	err = proto.Unmarshal(record.Value, ch)
-	if err != nil {
-		err = errors.BadRequest("go.micro.config.Read", "unmarshal value error: %v", err)
-		log.Error(err)
-		return err
+	if err := json.Unmarshal(record.Value, ch); err != nil {
+		return errors.BadRequest("go.micro.config.Read", "unmarshal value error: %v", err)
 	}
 
 	// Get the current config as values
@@ -294,8 +267,7 @@ func (c *Handler) Delete(ctx context.Context, req *mp.DeleteRequest, rsp *mp.Del
 		Format:    ch.ChangeSet.Format,
 	})
 	if err != nil {
-		err = errors.BadRequest("go.micro.srv.Delete", "Get the current config as values error: %v", err)
-		return err
+		return errors.BadRequest("go.micro.srv.Delete", "Get the current config as values error: %v", err)
 	}
 
 	// Delete at the given path
@@ -304,12 +276,11 @@ func (c *Handler) Delete(ctx context.Context, req *mp.DeleteRequest, rsp *mp.Del
 	// Create a change record from the values
 	change, err := merge(&source.ChangeSet{Data: values.Bytes()})
 	if err != nil {
-		err = errors.BadRequest("go.micro.srv.Delete", "Create a change record from the values error: %v", err)
-		return err
+		return errors.BadRequest("go.micro.srv.Delete", "Create a change record from the values error: %v", err)
 	}
 
 	// Update change set
-	req.Change.ChangeSet = &mp.ChangeSet{
+	req.Change.ChangeSet = &pb.ChangeSet{
 		Timestamp: change.Timestamp.Unix(),
 		Data:      change.Data,
 		Checksum:  change.Checksum,
@@ -317,41 +288,31 @@ func (c *Handler) Delete(ctx context.Context, req *mp.DeleteRequest, rsp *mp.Del
 		Source:    change.Source,
 	}
 
-	record.Value, err = proto.Marshal(req.Change)
+	record.Value, err = json.Marshal(req.Change)
 	if err != nil {
-		err = errors.BadRequest("go.micro.config.Update", "marshal error: %v", err)
-		return err
+		return errors.BadRequest("go.micro.config.Update", "marshal error: %v", err)
 	}
 
 	if err := db.Update(record); err != nil {
-		err = errors.BadRequest("go.micro.srv.Delete", "update record set to db error: %v", err)
-		return err
+		return errors.BadRequest("go.micro.srv.Delete", "update record set to db error: %v", err)
 	}
 
-	_ = publish(ctx, &mp.WatchResponse{Key: req.Change.Key, ChangeSet: req.Change.ChangeSet})
+	_ = publish(ctx, &pb.WatchResponse{Namespace: req.Change.Namespace, ChangeSet: req.Change.ChangeSet})
 
 	return nil
 }
 
-func (c *Handler) List(ctx context.Context, req *mp.ListRequest, rsp *mp.ListResponse) (err error) {
-	defer func() {
-		if err != nil {
-			log.Error(err)
-		}
-	}()
-
+func (c *Handler) List(ctx context.Context, req *pb.ListRequest, rsp *pb.ListResponse) (err error) {
 	list, err := db.List()
 	if err != nil {
-		err = errors.BadRequest("go.micro.config.List", "query value error: %v", err)
-		return err
+		return errors.BadRequest("go.micro.config.List", "query value error: %v", err)
 	}
 
 	for _, v := range list {
-		ch := &mp.Change{}
-		err := proto.Unmarshal(v.Value, ch)
+		ch := &pb.Change{}
+		err := json.Unmarshal(v.Value, ch)
 		if err != nil {
-			err = errors.BadRequest("go.micro.config.Read", "unmarshal value error: %v", err)
-			return err
+			return errors.BadRequest("go.micro.config.Read", "unmarshal value error: %v", err)
 		}
 		rsp.Values = append(rsp.Values, ch)
 	}
@@ -359,22 +320,14 @@ func (c *Handler) List(ctx context.Context, req *mp.ListRequest, rsp *mp.ListRes
 	return nil
 }
 
-func (c *Handler) Watch(ctx context.Context, req *mp.WatchRequest, stream mp.Config_WatchStream) (err error) {
-	defer func() {
-		if err != nil {
-			log.Error(err)
-		}
-	}()
-
-	if len(req.Key) == 0 {
-		err = errors.BadRequest("go.micro.srv.Watch", "invalid id")
-		return err
+func (c *Handler) Watch(ctx context.Context, req *pb.WatchRequest, stream pb.Config_WatchStream) error {
+	if len(req.Namespace) == 0 {
+		return errors.BadRequest("go.micro.srv.Watch", "invalid id")
 	}
 
-	watch, err := Watch(req.Key)
+	watch, err := Watch(req.Namespace)
 	if err != nil {
-		err = errors.BadRequest("go.micro.srv.Watch", "watch error: %v", err)
-		return err
+		return errors.BadRequest("go.micro.srv.Watch", "watch error: %v", err)
 	}
 	defer watch.Stop()
 
@@ -382,22 +335,20 @@ func (c *Handler) Watch(ctx context.Context, req *mp.WatchRequest, stream mp.Con
 		ch, err := watch.Next()
 		if err != nil {
 			_ = stream.Close()
-			err = errors.BadRequest("go.micro.srv.Watch", "listen the Next error: %v", err)
-			return err
+			return errors.BadRequest("go.micro.srv.Watch", "listen the Next error: %v", err)
 		}
 
 		if err := stream.Send(ch); err != nil {
 			_ = stream.Close()
-			err = errors.BadRequest("go.micro.srv.Watch", "send the Change error: %v", err)
-			return err
+			return errors.BadRequest("go.micro.srv.Watch", "send the Change error: %v", err)
 		}
 	}
 }
 
 // Used as a subscriber between config services for events
-func Watcher(ctx context.Context, ch *mp.WatchResponse) error {
+func Watcher(ctx context.Context, ch *pb.WatchResponse) error {
 	mtx.RLock()
-	for _, sub := range watchers[ch.Key] {
+	for _, sub := range watchers[ch.Namespace] {
 		select {
 		case sub.next <- ch:
 		case <-time.After(time.Millisecond * 100):
@@ -416,7 +367,7 @@ func values(ch *source.ChangeSet) (cr.Values, error) {
 }
 
 // publish a change
-func publish(ctx context.Context, ch *mp.WatchResponse) error {
+func publish(ctx context.Context, ch *pb.WatchResponse) error {
 	req := client.NewMessage(WatchTopic, ch)
 	return client.Publish(ctx, req)
 }
