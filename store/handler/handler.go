289bb1030bb5f88fda6608922ca937faca9904f7
Vasiliy Tolstov <v.tolstov@unistack.org>
2020-03-17 14:33:58 +0300

    store/handler: fix (#645)
    * store/handler fix
    
    Signed-off-by: Vasiliy Tolstov <v.tolstov@unistack.org>

--- store/handler/handler.go
+++ store/handler/handler.go
@@ -70,7 +70,7 @@ func (s *Store) Read(ctx context.Context, req *pb.ReadRequest, rsp *pb.ReadRespo
 	}
 
 	vals, err := st.Read(req.Key, opts...)
-	if err == store.ErrNotFound {
+	if err != nil && err == store.ErrNotFound {
 		return errors.NotFound("go.micro.store", err.Error())
 	} else if err != nil {
 		return errors.InternalServerError("go.micro.store", err.Error())
@@ -103,7 +103,10 @@ func (s *Store) Write(ctx context.Context, req *pb.WriteRequest, rsp *pb.WriteRe
 		Expiry: time.Duration(req.Record.Expiry) * time.Second,
 	}
 
-	if err := st.Write(record); err != nil {
+	err = st.Write(record)
+	if err != nil && err == store.ErrNotFound {
+		return errors.NotFound("go.micro.store", err.Error())
+	} else if err != nil {
 		return errors.InternalServerError("go.micro.store", err.Error())
 	}
 
@@ -132,11 +135,12 @@ func (s *Store) List(ctx context.Context, req *pb.ListRequest, stream pb.Store_L
 	}
 
 	vals, err := st.List()
-	if err == store.ErrNotFound {
+	if err != nil && err == store.ErrNotFound {
 		return errors.NotFound("go.micro.store", err.Error())
 	} else if err != nil {
 		return errors.InternalServerError("go.micro.store", err.Error())
 	}
+
 	rsp := new(pb.ListResponse)
 
 	// TODO: batch sync
