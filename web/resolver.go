b96a59f38a4f1643c0446041daa9783f00cb3344
Asim Aslam <asim@aslam.me>
2020-04-07 00:34:18 +0100

    fix web resolver

--- web/resolver.go
+++ web/resolver.go
@@ -44,7 +44,7 @@ func (r *resolver) Resolve(req *http.Request) (*res.Endpoint, error) {
 	// split out ip
 	h, _, err := net.SplitHostPort(host)
 	if err != nil && err.Error() == "missing port in address" {
-		host = h
+		host = req.Host
 	} else if err == nil {
 		host = h
 	}
