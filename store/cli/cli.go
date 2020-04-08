9838ce038d0126f79846779f63e37014504585be
Jake Sanders <jake@micro.mu>
2020-04-08 16:22:17 +0100

    Add store sync command (#760)
    * Add store sync command
    
    * Add doc line

---
+++ store/cli/cli.go
@@ -0,0 +1,35 @@
+// Package cli implements the `micro store` subcommands
+// for example:
+//   micro store snapshot
+//   micro store restore
+//   micro store sync
+package cli
+
+import (
+	"github.com/micro/cli/v2"
+)
+
+// CommonFlags are flags common to cli commands snapshot and restore
+var CommonFlags = []cli.Flag{
+	&cli.StringFlag{
+		Name:    "backend",
+		Usage:   "Set the backend for the micro store",
+		EnvVars: []string{"MICRO_STORE_BACKEND"},
+		Value:   "memory",
+	},
+	&cli.StringFlag{
+		Name:    "nodes",
+		Usage:   "Comma separated list of Nodes to pass to the store backend",
+		EnvVars: []string{"MICRO_STORE_NODES"},
+	},
+	&cli.StringFlag{
+		Name:    "database",
+		Usage:   "Database option to pass to the store backend",
+		EnvVars: []string{"MICRO_STORE_DATABASE"},
+	},
+	&cli.StringFlag{
+		Name:    "table",
+		Usage:   "Table option to pass to the store backend",
+		EnvVars: []string{"MICRO_STORE_TABLE"},
+	},
+}
