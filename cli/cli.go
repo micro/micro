8012bf05ccc3142aa5d1c0c08114014ce7c08f02
Jake Sanders <jake@micro.mu>
2020-04-09 21:49:53 +0100

    Add the ability to view store contents, databases, tables form the command line (#772)
    * Add databases and table list
    
    * Implement Database and Tables
    
    * Store visibility commands
    
    * Bump go-micro
    
    * Fix help text

--- cli/cli.go
+++ cli/cli.go
@@ -340,6 +340,86 @@ func StoreCommands() []*cli.Command {
 				},
 			),
 		},
+		{
+			Name:   "databases",
+			Usage:  "List all databases known to the store service",
+			Action: storecli.Databases,
+			Flags: []cli.Flag{
+				&cli.StringFlag{
+					Name:  "store",
+					Usage: "store service to call",
+					Value: "go.micro.store",
+				},
+			},
+		},
+		{
+			Name:   "tables",
+			Usage:  "List all tables in the specified database known to the store service",
+			Action: storecli.Tables,
+			Flags: []cli.Flag{
+				&cli.StringFlag{
+					Name:  "store",
+					Usage: "store service to call",
+					Value: "go.micro.store",
+				},
+				&cli.StringFlag{
+					Name:  "database",
+					Usage: "database to list tables of",
+					Value: "micro",
+				},
+			},
+		},
+		{
+			Name:      "read",
+			Usage:     "read a record from the store (MICRO_STORE)",
+			UsageText: `micro store read [options] key`,
+			Action:    storecli.Read,
+			Flags: []cli.Flag{
+				&cli.StringFlag{
+					Name:  "database",
+					Usage: "database to write to",
+					Value: "micro",
+				},
+				&cli.StringFlag{
+					Name:  "table",
+					Usage: "table to write to",
+					Value: "micro",
+				},
+				&cli.StringFlag{
+					Name:  "output",
+					Usage: "output format (json, table)",
+					Value: "table",
+				},
+				&cli.BoolFlag{
+					Name:  "prefix",
+					Usage: "read prefix",
+					Value: false,
+				},
+			},
+		},
+		{
+			Name:      "write",
+			Usage:     "write a record to the store (MICRO_STORE)",
+			UsageText: `micro store write [options] key value`,
+			Action:    storecli.Write,
+			Flags: []cli.Flag{
+				&cli.StringFlag{
+					Name:  "expiry",
+					Usage: "expiry in time.ParseDuration format",
+					Value: "",
+				},
+				&cli.StringFlag{
+					Name:  "database",
+					Usage: "database to write to",
+					Value: "micro",
+				},
+				&cli.StringFlag{
+					Name:  "table",
+					Usage: "table to write to",
+					Value: "micro",
+				},
+			},
+		},
 	}
 }
 
