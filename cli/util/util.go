2ad400c72adcd345aedffdf9b63855028e115d4f
Janos Dobronszki <dobronszki@gmail.com>
2020-04-16 15:48:41 +0200

    Default env

--- cli/util/util.go
+++ cli/util/util.go
@@ -17,43 +17,15 @@ const (
 
 // SetupCommand includes things that should run for each command.
 func SetupCommand() {
-	env, err := config.Get("env")
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-	if len(env) == 0 {
-		fmt.Println("No env configured")
-		os.Exit(1)
-	}
 	os.Setenv("MICRO_PROXY", "service")
+	env, err := config.Get("env")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	if len(env) == 0 {
+		os.Setenv("MICRO_PROXY_ADDRESS", localAddress)
+		return
+	}
 	os.Setenv("MICRO_PROXY_ADDRESS", env)
-}
-
-// IsLocal returns true if we are connected to the local micro server
-func IsLocal() bool {
-	env, err := config.Get("env")
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-	if len(env) == 0 {
-		fmt.Println("No env configured")
-		os.Exit(1)
-	}
-	return env == localAddress
-}
-
-// IsLive returns true if we are connected to the live platform
-func IsLive() bool {
-	env, err := config.Get("env")
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-	if len(env) == 0 {
-		fmt.Println("No env configured")
-		os.Exit(1)
-	}
-	return env == liveAddress
 }
