088e06ab3121d9c63a6725c2544f532054bbb1cd
Janos Dobronszki <dobronszki@gmail.com>
2020-04-21 10:29:26 +0200

    See if micro is in path

--- test/runtime_test.go
+++ test/runtime_test.go
@@ -3,9 +3,19 @@
 package test
 
 import (
+	"log"
+	"os/exec"
 	"testing"
+	"time"
 )
 
 func TestMicroServer(t *testing.T) {
-
+	serverCmd := exec.Command("micro ", "server")
+	go func() {
+		if err := serverCmd.Start(); err != nil {
+			log.Fatal(err)
+		}
+	}()
+	defer serverCmd.Process.Kill()
+	time.Sleep(3 * time.Second)
 }
