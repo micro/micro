3519c327c3d3876ba3abb00fb2f90fae6901e830
Janos Dobronszki <dobronszki@gmail.com>
2020-04-21 14:32:08 +0200

    Refactor, example service

--- go.mod
+++ go.mod
@@ -29,7 +29,6 @@ require (
 	github.com/xlab/treeprint v0.0.0-20181112141820-a009c3971eca
 	golang.org/x/crypto v0.0.0-20200323165209-0ec3e9974c59
 	golang.org/x/net v0.0.0-20200301022130-244492dfa37a
-	golang.org/x/oauth2 v0.0.0-20191202225959-858c2ad4c8b6
 	golang.org/x/tools v0.0.0-20191216173652-a0e659d51361
 	google.golang.org/genproto v0.0.0-20191216164720-4f79533eabd1
 	google.golang.org/grpc v1.26.0
--- test/runtime_test.go
+++ test/runtime_test.go
@@ -10,18 +10,7 @@ import (
 	"time"
 )
 
-func TestMicroServerModeCall(t *testing.T) {
-	outp, err := exec.Command("micro", "env", "set", "server").CombinedOutput()
-	if err != nil {
-		t.Fatalf("Failed to set env to server, err: %v, output: %v", err, string(outp))
-	}
-
-	callCmd := exec.Command("micro", "call", "go.micro.runtime", "Runtime.Read", "{}")
-	outp, err = callCmd.CombinedOutput()
-	if err == nil {
-		t.Fatalf("Call to server should fail, got no error, output: %v", string(outp))
-	}
-
+func launchServer(t *testing.T) {
 	serverCmd := exec.Command("micro", "server")
 	go func() {
 		if err := serverCmd.Start(); err != nil {
@@ -34,9 +23,26 @@ func TestMicroServerModeCall(t *testing.T) {
 		}
 	}()
 	time.Sleep(1500 * time.Millisecond)
+}
+
+func TestMicroServerModeCall(t *testing.T) {
+	outp, err := exec.Command("micro", "env", "set", "server").CombinedOutput()
+	if err != nil {
+		t.Fatalf("Failed to set env to server, err: %v, output: %v", err, string(outp))
+	}
+
+	callCmd := exec.Command("micro", "call", "go.micro.runtime", "Runtime.Read", "{}")
+	outp, err = callCmd.CombinedOutput()
+	if err == nil {
+		t.Fatalf("Call to server should fail, got no error, output: %v", string(outp))
+	}
+
+	launchServer()
 
 	outp, err = exec.Command("micro", "call", "go.micro.runtime", "Runtime.Read", "{}").CombinedOutput()
 	if err != nil {
 		t.Fatalf("Call to runtime read should succeed, err: %v, outp: %v", err, string(outp))
 	}
 }
+
+func 
