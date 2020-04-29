85b636c6ef0a2584a43673951e9b3e788549b887
Janos Dobronszki <dobronszki@gmail.com>
2020-04-29 10:12:18 +0200

    Testing delete

--- test/config_test.go
+++ test/config_test.go
@@ -12,16 +12,16 @@ func TestConfig(t *testing.T) {
 	serv.launch()
 	defer serv.close()
 
-	getCmd := exec.Command("micro", "config", "get", "somevalue")
+	getCmd := exec.Command("micro", "config", "get", "somekey")
 	outp, err := getCmd.CombinedOutput()
 	if err == nil {
 		t.Fatalf("Config get should fail: %v", string(outp))
 	}
 	if string(outp) != "not found\n" {
-		t.Fatalf("Expected 'not found', got: '%v'", string(outp))
+		t.Fatalf("Expected 'not found\n', got: '%v'", string(outp))
 	}
 
-	setCmd := exec.Command("micro", "config", "set", "somevalue", "val1")
+	setCmd := exec.Command("micro", "config", "set", "somekey", "val1")
 	outp, err = setCmd.CombinedOutput()
 	if err != nil {
 		t.Fatal(err)
@@ -30,12 +30,30 @@ func TestConfig(t *testing.T) {
 		t.Fatalf("Expected no output, got: %v", string(outp))
 	}
 
-	getCmd = exec.Command("micro", "config", "get", "somevalue")
+	getCmd = exec.Command("micro", "config", "get", "somekey")
 	outp, err = getCmd.CombinedOutput()
 	if err != nil {
 		t.Fatal(err)
 	}
 	if string(outp) != "val1\n" {
-		t.Fatalf("Expected 'val1', got: '%v'", string(outp))
+		t.Fatalf("Expected 'val1\n', got: '%v'", string(outp))
+	}
+
+	delCmd := exec.Command("micro", "config", "del", "somekey")
+	outp, err = delCmd.CombinedOutput()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if string(outp) != "" {
+		t.Fatalf("Expected '', got: '%v'", string(outp))
+	}
+
+	getCmd = exec.Command("micro", "config", "get", "somekey")
+	outp, err = getCmd.CombinedOutput()
+	if err == nil {
+		t.Fatalf("Config get should fail: %v", string(outp))
+	}
+	if string(outp) != "not found\n" {
+		t.Fatalf("Expected 'not found\n', got: '%v'", string(outp))
 	}
 }
