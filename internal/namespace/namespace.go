b96491cfa89780347501c204a5d6091abb17e802
Ben Toogood <bentoogood@gmail.com>
2020-05-14 13:29:37 +0100

    Refactor namespace.FromContext

--- internal/namespace/namespace.go
+++ internal/namespace/namespace.go
@@ -17,16 +17,40 @@ const (
 
 // FromContext gets the namespace from the context
 func FromContext(ctx context.Context) string {
-	// if there is an account, we use its namespace
-	if acc, ok := auth.AccountFromContext(ctx); ok {
+	// get the namespace which is set at ingress by
+	// micro web / api / proxy etc
+	ns, _ := metadata.Get(ctx, NamespaceKey)
+
+	// get the account making the request. if there is
+	// no account then we return the namespace
+	acc, ok := auth.AccountFromContext(ctx)
+	if !ok && len(ns) > 0 {
+		return ns
+	} else if !ok {
+		return DefaultNamespace
+	}
+
+	// if no namespace was requested or it matches
+	// the accounts, return the accounts namespace
+	if len(ns) == 0 || ns == acc.Namespace {
 		return acc.Namespace
 	}
 
-	// next check for the namespace key set by micro web or api
-	if ns, ok := metadata.Get(ctx, NamespaceKey); ok {
+	// always allow access to the default namespace
+	if ns == DefaultNamespace {
 		return ns
 	}
 
-	// fallback to the default namespace
-	return DefaultNamespace
+	// allow the runtime access to all namespaces.
+	// TODO: grant runtime services elevated privelages
+	// and validate them here instead of assuming all
+	// services in the default namespace are the runtime.
+	if acc.Namespace == DefaultNamespace {
+		return ns
+	}
+
+	// a forbidden cross namespace request was made,
+	// return the accounts own namespace instead of
+	// the one requested
+	return acc.Namespace
 }
