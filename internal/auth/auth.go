ce725771d0b130309b0b1746c93fb04fca19f805
Ben Toogood <bentoogood@gmail.com>
2020-05-15 10:35:28 +0100

    Auth: Update rules to allow any user access to the runtime

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -4,20 +4,14 @@ import "github.com/micro/go-micro/v2/auth"
 
 // SystemRules are the default rules which are applied to the runtime services
 var SystemRules = map[string][]*auth.Resource{
-	"service": {
-		&auth.Resource{Namespace: auth.DefaultNamespace, Type: "*", Name: "*", Endpoint: "*"},
-	},
-	"admin": {
-		&auth.Resource{Namespace: auth.DefaultNamespace, Type: "*", Name: "*", Endpoint: "*"},
-	},
-	"developer": {
-		&auth.Resource{Namespace: auth.DefaultNamespace, Type: "*", Name: "*", Endpoint: "*"},
-	},
 	"*": {
+		&auth.Resource{Namespace: auth.DefaultNamespace, Type: "*", Name: "*", Endpoint: "*"},
+	},
+	"": {
 		&auth.Resource{Namespace: auth.DefaultNamespace, Type: "service", Name: "go.micro.auth", Endpoint: "Auth.Generate"},
 		&auth.Resource{Namespace: auth.DefaultNamespace, Type: "service", Name: "go.micro.auth", Endpoint: "Auth.Token"},
 		&auth.Resource{Namespace: auth.DefaultNamespace, Type: "service", Name: "go.micro.auth", Endpoint: "Auth.Inspect"},
 		&auth.Resource{Namespace: auth.DefaultNamespace, Type: "service", Name: "go.micro.registry", Endpoint: "Registry.GetService"},
-		&auth.Resource{Namespace: auth.DefaultNamespace, Type: "service", Name: "go.micro.registry", Endpoint: "*"},
+		&auth.Resource{Namespace: auth.DefaultNamespace, Type: "service", Name: "go.micro.registry", Endpoint: "Registry.ListServices"},
 	},
 }
