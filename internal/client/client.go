69ded9b8cf108a4865fe100e3f92160b50e9f1fa
Ben Toogood <bentoogood@gmail.com>
2020-05-19 17:45:19 +0100

    Pass Micro-Namespace in requests from the CLI

--- internal/client/client.go
+++ internal/client/client.go
@@ -2,11 +2,13 @@ package client
 
 import (
 	"context"
+	"strings"
 
 	"github.com/micro/go-micro/v2/auth"
 	"github.com/micro/go-micro/v2/client"
 	"github.com/micro/go-micro/v2/client/grpc"
 	"github.com/micro/go-micro/v2/metadata"
+	"github.com/micro/micro/v2/client/cli/util"
 	"github.com/micro/micro/v2/internal/config"
 )
 
@@ -14,17 +16,24 @@ import (
 // token found in config into each request
 func New() client.Client {
 	token, _ := config.Get("micro", "auth", "token")
-	return &wrapper{grpc.NewClient(), token}
+	env, _ := config.Get("env")
+	return &wrapper{grpc.NewClient(), token, env}
 }
 
 type wrapper struct {
 	client.Client
+
 	token string
+	env   string
 }
 
 func (a *wrapper) Call(ctx context.Context, req client.Request, rsp interface{}, opts ...client.CallOption) error {
 	if len(a.token) > 0 {
 		ctx = metadata.Set(ctx, "Authorization", auth.BearerScheme+a.token)
+	}
+	if len(a.env) > 0 && !util.IsLocal() && !util.IsServer() {
+		env := strings.ReplaceAll(a.env, "/", "-")
+		ctx = metadata.Set(ctx, "Micro-Namespace", env)
 	}
 	return a.Client.Call(ctx, req, rsp, opts...)
 }
