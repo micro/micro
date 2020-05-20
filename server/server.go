9139736d5ea43cd5acbe3563055a183e666a9f33
Janos Dobronszki <dobronszki@gmail.com>
2020-05-20 15:14:52 +0200

    Fix micro server with JWT, test the JWT tokens (#883)

--- server/server.go
+++ server/server.go
@@ -20,6 +20,7 @@ var (
 	services = []string{
 		// runtime services
 		"config",   // ????
+		"auth",     // :8010
 		"network",  // :8085
 		"runtime",  // :8088
 		"registry", // :8000
@@ -29,7 +30,6 @@ var (
 		"debug",    // :????
 		"proxy",    // :8081
 		"api",      // :8080
-		"auth",     // :8010
 		"web",      // :8082
 		"bot",      // :????
 		"init",     // no port, manage self
@@ -146,12 +146,18 @@ func Run(context *cli.Context) error {
 		}
 
 		log.Infof("Registering %s", name)
+		// @todo this is a hack
+		envs := env
+		switch service {
+		case "proxy", "web", "api":
+			envs = append(envs, "MICRO_AUTH=service")
+		}
 
 		// runtime based on environment we run the service in
 		args := []gorun.CreateOption{
 			gorun.WithCommand(os.Args[0]),
 			gorun.WithArgs(service),
-			gorun.WithEnv(env),
+			gorun.WithEnv(envs),
 			gorun.WithOutput(os.Stdout),
 		}
 
