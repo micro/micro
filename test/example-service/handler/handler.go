1c1ab6e2d079d64bd7642bd3475af671ca7a8afb
Janos Dobronszki <dobronszki@gmail.com>
2020-05-26 17:11:17 +0200

    Revert changes made by local running of tests

--- test/example-service/handler/handler.go
+++ test/example-service/handler/handler.go
@@ -13,6 +13,6 @@ type Example struct{}
 // Call is a single request handler called via client.Call or the generated client code
 func (e *Example) Call(ctx context.Context, req *example.Request, rsp *example.Response) error {
 	log.Log("Received Example.Call request")
-	rsp.Msg = "Hi " + req.Name
+	rsp.Msg = "Hello " + req.Name
 	return nil
 }
