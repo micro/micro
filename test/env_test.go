43ff11d949686ee4e552216df0a1db0d6c59034c
Janos Dobronszki <dobronszki@gmail.com>
2020-05-26 17:33:17 +0200

    Add basic env test

--- test/env_test.go
+++ test/env_test.go
@@ -8,6 +8,25 @@ import (
 	"testing"
 )
 
+func TestEnvBasic(t *testing.T) {
+	trySuite(t, testEnvOverrides, retryCount)
+}
+
+func testEnvBasic(t *t) {
+	outp, err := exec.Command("micro", "-env=platform", "env").CombinedOutput()
+	if err != nil {
+		t.Fatal(err)
+		return
+	}
+	if !strings.Contains(string(outp), "platform") ||
+		!strings.Contains(string(outp), "server") ||
+		!strings.Contains(string(outp), "local") {
+		t.Fatal("Env output lacks local, server, or platform")
+		return
+	}
+
+}
+
 func TestEnvOverrides(t *testing.T) {
 	trySuite(t, testEnvOverrides, retryCount)
 }
