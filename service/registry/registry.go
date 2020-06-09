7e82fcb1f8eda0298612445e51eebc9bb676b616
Dominic Wong <domwongemail@googlemail.com>
2020-06-09 15:50:51 +0100

    Show help if subcommand is not recognised rather than run the core service (#933)
    * Show help if subcommand is not recognised rather than run the server

--- service/registry/registry.go
+++ service/registry/registry.go
@@ -12,6 +12,7 @@ import (
 	"github.com/micro/go-micro/v2/registry/service"
 	pb "github.com/micro/go-micro/v2/registry/service/proto"
 	rcli "github.com/micro/micro/v2/client/cli"
+	"github.com/micro/micro/v2/internal/helper"
 	"github.com/micro/micro/v2/service/registry/handler"
 )
 
@@ -111,20 +112,6 @@ func Run(ctx *cli.Context, srvOpts ...micro.Option) {
 	// get server id
 	id := service.Server().Options().Id
 
-	/*
-		// create the subscriber
-		s := &subscriber{
-			Id:       id,
-			Registry: service.Options().Registry,
-		}
-
-		// register the subscriber
-			if err := micro.RegisterSubscriber(Topic, service.Server(), s); err != nil {
-				log.Debugf("failed to subscribe to events: %s", err)
-				os.Exit(1)
-			}
-	*/
-
 	// register the handler
 	pb.RegisterRegistryHandler(service.Server(), &handler.Registry{
 		Id:        id,
@@ -151,6 +138,9 @@ func Commands(options ...micro.Option) []*cli.Command {
 			},
 		},
 		Action: func(ctx *cli.Context) error {
+			if err := helper.UnexpectedSubcommand(ctx); err != nil {
+				return err
+			}
 			Run(ctx, options...)
 			return nil
 		},
