a30c479011e5c5e462e5b03b3de7f3565d1c1947
Dominic Wong <domwongemail@googlemail.com>
2020-06-19 13:22:17 +0100

    Return quickly from unrecognised command rather than trying to run auth generate (#957)

--- test/misc_test.go
+++ test/misc_test.go
@@ -145,3 +145,12 @@ func testHelps(t *t) {
 		}
 	}
 }
+
+func TestUnrecognisedCommand(t *testing.T) {
+	t.Parallel()
+	outp, _ := exec.Command("micro", "foobar").CombinedOutput()
+	if !strings.Contains(string(outp), "Unrecognized micro command: foobar. Please refer to 'micro --help'") {
+		t.Fatalf("micro foobar does not return correct error %v", string(outp))
+		return
+	}
+}
