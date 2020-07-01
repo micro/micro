97371e26afc0df92847704111831e79adc5cc8f2
Janos Dobronszki <dobronszki@gmail.com>
2020-07-01 09:34:33 +0200

    Fix

--- test/m3o_test.go
+++ test/m3o_test.go
@@ -28,13 +28,14 @@ func testM3oSignupFlow(t *t) {
 	defer serv.close()
 
 	envToConfigKey := map[string]string{
-		"MICRO_STRIPE_API_KEY":       "micro.payments.stripe.api_key",
-		"MICRO_SENDGRID_API_KEY":     "micro.signup.sendgrid.api_key",
-		"MICRO_SENDGRID_TEMPLATE_ID": "micro.signup.sendgrid.template_id",
-		"MICRO_STRIPE_PLAN_ID":       "micro.signup.sendgrid.template_id",
-		"MICRO_STRIPE_PLAN_ID":       "micro.signup.plan_id",
-		"MICRO_EMAIL_FROM":           "micro.signup.email_from",
-		"MICRO_TEST_ENV", "micro.test_env",
+		"MICRO_STRIPE_API_KEY":           "micro.payments.stripe.api_key",
+		"MICRO_SENDGRID_API_KEY":         "micro.signup.sendgrid.api_key",
+		"MICRO_SENDGRID_TEMPLATE_ID":     "micro.signup.sendgrid.template_id",
+		"MICRO_STRIPE_PLAN_ID":           "micro.signup.sendgrid.template_id",
+		"MICRO_STRIPE_PLAN_ID":           "micro.signup.plan_id",
+		"MICRO_EMAIL_FROM":               "micro.signup.email_from",
+		"MICRO_TEST_ENV":                 "micro.signup.test_env",
+		"MICRO_STRIPE_PAYMENT_METHOD_ID": "micro.signup.test_payment_method",
 	}
 
 	for envKey, configKey := range envToConfigKey {
@@ -42,7 +43,7 @@ func testM3oSignupFlow(t *t) {
 		if len(val) == 0 {
 			t.Fatalf("'%v' flag is missing", envKey)
 		}
-		outp, err = exec.Command("micro", serv.envFlag(), "config", "set", configKey, val.CombinedOutput()
+		outp, err = exec.Command("micro", serv.envFlag(), "config", "set", configKey, val).CombinedOutput()
 		if err != nil {
 			t.Fatal(string(outp))
 		}
