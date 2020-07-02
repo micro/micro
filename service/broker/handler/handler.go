449a050b61eba4e0d887072034ab163b28df8248
Ben Toogood <bentoogood@gmail.com>
2020-07-02 14:34:50 +0100

    service/broker: authorize requests

--- service/broker/handler/handler.go
+++ service/broker/handler/handler.go
@@ -3,6 +3,7 @@ package handler
 import (
 	"context"
 
+	"github.com/micro/go-micro/v2/auth"
 	"github.com/micro/go-micro/v2/broker"
 	pb "github.com/micro/go-micro/v2/broker/service/proto"
 	"github.com/micro/go-micro/v2/errors"
@@ -10,12 +11,19 @@ import (
 	"github.com/micro/micro/v2/internal/namespace"
 )
 
+const defaultNamespace = "micro"
+
 type Broker struct {
 	Broker broker.Broker
 }
 
 func (b *Broker) Publish(ctx context.Context, req *pb.PublishRequest, rsp *pb.Empty) error {
 	ns := namespace.FromContext(ctx)
+
+	// authorize the request
+	if err := authorizeNamespace(ctx, ns); err != nil {
+		return err
+	}
 
 	log.Debugf("Publishing message to %s topic in the %v namespace", req.Topic, ns)
 	err := b.Broker.Publish(ns+"."+req.Topic, &broker.Message{
@@ -31,9 +39,14 @@ func (b *Broker) Publish(ctx context.Context, req *pb.PublishRequest, rsp *pb.Em
 
 func (b *Broker) Subscribe(ctx context.Context, req *pb.SubscribeRequest, stream pb.Broker_SubscribeStream) error {
 	ns := namespace.FromContext(ctx)
-	errChan := make(chan error, 1)
+
+	// authorize the request
+	if err := authorizeNamespace(ctx, ns); err != nil {
+		return err
+	}
 
 	// message handler to stream back messages from broker
+	errChan := make(chan error, 1)
 	handler := func(p broker.Event) error {
 		if err := stream.Send(&pb.Message{
 			Header: p.Message().Header,
@@ -67,4 +80,26 @@ func (b *Broker) Subscribe(ctx context.Context, req *pb.SubscribeRequest, stream
 		log.Debugf("Subscription error for topic %s: %v", req.Topic, err)
 		return err
 	}
+}
+
+// authorizeNamespace returns an error if the context doesn't have access to the given namespace
+func authorizeNamespace(ctx context.Context, namespace string) error {
+	if namespace == defaultNamespace {
+		return nil
+	}
+
+	// accounts are always required so we can identify the caller. If auth is not configured, the noop
+	// auth implementation will return a blank account with the default domain set, allowing the caller
+	// access to all resources
+	acc, ok := auth.AccountFromContext(ctx)
+	if !ok {
+		return errors.Unauthorized("go.micro.registry", "An account is required")
+	}
+
+	// ensure the account is requesing access to it's own domain
+	if acc.Issuer != namespace {
+		return errors.Forbidden("go.micro.registry", "An account issued by %v is required", namespace)
+	}
+
+	return nil
 }
