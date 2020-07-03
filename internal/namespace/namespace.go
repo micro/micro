bc4c174d26895c5cc9b7b6dc76cd209f8cf3d82e
Ben Toogood <bentoogood@gmail.com>
2020-07-03 12:36:46 +0100

    internal/namespace: add authorize method

--- internal/namespace/namespace.go
+++ internal/namespace/namespace.go
@@ -2,11 +2,23 @@ package namespace
 
 import (
 	"context"
+	"errors"
 
+	"github.com/micro/go-micro/v2/auth"
 	"github.com/micro/go-micro/v2/metadata"
 )
 
+var (
+	// ErrUnauthorized is returned by Authorize when a context without a blank account tries to access
+	// a restricted namespace
+	ErrUnauthorized = errors.New("An account is required")
+	// ErrForbidden is returned by Authorize when a context is trying to access a namespace it doesn't
+	// have access to
+	ErrForbidden = errors.New("Access denied to namespace")
+)
+
 const (
+	// DefaultNamespace used by the server
 	DefaultNamespace = "micro"
 	// NamespaceKey is used to set/get the namespace from the context
 	NamespaceKey = "Micro-Namespace"
@@ -23,4 +35,32 @@ func FromContext(ctx context.Context) string {
 // ContextWithNamespace sets the namespace in the context
 func ContextWithNamespace(ctx context.Context, ns string) context.Context {
 	return metadata.Set(ctx, NamespaceKey, ns)
+}
+
+// Authorize will return an error if the context cannot access the given namespace
+func Authorize(ctx context.Context, namespace string) error {
+	// anyone can access the default namespace
+	if namespace == DefaultNamespace {
+		return nil
+	}
+
+	// accounts are always required so we can identify the caller. If auth is not configured, the noop
+	// auth implementation will return a blank account with the default namespace set, allowing the caller
+	// access to all resources
+	acc, ok := auth.AccountFromContext(ctx)
+	if !ok {
+		return ErrUnauthorized
+	}
+
+	// the server can access all namespaces
+	if acc.Issuer == DefaultNamespace {
+		return nil
+	}
+
+	// ensure the account is requesing access to it's own namespace
+	if acc.Issuer != namespace {
+		return ErrForbidden
+	}
+
+	return nil
 }
