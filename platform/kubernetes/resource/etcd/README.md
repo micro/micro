0f613bcbbb60904adaa091edcb79387c368f2e63
Ben Toogood <bentoogood@gmail.com>
2020-07-08 10:47:56 +0100

    platform/kubernetes/resource/etcd: secure etcd

--- platform/kubernetes/resource/etcd/README.md
+++ platform/kubernetes/resource/etcd/README.md
@@ -20,3 +20,42 @@ helm list
 helm delete etcd-cluster
 ```
 
+Note: When connecting to etcd, the ca and client certs must be used, e.g:
+
+```yaml
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  namespace: default
+  name: micro-registry
+spec:
+  replicas: 1
+  template:
+    spec:
+      containers:
+      - name: registry
+        env:
+        - name: MICRO_REGISTRY
+          value: "etcd"
+        - name: MICRO_REGISTRY_ADDRESS
+          value: "etcd-cluster"
+        - name: MICRO_REGISTRY_SECURE
+          value: "true"
+        - name: MICRO_CERTIFICATE_AUTHORITIES
+          value: "/certs/registry/ca.crt"
+        args:
+        - registry
+        image: bentoogood/micro:mtls
+        imagePullPolicy: Always
+        ports:
+        - containerPort: 8000
+          name: registry-port
+        volumeMounts:
+        - name: etcd-client-certs
+          mountPath: "/certs/registry"
+          readOnly: true
+      volumes:
+      - name: etcd-client-certs
+        secret:
+          secretName: etcd-client-certs
+```
--- platform/kubernetes/resource/etcd/install.sh
+++ platform/kubernetes/resource/etcd/install.sh
@@ -1,17 +1,34 @@
 #!/bin/bash
 
+# move into the certs directory
+cd certs;
 
-#helm repo add bitnami https://charts.bitnami.com/bitnami
-#helm install etcd-cluster bitnami/etcd \
-#	--set auth.rbac.enabled=false \
-#	--set auth.client.enableAuthentication=false \
-#	--set livenessProbe.enabled=false \
-#	--set readinessProbe.enabled=false
+# generate a certificate authority
+cfssl gencert -initca ca-csr.json | cfssljson -bare ca -
 
-helm repo add stable https://kubernetes-charts.storage.googleapis.com
-helm install etcd-cluster stable/etcd-operator \
-	--set resources.limits.cpu=500m \
-	--set resources.limits.memory=512Mi \
-	--set cluster.version=v3.4.9 \
-	--set cluster.size=3 \
-	--set customResources.createEtcdClusterCRD=true
+# generate certificates for client, server and peer
+cfssl gencert -ca=ca.pem -ca-key=ca-key.pem -config=ca-config.json -profile=client client.json | cfssljson -bare client;
+cfssl gencert -ca=ca.pem -ca-key=ca-key.pem -config=ca-config.json -profile=server server.json | cfssljson -bare server;
+cfssl gencert -ca=ca.pem -ca-key=ca-key.pem -config=ca-config.json -profile=peer peer.json | cfssljson -bare peer;
+
+# create the secrets in etcd
+kubectl create secret generic etcd-client-certs --from-file=ca.crt=ca.pem --from-file=cert.pem=client.pem --from-file=key.pem=client-key.pem;
+kubectl create secret generic etcd-server-certs --from-file=ca.crt=ca.pem --from-file=cert.pem=server.pem --from-file=key.pem=server-key.pem;
+kubectl create secret generic etcd-peer-certs --from-file=ca.crt=ca.pem --from-file=cert.pem=peer.pem --from-file=key.pem=peer-key.pem;
+
+# move back into the /etcd directory
+cd ../;
+
+# install the cluster using helm
+helm repo add bitnami https://charts.bitnami.com/bitnami
+helm install etcd-cluster bitnami/etcd \
+	--set auth.rbac.enabled=false \
+	--set auth.peer.secureTransport=true \
+	--set auth.peer.enableAuthentication=true \
+	--set auth.peer.existingSecret=etcd-peer-certs \
+	--set auth.client.secureTransport=true \
+	--set auth.client.enableAuthentication=true \
+	--set auth.client.existingSecret=etcd-server-certs \
+	--set statefulset.replicaCount=3 \
+	--set livenessProbe.enabled=false \
+	--set readinessProbe.enabled=false
