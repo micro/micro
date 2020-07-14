d5f942bfab9bda4966c3312df9159ba042d44bcb
Ben Toogood <bentoogood@gmail.com>
2020-07-14 11:40:14 +0100

    test/m3o: change _ to -

--- test/m3o_test.go
+++ test/m3o_test.go
@@ -142,8 +142,8 @@ func testM3oSignupFlow(t *t) {
 		defer func() {
 			namespace.Remove(ns, serv.envName)
 		}()
-		if strings.Count(ns, "_") != 2 {
-			t.Fatalf("Expected 2 underscores in namespace but namespace is: %v", ns)
+		if strings.Count(ns, "-") != 2 {
+			t.Fatalf("Expected 2 dashes in namespace but namespace is: %v", ns)
 			return
 		}
 		t.t.Logf("Namespace set is %v", ns)
