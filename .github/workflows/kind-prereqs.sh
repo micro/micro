85db197ffd2d8a5347392faaf978d39f4e440a61
Dominic Wong <dom@micro.mu>
2020-07-17 10:26:41 +0100

    doh

--- .github/workflows/kind-prereqs.sh
+++ .github/workflows/kind-prereqs.sh
@@ -3,6 +3,6 @@ git clone https://github.com/cloudflare/cfssl.git
 pushd cfssl 
 make
 popd
-echo "::addpath::$(pwd)/cfssl/bin"
+echo "::add-path::$(pwd)/cfssl/bin"
 # yq is used to manipulate yaml
 GO111MODULE=on go get github.com/mikefarah/yq/v3
