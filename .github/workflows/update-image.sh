85848cc6e1cd852de1414d7b05cc4feb5032e941
Dominic Wong <dom@micro.mu>
2020-07-20 11:19:17 +0100

    update yamls

---
+++ .github/workflows/update-image.sh
@@ -0,0 +1,5 @@
+set -x
+branch_name=${GITHUB_REF#refs/heads/}
+branch_name=${branch_name//\//-}
+sed_expression="s/: micro\/micro/: micro\/integbuilds:$branch_name/g"
+sed -e '$sed_expression' -i.bak platform/kubernetes/network/*.yaml
