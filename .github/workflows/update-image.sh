2dc30ed8d92eca5120412e387d46c3aa33ce1724
Dominic Wong <dom@micro.mu>
2020-07-20 11:20:17 +0100

    debugging

--- .github/workflows/update-image.sh
+++ .github/workflows/update-image.sh
@@ -3,3 +3,4 @@ branch_name=${GITHUB_REF#refs/heads/}
 branch_name=${branch_name//\//-}
 sed_expression="s/: micro\/micro/: micro\/integbuilds:$branch_name/g"
 sed -e '$sed_expression' -i.bak platform/kubernetes/network/*.yaml
+cat platform/kubernetes/network/proxy.yaml
