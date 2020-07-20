bddb4174fcc9c09401e1e14115da02c053aeff3d
Dominic Wong <dom@micro.mu>
2020-07-20 11:27:54 +0100

    tweak sed

--- .github/workflows/update-image.sh
+++ .github/workflows/update-image.sh
@@ -2,5 +2,5 @@ set -x
 branch_name=${GITHUB_REF#refs/heads/}
 branch_name=${branch_name//\//-}
 sed_expression="s/: micro\/micro/: micro\/integbuilds:$branch_name/g"
-sed -e '$sed_expression' -i.bak platform/kubernetes/network/*.yaml
+sed -e $sed_expression -i.bak platform/kubernetes/network/*.yaml
 cat platform/kubernetes/network/proxy.yaml
