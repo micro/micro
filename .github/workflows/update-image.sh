fc446c246a8294971a35bc7296c31edf24ea305d
Dominic Wong <dom@micro.mu>
2020-07-22 15:05:23 +0100

    use local reg for kind tests

--- .github/workflows/update-image.sh
+++ .github/workflows/update-image.sh
@@ -1,5 +1,5 @@
 set -x
 branch_name=${GITHUB_REF#refs/heads/}
 branch_name=${branch_name//\//-}
-sed_expression="s/: micro\/micro/: micro\/integbuilds:$branch_name/g"
+sed_expression="s/: micro\/micro/: localhost:5000\/micro/g"
 sed -e "$sed_expression" -i.bak platform/kubernetes/network/*.yaml
