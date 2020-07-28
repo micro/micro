83499ed6c18eaa59a8be99513663cb29ec19b704
Janos Dobronszki <dobronszki@gmail.com>
2020-07-28 10:36:31 +0200

    Added file upload test

---
+++ test/file_test.go
@@ -0,0 +1,46 @@
+// +build integration
+
+package test
+
+import (
+	"fmt"
+	"os/exec"
+	"strings"
+	"testing"
+	"time"
+)
+
+func TestFileUpload(t *testing.T) {
+	trySuite(t, testFileUpload, retryCount)
+}
+
+func testFileUpload(t *t) {
+	t.Parallel()
+	serv := newServer(t, options{
+		auth: "jwt",
+	})
+	defer serv.close()
+	if err := serv.launch(); err != nil {
+		return
+	}
+
+	login(serv, t, "default", "password")
+
+	outp, err := exec.Command("micro", serv.envFlag(), "upload", "./example-service").CombinedOutput()
+	if err != nil {
+		t.Fatal(string(outp))
+	}
+
+	if err := try("Test store for existence of the file", t, func() ([]byte, error) {
+		outp, err := exec.Command("micro", serv.envFlag(), "store", "list", "--table", "server").CombinedOutput()
+		if err != nil {
+			return outp, err
+		}
+		if !strings.Contains(string(outp), "example-service") {
+			return outp, fmt.Errorf("Output should contain example service")
+		}
+		return outp, nil
+	}, 15*time.Second); err != nil {
+		return
+	}
+}
