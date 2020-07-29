e34b60e936d0d36bf61113a2bc3cd7b9bdc7fada
Ben Toogood <bentoogood@gmail.com>
2020-07-29 14:30:56 +0100

    cmd/protoc-gen-micro: update to use v3 defaults

--- service/server/server.go
+++ service/server/server.go
@@ -10,3 +10,23 @@ import (
 var DefaultServer server.Server = grpc.NewServer(
 	server.Registry(registry.DefaultRegistry),
 )
+
+// Register a handler
+func Handle(hdlr server.Handler) error {
+	return DefaultServer.Handle(hdlr)
+}
+
+// Create a new handler
+func NewHandler(hdlr interface{}, opts ...server.HandlerOption) server.Handler {
+	return DefaultServer.NewHandler(hdlr, opts...)
+}
+
+// Create a new subscriber
+func NewSubscriber(topic string, hdlr interface{}, opts ...server.SubscriberOption) server.Subscriber {
+	return DefaultServer.NewSubscriber(topic, hdlr, opts...)
+}
+
+// Register a subscriber
+func Subscribe(sub server.Subscriber) error {
+	return DefaultServer.Subscribe(sub)
+}
