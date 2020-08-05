0e6eea03fadc236205f3696b089ba6d4a8cf9346
Asim Aslam <asim@aslam.me>
2020-08-05 11:31:23 +0100

    Specify env to create and shrink cockroachdb where necessary

--- platform/kubernetes/install.sh
+++ platform/kubernetes/install.sh
@@ -1,10 +1,26 @@
 #!/bin/bash
 
+# which env to deploy to. does not yet switch k8s context
+ENV=$1
+# default to size for platform
+DB_SIZE=100Gi
+
 # Run this script to install the platform on a kubernetes cluster. 
 
 # NOTE: This script will not set the cloudflare or slack tokens in the secret. Hence, the 
 # clients (web, api, proxy, bot) will have a status of "CreateContainerConfigError" until these
 # secrets are manually added.
+
+# expect an env to be specified
+if [ "$ENV" == "" ]; then
+  echo "Must specify env e.g ./install.sh {dev|staging|platform}"
+  exit 1
+fi
+
+## Set DB to smaller size for staging
+if [ "$ENV" != "platform" ]; then
+  DB_SIZE=25Gi
+fi
 
 # Generate keys for JWT auth.
 ssh-keygen -f /tmp/jwt -m pkcs8 -q -N "";
@@ -22,7 +38,7 @@ rm /tmp/jwt /tmp/jwt.pub /tmp/jwt-base64 /tmp/jwt-base64.pub
 
 # install the resources
 cd ./resource/cockroachdb;
-bash install.sh;
+bash install.sh $DB_SIZE;
 cd ../etcd;
 bash install.sh;
 cd ../nats;
@@ -30,7 +46,19 @@ bash install.sh;
 
 # move to the /kubernetes folder and apply the deployments
 cd ../..;
+
+# replace m3o.com with m3o.dev
+if [ $ENV == "staging" ]; then
+  sed -i 's@m3o.com@m3o.dev@g' network/*.yaml
+fi
+
+# execute the yaml
 kubectl apply -f network
+
+# replace back
+if [ $ENV == "staging" ]; then
+  sed -i 's@m3o.dev@m3o.com@g' network/*.yaml
+fi
 
 # go back to the top level
 cd ..;
