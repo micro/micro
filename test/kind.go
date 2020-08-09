3c365a97793a1488f99307f27b310e2317e15f36
Asim Aslam <asim@aslam.me>
2020-08-09 23:09:51 +0100

    more updates

--- test/kind.go
+++ test/kind.go
@@ -83,7 +83,7 @@ func (s *testK8sServer) Run() error {
 	}
 
 	// generate a new admin account for the env : user=ENV_NAME pass=password
-	req := fmt.Sprintf(`{"id":"%s", "secret":"password", "options":{"namespace":"%s"}}`, s.Env(), s.namespace)
+	req := fmt.Sprintf(`{"id":"%s", "secret":"micro", "options":{"namespace":"%s"}}`, s.Env(), s.namespace)
 	outp, err := s.Command().Exec("call", "auth", "Auth.Generate", req)
 	if err != nil && !strings.Contains(string(outp), "already exists") { // until auth.Delete is implemented
 		s.t.Fatalf("Error generating auth: %s, %s", err, outp)
@@ -92,7 +92,7 @@ func (s *testK8sServer) Run() error {
 
 	ChangeNamespace(s.Command(), s.Env(), s.Env())
 	if s.opts.Login {
-		Login(s, s.t, s.Env(), "password")
+		Login(s, s.t, s.Env(), "micro")
 	}
 
 	return nil
