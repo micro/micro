8e31049dc3c231d276d791cdcde870552c131aa6
Ben Toogood <bentoogood@gmail.com>
2020-08-12 09:23:55 +0100

    login to default admin account

--- test/kind.go
+++ test/kind.go
@@ -84,16 +84,9 @@ func (s *testK8sServer) Run() error {
 	// switch to the namespace
 	ChangeNamespace(s.Command(), s.Env(), s.Env())
 
-	// generate a new admin account for the env : user=ENV_NAME pass=password
-	req := fmt.Sprintf(`{"id":"%s", "secret":"micro"}`, s.Env())
-	outp, err := s.Command().Exec("call", "auth", "Auth.Generate", req)
-	if err != nil && !strings.Contains(string(outp), "already exists") { // until auth.Delete is implemented
-		s.t.Fatalf("Error generating auth: %s, %s", err, outp)
-		return err
-	}
-
+	// login to the admin account which is generated for each namespace
 	if s.opts.Login {
-		Login(s, s.t, s.Env(), "micro")
+		Login(s, s.t, "admin", "micro")
 	}
 
 	return nil
