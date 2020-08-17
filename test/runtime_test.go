7bb1119fdc5cf886119788787d2ae458cf3b85b6
Janos Dobronszki <dobronszki@gmail.com>
2020-08-17 19:10:53 +0200

    Higher timeouts again

--- test/runtime_test.go
+++ test/runtime_test.go
@@ -642,7 +642,7 @@ func testRunPrivateSource(t *T) {
 	// call the service
 	if err := Try("Calling helloworld", t, func() ([]byte, error) {
 		return cmd.Exec("helloworld", "--name=John")
-	}, 10*time.Second); err != nil {
+	}, 30*time.Second); err != nil {
 		return
 	}
 }
