c9ad681c297f6a9890e6fdc6a241ac0b11f5f89e
Janos Dobronszki <dobronszki@gmail.com>
2020-08-18 18:03:30 +0200

    Fix update & test update

--- test/runtime_test.go
+++ test/runtime_test.go
@@ -779,4 +779,51 @@ func testRunPrivateGitlabSource(t *T) {
 	}, 30*time.Second); err != nil {
 		return
 	}
+
+	// update service
+
+	// run the service
+	if outp, err := cmd.Exec("run", "--image", "localhost:5000/cells:go", "gitlab.com/micro-test/private-monorepo-test/subfolder-test"); err != nil {
+		t.Fatalf("Expected no error, got %v %v", err, string(outp))
+		return
+	}
+
+	// We need a better way of knowing the server restarted than a sleep...
+	time.Sleep(5 * time.Second)
+
+	if err := Try("Find micro/test/helloworld in runtime", t, func() ([]byte, error) {
+		outp, err := cmd.Exec("status")
+		if err != nil {
+			return outp, err
+		}
+
+		if !statusRunning("subfolder-test", "latest", outp) {
+			return outp, errors.New("Can't find helloworld service in runtime")
+		}
+		return outp, err
+	}, 60*time.Second); err != nil {
+		return
+	}
+
+	if err := Try("Find helloworld in registry", t, func() ([]byte, error) {
+		outp, err := cmd.Exec("services")
+		if err != nil {
+			return outp, err
+		}
+		if !strings.Contains(string(outp), "example") {
+			return outp, errors.New("Does not contain example")
+		}
+		return outp, err
+	}, 300*time.Second); err != nil {
+		outp, _ := cmd.Exec("logs", "subfolder-test")
+		t.Log(string(outp))
+		return
+	}
+
+	// call the service
+	if err := Try("Calling example", t, func() ([]byte, error) {
+		return cmd.Exec("example", "--name=John")
+	}, 30*time.Second); err != nil {
+		return
+	}
 }
