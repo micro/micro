996a09de5eeccec827a1944daaba74e485016073
Ben Toogood <bentoogood@gmail.com>
2020-08-18 10:01:00 +0100

    test: fix syntax error

--- test/test_framework.go
+++ test/test_framework.go
@@ -367,8 +367,8 @@ func (s *ServerDefault) Run() error {
 			!strings.Contains(string(out), "config") ||
 			!strings.Contains(string(out), "proxy") ||
 			!strings.Contains(string(out), "auth") ||
+			!strings.Contains(string(out), "events") ||
 			!strings.Contains(string(out), "store") {
-			!strings.Contains(string(out), "events") {
 			return out, errors.New("Not ready")
 		}
 
