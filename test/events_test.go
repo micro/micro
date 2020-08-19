9926ffd4b4686615343ac7a9d654c7afaa39d3eb
Ben Toogood <bentoogood@gmail.com>
2020-08-19 09:38:11 +0100

    test: add test coverage for events

---
+++ test/events_test.go
@@ -0,0 +1,47 @@
+// +build integration
+
+package test
+
+import (
+	"errors"
+	"strings"
+	"testing"
+	"time"
+)
+
+func TestEventsStream(t *testing.T) {
+	TrySuite(t, testEventsStream, retryCount)
+}
+
+func testEventsStream(t *T) {
+	t.Parallel()
+	serv := NewServer(t, WithLogin(), WithNamespace("micro"))
+	defer serv.Close()
+	if err := serv.Run(); err != nil {
+		return
+	}
+
+	cmd := serv.Command()
+
+	outp, err := cmd.Exec("run", "./service/stream")
+	if err != nil {
+		t.Fatalf("micro run failure, output: %v", string(outp))
+		return
+	}
+
+	if err := Try("Check logs for success", t, func() ([]byte, error) {
+		outp, err := cmd.Exec("logs", "test/service/stream")
+		if err != nil {
+			return outp, err
+		}
+		if !strings.Contains(string(outp), "Published event ok") {
+			return outp, errors.New("Published event log not found")
+		}
+		if !strings.Contains(string(outp), "Recieved event ok") {
+			return outp, errors.New("Recieved event log not found")
+		}
+		return outp, nil
+	}, 30*time.Second); err != nil {
+		return
+	}
+}
