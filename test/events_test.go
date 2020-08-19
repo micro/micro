5266db42136c1e1099a52dccb16f2eae725581cc
Ben Toogood <bentoogood@gmail.com>
2020-08-19 16:16:21 +0100

    increase test timeout

--- test/events_test.go
+++ test/events_test.go
@@ -28,6 +28,7 @@ func testEventsStream(t *T) {
 	var outp []byte
 	// Temp fix to support k8s tests until we have file upload to remote server
 	if ref := os.Getenv("GITHUB_REF"); len(ref) > 0 {
+		ref = strings.TrimPrefix(ref, "refs/heads/")
 		t.Logf("Running service from the %v branch of micro", ref)
 		outp, err = cmd.Exec("run", "github.com/micro/micro/test/service/stream@"+ref)
 	} else {
@@ -39,10 +40,11 @@ func testEventsStream(t *T) {
 	}
 
 	if err := Try("Check logs for success", t, func() ([]byte, error) {
-		outp, err := cmd.Exec("logs", "stream")
+		outp, err := cmd.Exec("logs", "-n", "200", "stream")
 		if err != nil {
 			return outp, err
 		}
+		t.Logf(string(outp))
 		if !strings.Contains(string(outp), "Published event ok") {
 			return outp, errors.New("Published event log not found")
 		}
@@ -50,7 +52,7 @@ func testEventsStream(t *T) {
 			return outp, errors.New("Recieved event log not found")
 		}
 		return outp, nil
-	}, 120*time.Second); err != nil {
+	}, 180*time.Second); err != nil {
 		return
 	}
 }
