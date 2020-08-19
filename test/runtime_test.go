4d5c4addb2c119bb2f4f616a0f839daaf900edd4
Asim Aslam <asim@aslam.me>
2020-08-19 16:28:20 +0100

    remove logging

--- test/runtime_test.go
+++ test/runtime_test.go
@@ -706,12 +706,8 @@ func testRunPrivateSource(t *T) {
 
 	// call the service
 	if err := Try("Calling helloworld", t, func() ([]byte, error) {
-		outp, _ := cmd.Exec("logs", "helloworld")
-		t.Log(string(outp))
 		return cmd.Exec("helloworld", "--name=John")
 	}, 30*time.Second); err != nil {
-		outp, _ := cmd.Exec("logs", "helloworld")
-		t.Log(string(outp))
 		return
 	}
 }
