8b0f41f3741b35b6ec37cd216a9ed7ba783d7896
ben-toogood <bentoogood@gmail.com>
2020-08-20 11:27:42 +0100

    events: update to new interface (#1280)
    * events: update to new interface
    
    * test/service: update stream service to use new interface

--- service/events/server/server.go
+++ service/events/server/server.go
@@ -54,15 +54,12 @@ func (h *handler) Publish(ctx context.Context, req *pb.PublishRequest, rsp *pb.P
 	if req.Timestamp > 0 {
 		opts = append(opts, goevents.WithTimestamp(time.Unix(req.Timestamp, 0)))
 	}
-	if req.Payload != nil {
-		opts = append(opts, goevents.WithPayload(req.Payload))
-	}
 	if req.Metadata != nil {
 		opts = append(opts, goevents.WithMetadata(req.Metadata))
 	}
 
 	// publish the event
-	if err := events.Publish(req.Topic, opts...); err != nil {
+	if err := events.Publish(req.Topic, req.Payload, opts...); err != nil {
 		return errors.InternalServerError("events.Stream.Publish", err.Error())
 	}
 
@@ -87,12 +84,9 @@ func (h *handler) Subscribe(ctx context.Context, req *pb.SubscribeRequest, rsp p
 	if len(req.Queue) > 0 {
 		opts = append(opts, goevents.WithQueue(req.Queue))
 	}
-	if len(req.Topic) > 0 {
-		opts = append(opts, goevents.WithTopic(req.Topic))
-	}
 
 	// create the subscriber
-	evChan, err := events.Subscribe(opts...)
+	evChan, err := events.Subscribe(req.Topic, opts...)
 	if err != nil {
 		return errors.InternalServerError("events.Stream.Subscribe", err.Error())
 	}
--- test/service/stream/go.mod
+++ test/service/stream/go.mod
@@ -7,7 +7,7 @@ replace google.golang.org/grpc => google.golang.org/grpc v1.26.0
 replace github.com/micro/micro/v3 => ../../..
 
 require (
-	github.com/micro/go-micro/v3 v3.0.0-beta.0.20200818203829-5d6b7b3d7d3c
+	github.com/micro/go-micro/v3 v3.0.0-beta.0.20200820082929-29e8cdbfe9b9
 	github.com/micro/micro/v3 v3.0.0-beta.0.20200817215434-d519cfc25878
 	github.com/micro/services v0.10.0 // indirect
 )
