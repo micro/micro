7494f2eab645ea6e81ed63943cdf17a9f3555265
Asim Aslam <asim@aslam.me>
2020-08-21 09:50:26 +0100

    Adhere to the go-micro router changes (#1285)

--- internal/resolver/web/resolver.go
+++ internal/resolver/web/resolver.go
@@ -48,12 +48,11 @@ func (r *Resolver) Resolve(req *http.Request, opts ...res.ResolveOption) (*res.E
 	}
 
 	// lookup the routes for the service
-	query := []router.QueryOption{
-		router.QueryService(name),
-		router.QueryNetwork(options.Domain),
+	query := []router.LookupOption{
+		router.LookupNetwork(options.Domain),
 	}
 
-	routes, err := r.Router.Lookup(query...)
+	routes, err := r.Router.Lookup(name, query...)
 	if err == router.ErrRouteNotFound {
 		return nil, res.ErrNotFound
 	} else if err != nil {
