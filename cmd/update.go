79d2141afb9556a92eadea3142b0aa56d2f247f6
Asim Aslam <asim@aslam.me>
2020-08-26 11:21:49 +0100

    Update blurb

--- cmd/update.go
+++ cmd/update.go
@@ -23,7 +23,7 @@ func confirmAndSelfUpdate() (bool, error) {
 		return false, nil
 	}
 
-	fmt.Print("Do you want to update to ", latest.Version, "? (yes/no): ")
+	fmt.Print("New version found. Do you want to update to ", latest.Version, "? (yes/no): ")
 	input, err := bufio.NewReader(os.Stdin).ReadString('\n')
 	if err != nil || (input != "yes\n" && input != "no\n") {
 		return false, fmt.Errorf("Invalid response")
