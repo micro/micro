02408306ae14aa2bb784cbe289aad02b33d6152d
Dominic Wong <dom@m3o.com>
2020-08-28 16:01:47 +0100

    parse dynamic args as more than just string

--- test/service/example/handler/handler.go
+++ test/service/example/handler/handler.go
@@ -14,6 +14,9 @@ type Example struct{}
 func (e *Example) Call(ctx context.Context, req *pb.Request, rsp *pb.Response) error {
 	fmt.Println("Received Example.Call request")
 	rsp.Msg = "Hello " + req.Name
+	if req.Number > 0 {
+		rsp.Msg = fmt.Sprintf("%s %d", rsp.Msg, req.Number)
+	}
 	if req.Caps {
 		rsp.Msg = strings.ToUpper(rsp.Msg)
 	}
