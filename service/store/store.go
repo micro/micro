9cc08e3843fabdffc98d57eddcea41c61edfbe0a
Ben Toogood <bentoogood@gmail.com>
2020-09-11 09:40:52 +0100

    service/store: add type aliases

--- service/store/store.go
+++ service/store/store.go
@@ -10,13 +10,18 @@ var (
 	DefaultStore store.Store = client.NewStore()
 )
 
+type (
+	// Record is an alias for store.Record
+	Record = store.Record
+)
+
 // Read takes a single key name and optional ReadOptions. It returns matching []*Record or an error.
-func Read(key string, opts ...store.ReadOption) ([]*store.Record, error) {
+func Read(key string, opts ...store.ReadOption) ([]*Record, error) {
 	return DefaultStore.Read(key, opts...)
 }
 
 // Write a record to the store, and returns an error if the record was not written.
-func Write(r *store.Record, opts ...store.WriteOption) error {
+func Write(r *Record, opts ...store.WriteOption) error {
 	return DefaultStore.Write(r, opts...)
 }
 
