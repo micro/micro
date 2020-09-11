4b21c8ea3a401eaf3e3c8fbe568c4410a3fb0c0b
Ben Toogood <bentoogood@gmail.com>
2020-09-11 09:27:45 +0100

    service/client: add type aliases

--- service/client/client.go
+++ service/client/client.go
@@ -10,27 +10,36 @@ import (
 // DefaultClient for the service
 var DefaultClient client.Client = grpc.NewClient()
 
+type (
+	// Message is an alias for client.Message
+	Message = client.Message
+	// Request is an alias for client.Request
+	Request = client.Request
+	// xStream is an alias for client.Stream
+	xStream = client.Stream
+)
+
 // NewMessage returns a message which can be published
-func NewMessage(topic string, msg interface{}, opts ...client.MessageOption) client.Message {
+func NewMessage(topic string, msg interface{}, opts ...client.MessageOption) Message {
 	return DefaultClient.NewMessage(topic, msg, opts...)
 }
 
 // NewRequest returns a request can which be executed using Call or Stream
-func NewRequest(service, endpoint string, req interface{}, reqOpts ...client.RequestOption) client.Request {
+func NewRequest(service, endpoint string, req interface{}, reqOpts ...client.RequestOption) Request {
 	return DefaultClient.NewRequest(service, endpoint, req, reqOpts...)
 }
 
 // Call performs a request
-func Call(ctx context.Context, req client.Request, rsp interface{}, opts ...client.CallOption) error {
+func Call(ctx context.Context, req Request, rsp interface{}, opts ...client.CallOption) error {
 	return DefaultClient.Call(ctx, req, rsp, opts...)
 }
 
 // Stream performs a streaming request
-func Stream(ctx context.Context, req client.Request, opts ...client.CallOption) (client.Stream, error) {
+func Stream(ctx context.Context, req Request, opts ...client.CallOption) (xStream, error) {
 	return DefaultClient.Stream(ctx, req, opts...)
 }
 
 // Publish a message
-func Publish(ctx context.Context, msg client.Message, opts ...client.PublishOption) error {
+func Publish(ctx context.Context, msg Message, opts ...client.PublishOption) error {
 	return DefaultClient.Publish(ctx, msg, opts...)
 }
