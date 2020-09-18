a3c7651257b74463128066e1c65cbefa7881fe43
Ben Toogood <bentoogood@gmail.com>
2020-09-18 12:24:51 +0100

    test/service/stream: fix to run with micro run

--- test/service/stream/client/main.go
+++ test/service/stream/client/main.go
@@ -2,13 +2,12 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"io"
-	"log"
 	"math/rand"
 	"time"
 
 	"github.com/micro/micro/v3/service"
+	"github.com/micro/micro/v3/service/logger"
 	pb "github.com/micro/micro/v3/test/service/stream/proto"
 )
 
@@ -17,46 +16,55 @@ func main() {
 	cli := pb.NewRouteGuideService("stream", srv.Client())
 
 	// Looking for a valid feature
+	logger.Infof("Testing Unary... Starting")
 	printFeature(cli, &pb.Point{Latitude: 409146138, Longitude: -746188906})
-
-	// Feature missing.
 	printFeature(cli, &pb.Point{Latitude: 0, Longitude: 0})
+	logger.Infof("Testing Unary... Done")
 
 	// Looking for features between 40, -75 and 42, -73.
+	logger.Infof("Testing Server Streaming... Starting")
 	printFeatures(cli, &pb.Rectangle{
 		Lo: &pb.Point{Latitude: 400000000, Longitude: -750000000},
 		Hi: &pb.Point{Latitude: 420000000, Longitude: -730000000},
 	})
+	logger.Infof("Testing Server Streaming... Done")
 
 	// RecordRoute
+	logger.Infof("Testing Client Streaming... Starting")
 	runRecordRoute(cli)
+	logger.Infof("Testing Client Streaming... Done")
 
 	// RouteChat
+	logger.Infof("Testing Bidirectional Streaming... Starting")
 	runRouteChat(cli)
+	logger.Infof("Testing Bidirectional Streaming... Done")
 
-	log.Println("Client completed ok")
+	logger.Infof("Client completed ok")
+
+	// prevent the client from restarting when running using micro run
+	srv.Run()
 }
 
 // printFeature gets the feature for the given point.
 func printFeature(client pb.RouteGuideService, point *pb.Point) {
-	log.Printf("Getting feature for point (%d, %d)", point.Latitude, point.Longitude)
+	logger.Tracef("Getting feature for point (%d, %d)", point.Latitude, point.Longitude)
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 	feature, err := client.GetFeature(ctx, point)
 	if err != nil {
-		log.Fatalf("%v.GetFeatures(_) = _, %v: ", client, err)
+		logger.Fatalf("%v.GetFeatures(_) = _, %v: ", client, err)
 	}
-	log.Println("Got Features:", feature)
+	logger.Tracef("Got Features: %v", feature)
 }
 
 // printFeatures lists all the features within the given bounding Rectangle.
 func printFeatures(client pb.RouteGuideService, rect *pb.Rectangle) {
-	log.Printf("Looking for features within %v", rect)
+	logger.Tracef("Looking for features within %v", rect)
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 	stream, err := client.ListFeatures(ctx, rect)
 	if err != nil {
-		log.Fatalf("%v.ListFeatures(_) = _, %v", client, err)
+		logger.Fatalf("%v.ListFeatures(_) = _, %v", client, err)
 	}
 	for {
 		feature, err := stream.Recv()
@@ -64,9 +72,9 @@ func printFeatures(client pb.RouteGuideService, rect *pb.Rectangle) {
 			break
 		}
 		if err != nil {
-			log.Fatalf("%v.ListFeatures(_) = _, %v", client, err)
+			logger.Fatalf("%v.ListFeatures(_) = _, %v", client, err)
 		}
-		log.Println("Got Feature:", feature)
+		logger.Tracef("Got Feature: %v", feature)
 	}
 }
 
@@ -79,25 +87,24 @@ func runRecordRoute(client pb.RouteGuideService) {
 	for i := 0; i < pointCount; i++ {
 		points = append(points, randomPoint(r))
 	}
-	log.Printf("Traversing %d points.", len(points))
+	logger.Tracef("Traversing %d points.", len(points))
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 	stream, err := client.RecordRoute(ctx)
 	if err != nil {
-		log.Fatalf("%v.RecordRoute(_) = _, %v", client, err)
+		logger.Fatalf("%v.RecordRoute(_) = _, %v", client, err)
 	}
 	for _, point := range points {
 		if err := stream.Send(point); err != nil {
-			log.Fatalf("%v.Send(%v) = %v", stream, point, err)
+			logger.Fatalf("%v.Send(%v) = %v", stream, point, err)
 		}
 	}
 
-	fmt.Println("Closing and waiting to recv")
 	summary, err := stream.CloseAndRecv()
 	if err != nil {
-		log.Fatalf("%v.CloseAndRecv() got error %v, want %v", stream, err, nil)
+		logger.Fatalf("%v.CloseAndRecv() got error %v, want %v", stream, err, nil)
 	}
-	log.Printf("Route summary: %v", summary)
+	logger.Tracef("Route summary: %v", summary)
 }
 
 // runRouteChat receives a sequence of route notes, while sending notes for various locations.
@@ -114,7 +121,7 @@ func runRouteChat(client pb.RouteGuideService) {
 	defer cancel()
 	stream, err := client.RouteChat(ctx)
 	if err != nil {
-		log.Fatalf("%v.RouteChat(_) = _, %v", client, err)
+		logger.Fatalf("%v.RouteChat(_) = _, %v", client, err)
 	}
 	waitc := make(chan struct{})
 	go func() {
@@ -126,18 +133,24 @@ func runRouteChat(client pb.RouteGuideService) {
 				return
 			}
 			if err != nil {
-				log.Fatalf("Failed to receive a note : %v", err)
+				logger.Fatalf("Failed to receive a note : %v", err)
 			}
-			log.Printf("Got message %s at point(%d, %d)", in.Message, in.Location.Latitude, in.Location.Longitude)
+			logger.Tracef("Got message %s at point(%d, %d)", in.Message, in.Location.Latitude, in.Location.Longitude)
 		}
 	}()
 	for _, note := range notes {
 		if err := stream.Send(note); err != nil {
-			log.Fatalf("Failed to send a note: %v", err)
+			logger.Fatalf("Failed to send a note: %v", err)
 		}
 	}
 	if err := stream.Close(); err != nil {
-		log.Fatalf("%v.Close() got error: %v, want %v", stream, err, nil)
+		logger.Fatalf("%v.Close() got error: %v, want %v", stream, err, nil)
 	}
 	<-waitc
+}
+
+func randomPoint(r *rand.Rand) *pb.Point {
+	lat := (r.Int31n(180) - 90) * 1e7
+	long := (r.Int31n(360) - 180) * 1e7
+	return &pb.Point{Latitude: lat, Longitude: long}
 }
