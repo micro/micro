8b8165c8f59748db68f42a56bcc4199312f6f469
Ben Toogood <bentoogood@gmail.com>
2020-09-21 12:30:09 +0100

    Revert "service/runtime/cli: refactor util methods"
    This reverts commit 259525761da6f265f7bef13e10dbde55b1fbb8ec.

--- service/runtime/cli/service.go
+++ service/runtime/cli/service.go
@@ -4,12 +4,16 @@ package runtime
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"net/http"
 	"os"
 	"os/signal"
+	"path"
 	"path/filepath"
 	"sort"
 	"strings"
 	"text/tabwriter"
+	"time"
 
 	golog "github.com/micro/go-micro/v3/logger"
 	"github.com/micro/go-micro/v3/runtime/local/source/git"
@@ -17,6 +21,7 @@ import (
 	"github.com/micro/micro/v3/client/cli/namespace"
 	"github.com/micro/micro/v3/client/cli/util"
 	cliutil "github.com/micro/micro/v3/client/cli/util"
+	"github.com/micro/micro/v3/internal/config"
 	muclient "github.com/micro/micro/v3/service/client"
 	"github.com/micro/micro/v3/service/context"
 	"github.com/micro/micro/v3/service/logger"
@@ -37,13 +42,8 @@ const (
 	GetUsage = "Get the status of services"
 	// ServicesUsage message for micro services command
 	ServicesUsage = "micro services"
-	// LogUsage message for logs command
-	LogUsage = "Required usage: micro log example"
 	// CannotWatch message for the run command
 	CannotWatch = "Cannot watch filesystem on this runtime"
-	// credentialsKey is the key for the secret in which git credentials should be passed when
-	// creating or updating a service
-	credentialsKey = "GIT_CREDENTIALS"
 )
 
 var (
@@ -51,9 +51,121 @@ var (
 	DefaultRetries = 3
 	// DefaultImage which should be run
 	DefaultImage = "micro/cells:micro"
-	// GitOrgs we currently support for credentials
+	// Git orgs we currently support for credentials
 	GitOrgs = []string{"github", "bitbucket", "gitlab"}
 )
+
+const (
+	credentialsKey = "GIT_CREDENTIALS"
+)
+
+// timeAgo returns the time passed
+func timeAgo(v string) string {
+	if len(v) == 0 {
+		return "unknown"
+	}
+	t, err := time.Parse(time.RFC3339, v)
+	if err != nil {
+		return v
+	}
+
+	return fmt.Sprintf("%v ago", fmtDuration(time.Since(t)))
+}
+
+func fmtDuration(d time.Duration) string {
+	// round to secs
+	d = d.Round(time.Second)
+
+	var resStr string
+	days := d / (time.Hour * 24)
+	if days > 0 {
+		d -= days * time.Hour * 24
+		resStr = fmt.Sprintf("%dd", days)
+	}
+	h := d / time.Hour
+	if len(resStr) > 0 || h > 0 {
+		d -= h * time.Hour
+		resStr = fmt.Sprintf("%s%dh", resStr, h)
+	}
+	m := d / time.Minute
+	if len(resStr) > 0 || m > 0 {
+		d -= m * time.Minute
+		resStr = fmt.Sprintf("%s%dm", resStr, m)
+	}
+	s := d / time.Second
+	resStr = fmt.Sprintf("%s%ds", resStr, s)
+	return resStr
+}
+
+// exists returns whether the given file or directory exists
+func dirExists(path string) (bool, error) {
+	_, err := os.Stat(path)
+	if err == nil {
+		return true, nil
+	}
+	if os.IsNotExist(err) {
+		return false, nil
+	}
+	return true, err
+}
+
+func sourceExists(source *git.Source) error {
+	ref := source.Ref
+	if ref == "" || ref == "latest" {
+		ref = "master"
+	}
+
+	sourceExistsAt := func(url string, source *git.Source) error {
+		req, _ := http.NewRequest("GET", url, nil)
+
+		// add the git credentials if set
+		if creds, ok := getGitCredentials(source.Repo); ok {
+			req.Header.Set("Authorization", "token "+creds)
+		}
+
+		client := new(http.Client)
+		resp, err := client.Do(req)
+
+		// @todo gracefully degrade?
+		if err != nil {
+			return err
+		}
+		if resp.StatusCode >= 400 && resp.StatusCode < 500 {
+			return fmt.Errorf("service at %v@%v not found", source.Repo, ref)
+		}
+		return nil
+	}
+
+	if strings.Contains(source.Repo, "github") {
+		// Github specific existence checs
+		repo := strings.ReplaceAll(source.Repo, "github.com/", "")
+		url := fmt.Sprintf("https://api.github.com/repos/%v/contents/%v?ref=%v", repo, source.Folder, ref)
+		return sourceExistsAt(url, source)
+	} else if strings.Contains(source.Repo, "gitlab") {
+		// Gitlab specific existence checks
+
+		// @todo better check for gitlab
+		url := fmt.Sprintf("https://%v", source.Repo)
+		return sourceExistsAt(url, source)
+	}
+	return nil
+}
+
+func appendSourceBase(ctx *cli.Context, workDir, source string) string {
+	isLocal, _ := git.IsLocal(workDir, source)
+	// @todo add list of supported hosts here or do this check better
+	if !isLocal && !strings.Contains(source, ".com") && !strings.Contains(source, ".org") && !strings.Contains(source, ".net") {
+		baseURL, _ := config.Get("git", util.GetEnv(ctx).Name, "baseurl")
+		if len(baseURL) == 0 {
+			baseURL, _ = config.Get("git", "baseurl")
+		}
+		if len(baseURL) == 0 {
+			return path.Join("github.com/micro/services", source)
+		}
+		return path.Join(baseURL, source)
+	}
+	return source
+}
 
 func runService(ctx *cli.Context) error {
 	// we need some args to run
@@ -178,6 +290,24 @@ func runService(ctx *cli.Context) error {
 	return nil
 }
 
+func getGitCredentials(repo string) (string, bool) {
+	repo = strings.Split(repo, "/")[0]
+
+	for _, org := range GitOrgs {
+		if !strings.Contains(repo, org) {
+			continue
+		}
+
+		// check the creds for the org
+		creds, err := config.Get("git", "credentials", org)
+		if err == nil && len(creds) > 0 {
+			return creds, true
+		}
+	}
+
+	return "", false
+}
+
 func killService(ctx *cli.Context) error {
 	// we need some args to run
 	if ctx.Args().Len() == 0 {
@@ -210,6 +340,27 @@ func killService(ctx *cli.Context) error {
 	}
 
 	return nil
+}
+
+func grepMain(path string) error {
+	files, err := ioutil.ReadDir(path)
+	if err != nil {
+		return err
+	}
+	for _, f := range files {
+		if !strings.HasSuffix(f.Name(), ".go") {
+			continue
+		}
+		file := filepath.Join(path, f.Name())
+		b, err := ioutil.ReadFile(file)
+		if err != nil {
+			continue
+		}
+		if strings.Contains(string(b), "package main") {
+			return nil
+		}
+	}
+	return fmt.Errorf("Directory does not contain a main package")
 }
 
 func upload(ctx *cli.Context, source *git.Source) (string, error) {
@@ -424,7 +575,10 @@ func getService(ctx *cli.Context) error {
 	return nil
 }
 
-const ()
+const (
+	// logUsage message for logs command
+	logUsage = "Required usage: micro log example"
+)
 
 func getLogs(ctx *cli.Context) error {
 	logger.DefaultLogger.Init(golog.WithFields(map[string]interface{}{"service": "runtime"}))
@@ -435,10 +589,10 @@ func getLogs(ctx *cli.Context) error {
 
 	name := ctx.Args().Get(0)
 
-	fmt.Println(LogUsage)
-	return nil
 	// must specify service name
 	if len(name) == 0 {
+		fmt.Println(logUsage)
+		return nil
 	}
 
 	// get the args
