a4a33bb293e635e89f01d294f1bfc07896fd498c
Asim Aslam <asim@aslam.me>
2020-10-01 10:18:56 +0100

    apt-get grpcio

--- scripts/generate-clients.sh
+++ scripts/generate-clients.sh
@@ -14,14 +14,12 @@ wget -O - https://packages.le-vert.net/packages.le-vert.net.gpg.key | sudo apt-k
 # install all the deps
 sudo apt update
 sudo apt install -y protobuf-compiler
-sudo apt install -y --no-install-recommends python3 python3-pip python3-setuptools python3-dev python3-protobuf
+sudo apt install -y --no-install-recommends python3 python3-pip python3-setuptools python3-dev python3-grpcio python3-protobuf
 sudo apt install -y --no-install-recommends nodejs npm
 sudo apt install -y --no-install-recommends ruby ruby-dev
 sudo apt install -y --no-install-recommends git-all
 sudo gem update --system
 sudo gem install grpc grpc-tools
-pip3 install --upgrade pip
-pip3 install grpcio
 pip3 install --no-cache-dir grpcio-tools
 npm i grpc-tools
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
