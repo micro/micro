185e2374edfba0020ea020bc2b0faa60cc4a3c4d
Asim Aslam <asim@aslam.me>
2020-10-02 11:51:20 +0100

    fix client protoc-gen-client path

--- scripts/generate-clients.sh
+++ scripts/generate-clients.sh
@@ -5,7 +5,7 @@ set -e
 
 GO_PATH=$(go env GOPATH)
 GO=$GO_PATH/bin/go
-PATH=$PATH:$GO_BIN:$(npm bin):/usr/local/bin/ 
+PATH=$PATH:$GO_BIN:$(npm bin):/usr/local/bin/:$HOME/.cargo/bin
 
 ### UBUNTU BIONIC ###
 echo "deb https://packages.le-vert.net/tensorflow/ubuntu bionic main" | sudo tee -a /etc/apt/sources.list
@@ -14,12 +14,13 @@ wget -O - https://packages.le-vert.net/packages.le-vert.net.gpg.key | sudo apt-k
 # install all the deps
 sudo apt update
 sudo apt install -y protobuf-compiler
-sudo apt install -y --no-install-recommends python3 python3-pip python3-setuptools python3-dev python3-grpcio python3-protobuf
+sudo apt install -y --no-install-recommends python3 python3-pip python3-setuptools python3-dev python3-protobuf
 sudo apt install -y --no-install-recommends nodejs npm
 sudo apt install -y --no-install-recommends ruby ruby-dev
 sudo apt install -y --no-install-recommends git-all
 sudo gem update --system
 sudo gem install grpc grpc-tools
+pip3 install grpcio
 pip3 install --no-cache-dir grpcio-tools
 npm i grpc-tools
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
@@ -32,6 +33,6 @@ go get github.com/golang/protobuf/protoc-gen-go@v1.4.2
 # delete the existing sdk directory
 rm -rf client/sdk
 # generate the clients
-protoc-gen-client -srcdir proto/ -dstdir sdk/ -langs go,python,java,ruby,js,rust
+PATH=$PATH:$GO_BIN:$(npm bin):/usr/local/bin/:$HOME/.cargo/bin protoc-gen-client -srcdir proto/ -dstdir sdk/ -langs go,python,java,ruby,js,rust
 # remove node garbage
 rm -rf node_modules/ package-lock.json
