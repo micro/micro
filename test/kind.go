7ed4f732ef6ffa60f98258d9b08e8e7aa82ffbe4
Ben Toogood <bentoogood@gmail.com>
2020-10-07 13:28:03 +0100

    Helm Chart

--- test/kind.go
+++ test/kind.go
@@ -6,6 +6,7 @@ import (
 	"errors"
 	"fmt"
 	"math/rand"
+	"os"
 	"os/exec"
 	"path/filepath"
 	"strings"
@@ -42,6 +43,13 @@ func newK8sServer(t *T, fname string, opts ...Option) Server {
 	portnum := rand.Intn(maxPort-minPort) + minPort
 	configFile := configFile(fname)
 
+	var cmd *exec.Cmd
+	if v := os.Getenv("IN_HELM_TEST"); len(v) > 0 {
+		cmd = exec.Command("kubectl", "port-forward", "--namespace", "micro", "svc/proxy", fmt.Sprintf("%d:443", portnum))
+	} else {
+		cmd = exec.Command("kubectl", "port-forward", "--namespace", "default", "svc/micro-proxy", fmt.Sprintf("%d:443", portnum))
+	}
+
 	s := &testK8sServer{ServerBase{
 		dir:       filepath.Dir(configFile),
 		config:    configFile,
@@ -49,7 +57,7 @@ func newK8sServer(t *T, fname string, opts ...Option) Server {
 		env:       options.Namespace,
 		proxyPort: portnum,
 		opts:      options,
-		cmd:       exec.Command("kubectl", "port-forward", "--namespace", "default", "svc/micro-proxy", fmt.Sprintf("%d:443", portnum)),
+		cmd:       cmd,
 	}}
 	s.namespace = s.env
 
@@ -81,7 +89,6 @@ func (s *testK8sServer) Run() error {
 			!strings.Contains(string(outp), "config") ||
 			!strings.Contains(string(outp), "proxy") ||
 			!strings.Contains(string(outp), "auth") ||
-			!strings.Contains(string(outp), "updater") ||
 			!strings.Contains(string(outp), "store") {
 			return outp, errors.New("Not ready")
 		}
