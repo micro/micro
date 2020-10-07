76ae43f69b85212fe1cec4a5d032c6ebd87c0c79
Ben Toogood <bentoogood@gmail.com>
2020-10-07 17:28:35 +0100

    server: pass jwt keys to core services

--- server/server.go
+++ server/server.go
@@ -10,6 +10,7 @@ import (
 	"github.com/micro/micro/v3/client/cli/util"
 	"github.com/micro/micro/v3/cmd"
 	"github.com/micro/micro/v3/service"
+	"github.com/micro/micro/v3/service/auth"
 	"github.com/micro/micro/v3/service/client"
 	"github.com/micro/micro/v3/service/context"
 	log "github.com/micro/micro/v3/service/logger"
@@ -199,8 +200,8 @@ func Run(context *cli.Context) error {
 			runtime.WithVolume("store-pvc", "/store"),
 			runtime.CreateImage("localhost:5000/micro"),
 			runtime.CreateNamespace("micro"),
-			// runtime.WithSecret("MICRO_AUTH_PUBLIC_KEY", auth.DefaultAuth.Options().PublicKey),
-			// runtime.WithSecret("MICRO_AUTH_PRIVATE_KEY", auth.DefaultAuth.Options().PrivateKey),
+			runtime.WithSecret("MICRO_AUTH_PUBLIC_KEY", auth.DefaultAuth.Options().PublicKey),
+			runtime.WithSecret("MICRO_AUTH_PRIVATE_KEY", auth.DefaultAuth.Options().PrivateKey),
 		}
 
 		// NOTE: we use Version right now to check for the latest release
