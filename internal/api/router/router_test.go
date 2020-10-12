b4e0f6f7532bb75de1e7a7e488acaac2073850ee
Ben Toogood <bentoogood@gmail.com>
2020-10-12 13:28:33 +0100

    Add missing licenses

--- internal/api/router/router_test.go
+++ internal/api/router/router_test.go
@@ -1,3 +1,18 @@
+// Copyright 2020 Asim Aslam
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// Original source: github.com/micro/go-micro/v3/api/router/router_test.go
 package router_test
 
 import (
@@ -9,12 +24,6 @@ import (
 	"testing"
 	"time"
 
-	"github.com/micro/micro/v3/service/api"
-	"github.com/micro/micro/v3/internal/api/handler"
-	"github.com/micro/micro/v3/internal/api/handler/rpc"
-	"github.com/micro/micro/v3/internal/api/router"
-	rregistry "github.com/micro/micro/v3/internal/api/router/registry"
-	rstatic "github.com/micro/micro/v3/internal/api/router/static"
 	"github.com/micro/go-micro/v3/client"
 	gcli "github.com/micro/go-micro/v3/client/grpc"
 	rmemory "github.com/micro/go-micro/v3/registry/memory"
@@ -23,6 +32,12 @@ import (
 	"github.com/micro/go-micro/v3/server"
 	gsrv "github.com/micro/go-micro/v3/server/grpc"
 	pb "github.com/micro/go-micro/v3/server/grpc/proto"
+	"github.com/micro/micro/v3/internal/api/handler"
+	"github.com/micro/micro/v3/internal/api/handler/rpc"
+	"github.com/micro/micro/v3/internal/api/router"
+	rregistry "github.com/micro/micro/v3/internal/api/router/registry"
+	rstatic "github.com/micro/micro/v3/internal/api/router/static"
+	"github.com/micro/micro/v3/service/api"
 )
 
 // server is used to implement helloworld.GreeterServer.
