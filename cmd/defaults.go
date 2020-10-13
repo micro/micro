f8b08990b96ac47626cae2d9d602f94404179fe4
Ben Toogood <bentoogood@gmail.com>
2020-10-13 16:56:50 +0100

    Remove dependancy on go-micro/v3/model package

--- cmd/defaults.go
+++ cmd/defaults.go
@@ -7,6 +7,8 @@ import (
 	brokerSrv "github.com/micro/micro/v3/service/broker/client"
 	"github.com/micro/micro/v3/service/client"
 	grpcCli "github.com/micro/micro/v3/service/client/grpc"
+	"github.com/micro/micro/v3/service/model"
+	"github.com/micro/micro/v3/service/model/mud"
 	"github.com/micro/micro/v3/service/network"
 	mucpNet "github.com/micro/micro/v3/service/network/mucp"
 	"github.com/micro/micro/v3/service/server"
@@ -19,6 +21,7 @@ func setupDefaults() {
 	client.DefaultClient = grpcCli.NewClient()
 	server.DefaultServer = grpcSvr.NewServer()
 	network.DefaultNetwork = mucpNet.NewNetwork()
+	model.DefaultModel = mud.NewModel()
 
 	// setup rpc implementations after the client is configured
 	auth.DefaultAuth = authSrv.NewAuth()
