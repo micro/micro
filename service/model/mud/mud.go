d46e846b511d5d593120f92f8ce6e457b777fdd5
Ben Toogood <bentoogood@gmail.com>
2020-10-14 15:15:52 +0100

    Remove dependancy on go-micro/v3/sync package

--- service/model/mud/mud.go
+++ service/model/mud/mud.go
@@ -18,7 +18,7 @@ package mud
 import (
 	"github.com/micro/micro/v3/service/store"
 	"github.com/micro/micro/v3/service/store/memory"
-	memsync "github.com/micro/go-micro/v3/sync/memory"
+	memsync "github.com/micro/micro/v3/internal/sync/memory"
 	"github.com/micro/micro/v3/internal/codec/json"
 	"github.com/micro/micro/v3/service/model"
 )
