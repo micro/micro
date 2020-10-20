c362933827e7066d025f0cb1f4ed73c4d275fba9
Ben Toogood <bentoogood@gmail.com>
2020-10-20 09:33:38 +0100

    Simplify tests

--- test/vendortest/go.mod
+++ test/vendortest/go.mod
@@ -4,9 +4,6 @@ go 1.13
 
 replace github.com/micro/micro/v3 => ../..
 
-replace github.com/micro/test => ../../../test
-
 require (
 	github.com/micro/micro/v3 v3.0.0-beta.6
-	github.com/micro/test v0.0.0-20201019141839-d620c1e4fc1c
 )
--- test/vendortest/main.go
+++ test/vendortest/main.go
@@ -1,16 +1,11 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/micro/micro/v3/service"
 	"github.com/micro/micro/v3/service/logger"
-	"github.com/micro/test/vendortest"
 )
 
 func main() {
-	fmt.Println("foo:", vendortest.Foo())
-
 	srv := service.New(
 		service.Name("vendortest"),
 	)
