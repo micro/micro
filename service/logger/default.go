f9d6877c33618c642718ad273237210e96c0b239
Dominic Wong <domwongemail@googlemail.com>
2020-11-24 14:55:53 +0000

    Fix logger redirection (#1609)

--- service/logger/default.go
+++ service/logger/default.go
@@ -128,7 +128,7 @@ func (l *defaultLogger) Log(level Level, v ...interface{}) {
 	}
 
 	t := rec.Timestamp.Format("2006-01-02 15:04:05")
-	fmt.Printf("%s %s %v\n", t, metadata, rec.Message)
+	fmt.Fprintf(l.opts.Out, "%s %s %v\n", t, metadata, rec.Message)
 }
 
 func (l *defaultLogger) Logf(level Level, format string, v ...interface{}) {
@@ -167,7 +167,7 @@ func (l *defaultLogger) Logf(level Level, format string, v ...interface{}) {
 	}
 
 	t := rec.Timestamp.Format("2006-01-02 15:04:05")
-	fmt.Printf("%s %s %v\n", t, metadata, rec.Message)
+	fmt.Fprintf(l.opts.Out, "%s %s %v\n", t, metadata, rec.Message)
 }
 
 func (l *defaultLogger) Options() Options {
