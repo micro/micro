ea059fefb160e00b801ca195c456047dda5b06d4
Asim Aslam <asim@aslam.me>
2020-12-14 10:26:26 +0000

    strip go-micro from all go mods

--- plugin/etcd/etcd.go
+++ plugin/etcd/etcd.go
@@ -29,9 +29,9 @@ import (
 	"sync"
 	"time"
 
-	"github.com/coreos/etcd/clientv3"
-	"github.com/coreos/etcd/etcdserver/api/v3rpc/rpctypes"
-	"github.com/coreos/etcd/mvcc/mvccpb"
+	"go.etcd.io/etcd/clientv3"
+	"go.etcd.io/etcd/etcdserver/api/v3rpc/rpctypes"
+	"go.etcd.io/etcd/mvcc/mvccpb"
 	"github.com/micro/micro/v3/service/logger"
 	"github.com/micro/micro/v3/service/registry"
 	hash "github.com/mitchellh/hashstructure"
