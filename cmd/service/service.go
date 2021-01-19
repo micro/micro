e3b697d791d849b5f61fec024a9c32c875e08f48
Asim Aslam <asim@aslam.me>
2021-01-19 10:16:06 +0000

    Server (#1674)
    * move server to cmd/server
    
    * move handler package

--- cmd/service/service.go
+++ cmd/service/service.go
@@ -32,8 +32,8 @@ import (
 	store "github.com/micro/micro/v3/service/store/server"
 
 	// misc commands
-	"github.com/micro/micro/v3/service/handler/exec"
-	"github.com/micro/micro/v3/service/handler/file"
+	"github.com/micro/micro/v3/cmd/service/handler/exec"
+	"github.com/micro/micro/v3/cmd/service/handler/file"
 )
 
 // Run starts a micro service sidecar to encapsulate any app
