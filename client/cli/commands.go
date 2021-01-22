e869b13b05f875a4e5bcc7eb7cadb08e5ce799fd
Daniel Joudat <danieljoudat@gmail.com>
2021-01-22 20:37:06 +0300

    added some additional functionality to micro stats

--- client/cli/commands.go
+++ client/cli/commands.go
@@ -1,6 +1,7 @@
 package cli
 
 import (
+	"bytes"
 	"context"
 	"errors"
 	"fmt"
@@ -14,6 +15,7 @@ import (
 
 	"github.com/micro/micro/v3/client/cli/namespace"
 	"github.com/micro/micro/v3/client/cli/util"
+	clic "github.com/micro/micro/v3/internal/command"
 	proto "github.com/micro/micro/v3/proto/debug"
 	"github.com/micro/micro/v3/service/client"
 	"github.com/micro/micro/v3/service/registry"
@@ -44,7 +46,64 @@ func help(c *cli.Context, args []string) ([]byte, error) {
 	return nil, nil
 }
 
+// QueryStats returns stats of specified service(s)
 func QueryStats(c *cli.Context, args []string) ([]byte, error) {
+	if c.String("all") == "builtin" {
+
+		sl, err := clic.ListServices(c)
+		if err != nil {
+			return nil, err
+		}
+
+		servList := strings.Split(string(sl), "\n")
+		var builtinList []string
+
+		for _, s := range servList {
+
+			if util.IsBuiltInService(s) {
+				builtinList = append(builtinList, s)
+			}
+		}
+
+		c.Set("all", "")
+
+		if len(builtinList) == 0 {
+			return nil, errors.New("no builtin service(s) found")
+		}
+
+		return QueryStats(c, builtinList)
+	}
+
+	if c.String("all") == "custom" {
+		sl, err := clic.ListServices(c)
+		if err != nil {
+			return nil, err
+		}
+
+		servList := strings.Split(string(sl), "\n")
+		var customList []string
+
+		for _, s := range servList {
+
+			// temporary excluding server
+			if s == "server" {
+				continue
+			}
+
+			if !util.IsBuiltInService(s) {
+				customList = append(customList, s)
+			}
+		}
+
+		c.Set("all", "")
+
+		if len(customList) == 0 {
+			return nil, errors.New("no custom service(s) found")
+		}
+
+		return QueryStats(c, customList)
+	}
+
 	if len(args) == 0 {
 		return nil, cli.ShowSubcommandHelp(c)
 	}
@@ -58,51 +117,58 @@ func QueryStats(c *cli.Context, args []string) ([]byte, error) {
 		return nil, err
 	}
 
-	service, err := registry.DefaultRegistry.GetService(args[0], registry.GetDomain(ns))
-	if err != nil {
-		return nil, err
-	}
+	titlesList := []string{"NODE", "ADDRESS:PORT", "STARTED", "UPTIME", "MEMORY", "THREADS", "GC"}
+	titles := strings.Join(titlesList, "\t")
 
-	if len(service) == 0 {
-		return nil, errors.New("Service not found")
-	}
+	var buf bytes.Buffer
+	w := tabwriter.NewWriter(&buf, 0, 1, 4, ' ', tabwriter.TabIndent)
 
-	req := client.NewRequest(service[0].Name, "Debug.Stats", &proto.StatsRequest{})
+	for _, a := range args {
 
-	var output []string
+		service, err := registry.DefaultRegistry.GetService(a, registry.GetDomain(ns))
+		if err != nil {
+			return nil, err
+		}
+		if len(service) == 0 {
+			return nil, errors.New("Service not found")
+		}
 
-	// print things
-	output = append(output, "service  "+service[0].Name)
+		req := client.NewRequest(service[0].Name, "Debug.Stats", &proto.StatsRequest{})
 
-	for _, serv := range service {
-		// print things
-		output = append(output, "\nversion "+serv.Version)
-		output = append(output, "\nnode\t\taddress:port\t\tstarted\tuptime\tmemory\tthreads\tgc")
+		fmt.Fprintln(w, "SERVICE\t"+service[0].Name+"\n")
 
-		// query health for every node
-		for _, node := range serv.Nodes {
-			address := node.Address
-			rsp := &proto.StatsResponse{}
+		for _, serv := range service {
 
-			var err error
+			fmt.Fprintln(w, "VERSION\t"+serv.Version+"\n")
+			fmt.Fprintln(w, titles)
 
-			// call using client
-			err = client.DefaultClient.Call(context.Background(), req, rsp, client.WithAddress(address))
+			// query health for every node
+			for _, node := range serv.Nodes {
+				address := node.Address
+				rsp := &proto.StatsResponse{}
 
-			var started, uptime, memory, gc string
-			if err == nil {
-				started = time.Unix(int64(rsp.Started), 0).Format("Jan 2 15:04:05")
-				uptime = fmt.Sprintf("%v", time.Duration(rsp.Uptime)*time.Second)
-				memory = fmt.Sprintf("%.2fmb", float64(rsp.Memory)/(1024.0*1024.0))
-				gc = fmt.Sprintf("%v", time.Duration(rsp.Gc))
+				var err error
+
+				// call using client
+				err = client.DefaultClient.Call(context.Background(), req, rsp, client.WithAddress(address))
+
+				var started, uptime, memory, gc string
+				if err == nil {
+					started = time.Unix(int64(rsp.Started), 0).Format("Jan 2 15:04:05")
+					uptime = fmt.Sprintf("%v", time.Duration(rsp.Uptime)*time.Second)
+					memory = fmt.Sprintf("%.2fmb", float64(rsp.Memory)/(1024.0*1024.0))
+					gc = fmt.Sprintf("%v", time.Duration(rsp.Gc))
+				}
+
+				line := fmt.Sprintf("%s\t%s\t%s\t%s\t%s\t%d\t%s\n",
+					node.Id, node.Address, started, uptime, memory, rsp.Threads, gc)
+
+				fmt.Fprintln(w, line)
 			}
-
-			line := fmt.Sprintf("%s\t\t%s\t\t%s\t%s\t%s\t%d\t%s",
-				node.Id, node.Address, started, uptime, memory, rsp.Threads, gc)
-
-			output = append(output, line)
 		}
 	}
 
-	return []byte(strings.Join(output, "\n")), nil
+	w.Flush()
+
+	return buf.Bytes(), nil
 }
