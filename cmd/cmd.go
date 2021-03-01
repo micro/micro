e0e966d08c06607c7ec576f273b6dd0760c3508c
Asim Aslam <asim@aslam.me>
2021-03-01 20:40:23 +0000

    Strip from service wrapper

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -395,7 +395,6 @@ func (c *command) Before(ctx *cli.Context) error {
 	client.DefaultClient = wrapper.AuthClient(client.DefaultClient)
 	client.DefaultClient = wrapper.CacheClient(client.DefaultClient)
 	client.DefaultClient = wrapper.TraceCall(client.DefaultClient)
-	client.DefaultClient = wrapper.FromService(client.DefaultClient)
 	client.DefaultClient = wrapper.LogClient(client.DefaultClient)
 
 	// wrap the server
