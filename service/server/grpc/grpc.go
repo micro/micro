77c922c43825b9df0206ed4d0e52a580114bf20e
Asim Aslam <asim@aslam.me>
2021-05-14 18:30:32 +0100

    move util/grpc

--- service/server/grpc/grpc.go
+++ service/server/grpc/grpc.go
@@ -43,7 +43,6 @@ import (
 	"github.com/micro/micro/v3/service/server"
 	"github.com/micro/micro/v3/util/addr"
 	"github.com/micro/micro/v3/util/backoff"
-	mgrpc "github.com/micro/micro/v3/util/grpc"
 	mnet "github.com/micro/micro/v3/util/net"
 	"golang.org/x/net/http2"
 	"golang.org/x/net/netutil"
@@ -248,7 +247,7 @@ func (g *grpcServer) handler(srv interface{}, stream grpc.ServerStream) (err err
 		return status.Errorf(codes.Internal, "method does not exist in context")
 	}
 
-	serviceName, methodName, err := mgrpc.ServiceMethod(fullMethod)
+	serviceName, methodName, err := ServiceMethod(fullMethod)
 	if err != nil {
 		return status.New(codes.InvalidArgument, err.Error()).Err()
 	}
@@ -315,7 +314,7 @@ func (g *grpcServer) handler(srv interface{}, stream grpc.ServerStream) (err err
 
 		// create a client.Request
 		request := &rpcRequest{
-			service:     mgrpc.ServiceFromMethod(fullMethod),
+			service:     ServiceFromMethod(fullMethod),
 			contentType: ct,
 			method:      fmt.Sprintf("%s.%s", serviceName, methodName),
 			codec:       codec,
