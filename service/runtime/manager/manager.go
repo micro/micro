9c4d796ee931eb9f7cab1d214da05f41c706dc02
Asim Aslam <asim@aslam.me>
2021-05-19 09:28:56 +0100

    add make api command to makefile

--- service/runtime/manager/manager.go
+++ service/runtime/manager/manager.go
@@ -15,9 +15,9 @@ import (
 	"github.com/micro/micro/v3/service/client"
 	"github.com/micro/micro/v3/service/logger"
 	"github.com/micro/micro/v3/service/runtime"
+	kclient "github.com/micro/micro/v3/service/runtime/kubernetes/client"
 	"github.com/micro/micro/v3/service/runtime/source/git"
 	"github.com/micro/micro/v3/service/store"
-	kclient "github.com/micro/micro/v3/service/runtime/kubernetes/client"
 	"github.com/micro/micro/v3/util/namespace"
 )
 
