e0cf996d185013c977fd43d1a434f262d55a46d8
Dominic Wong <dom@m3o.com>
2021-06-10 17:06:18 +0100

    fixes

--- util/wrapper/opentracing.go
+++ util/wrapper/opentracing.go
@@ -7,17 +7,17 @@ import (
 	"reflect"
 	"strings"
 
+	"github.com/micro/micro/v3/service/client"
+	mmd "github.com/micro/micro/v3/service/context/metadata"
 	"github.com/micro/micro/v3/service/logger"
 	"github.com/micro/micro/v3/service/server"
 	"github.com/micro/micro/v3/util/opentelemetry"
 	"github.com/opentracing/opentracing-go"
 	"github.com/opentracing/opentracing-go/ext"
-	"google.golang.org/grpc/metadata"
 )
 
 // OpenTraceHandler wraps a server handler to perform opentracing:
 func OpenTraceHandler() server.HandlerWrapper {
-
 	// return a handler wrapper
 	return func(h server.HandlerFunc) server.HandlerFunc {
 
@@ -33,17 +33,17 @@ func OpenTraceHandler() server.HandlerWrapper {
 			}
 			logger.Infof("Tracing call using (%s)", reflect.TypeOf(opentelemetry.DefaultOpenTracer))
 
-			// TODO hardcodes grpc as the impl
-			md, ok := metadata.FromIncomingContext(ctx)
+			md, ok := mmd.FromContext(ctx)
 			if !ok {
 
 			}
-			spanCtx, err := opentelemetry.DefaultOpenTracer.Extract(opentracing.HTTPHeaders, opentelemetry.MetadataReaderWriter{md})
+			spanCtx, err := opentelemetry.DefaultOpenTracer.Extract(opentracing.HTTPHeaders, opentelemetry.MicroMetadataReaderWriter{md})
 			if err != nil && err != opentracing.ErrSpanContextNotFound {
 				logger.Errorf("Error reconstructing span %s", err)
 			}
 			var opts []opentracing.StartSpanOption
 			if spanCtx != nil {
+				logger.Infof("Reconstructing the span")
 				opts = append(opts, ext.RPCServerOption(spanCtx))
 			}
 			// Start a span from context:
@@ -113,4 +113,36 @@ type statusRecorder struct {
 func (sr *statusRecorder) WriteHeader(statusCode int) {
 	sr.statusCode = statusCode
 	sr.ResponseWriter.WriteHeader(statusCode)
+}
+
+type opentraceWrapper struct {
+	client.Client
+}
+
+func (o *opentraceWrapper) Call(ctx context.Context, req client.Request, rsp interface{}, opts ...client.CallOption) error {
+	ctx = o.wrapContext(ctx, opts...)
+	return o.Client.Call(ctx, req, rsp, opts...)
+}
+
+func (o *opentraceWrapper) Stream(ctx context.Context, req client.Request, opts ...client.CallOption) (client.Stream, error) {
+	ctx = o.wrapContext(ctx, opts...)
+	return o.Client.Stream(ctx, req, opts...)
+}
+
+func (o *opentraceWrapper) wrapContext(ctx context.Context, opts ...client.CallOption) context.Context {
+	// set the open tracing headers
+	md := mmd.Metadata{}
+	span := opentracing.SpanFromContext(ctx)
+	if span != nil {
+		opentelemetry.DefaultOpenTracer.Inject(span.Context(), opentracing.TextMap, opentelemetry.MicroMetadataReaderWriter{md})
+	}
+
+	ctx = mmd.MergeContext(ctx, md, true)
+
+	return ctx
+}
+
+// OpentraceClient wraps requests with the open tracing headers
+func OpentraceClient(c client.Client) client.Client {
+	return &opentraceWrapper{c}
 }
