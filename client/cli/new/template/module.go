aeb393e7ff595c3c10879e440cf78f85a6be7bdd
Amin Jamali <ajamali@pivotal.io>
2022-01-21 17:56:03 +0000

    Ensure the binary can be build locally
    - grpc {1.26.0,1.27.1} because grpc.ClientConnInterface was undefined

--- client/cli/new/template/module.go
+++ client/cli/new/template/module.go
@@ -6,11 +6,13 @@ var (
 go 1.15
 
 require (
-	github.com/micro/micro/v3 v3.0.0
+	github.com/golang/protobuf latest
+	github.com/micro/micro/v3 latest
+	google.golang.org/protobuf latest
 )
 
 // This can be removed once etcd becomes go gettable, version 3.4 and 3.5 is not,
 // see https://github.com/etcd-io/etcd/issues/11154 and https://github.com/etcd-io/etcd/issues/11931.
-replace google.golang.org/grpc => google.golang.org/grpc v1.26.0
+replace google.golang.org/grpc => google.golang.org/grpc v1.27.1
 `
 )
