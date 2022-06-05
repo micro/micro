68139ec009bd8c3e24c79509b95ec7a0c4e8e544
AV <62926341+a0v0@users.noreply.github.com>
2022-06-06 01:34:10 +0530

    Update git.go

--- service/runtime/source/git/git.go
+++ service/runtime/source/git/git.go
@@ -221,30 +221,35 @@ func (g *binaryGitter) checkoutGitLabPublic(repo, branchOrCommit string) error {
 }
 
 func (g *binaryGitter) checkoutGitLabPrivate(repo, branchOrCommit string) error {
-	git, err := gitlab.NewClient(g.secrets[credentialsKey])
-	if err != nil {
-		return err
+// edited
+	// git, err := gitlab.NewClient(g.secrets[credentialsKey])
+	// if err != nil {
+	// 	return err
+	// }
+	// owned := true
+	// projects, _, err := git.Projects.ListProjects(&gitlab.ListProjectsOptions{
+	// 	Owned: &owned,
+	// })
+	// if err != nil {
+	// 	return err
+	// }
+	// projectID := ""
+	// for _, project := range projects {
+	// 	if strings.Contains(repo, project.Name) {
+	// 		projectID = fmt.Sprintf("%v", project.ID)
+	// 	}
+	// }
+	// if len(projectID) == 0 {
+	// 	return fmt.Errorf("Project id not found for repo %v", repo)
+	// }
+	repoFolder := strings.ReplaceAll(strings.ReplaceAll(repo, "/", "-"), "https:--", "")
+	g.folder = filepath.Join(os.TempDir(),
+		repoFolder+"-"+shortid.MustGenerate())
+	tarName := strings.ReplaceAll(strings.ReplaceAll(repo, "gitlab.com/", ""), "/", "-")
+	url := fmt.Sprintf("%v/-/archive/%v/%v.zip?private_token=%v", repo, branchOrCommit, tarName, g.secrets[credentialsKey])
+	if !strings.HasPrefix(url, "https://") {
+		url = "https://" + url
 	}
-	owned := true
-	projects, _, err := git.Projects.ListProjects(&gitlab.ListProjectsOptions{
-		Owned: &owned,
-	})
-	if err != nil {
-		return err
-	}
-	projectID := ""
-	for _, project := range projects {
-		if strings.Contains(repo, project.Name) {
-			projectID = fmt.Sprintf("%v", project.ID)
-		}
-	}
-	if len(projectID) == 0 {
-		return fmt.Errorf("Project id not found for repo %v", repo)
-	}
-	// Example URL:
-	// https://gitlab.com/api/v3/projects/0000000/repository/archive?private_token=XXXXXXXXXXXXXXXXXXXX
-	url := fmt.Sprintf("https://gitlab.com/api/v4/projects/%v/repository/archive?private_token=%v", projectID, g.secrets[credentialsKey])
-
 	req, _ := http.NewRequest("GET", url, nil)
 	resp, err := g.client.Do(req)
 	if err != nil {
@@ -253,7 +258,9 @@ func (g *binaryGitter) checkoutGitLabPrivate(repo, branchOrCommit string) error 
 
 	defer resp.Body.Close()
 
-	src := g.folder + ".tar.gz"
+	src := g.folder + ".zip"
+// edited
+	
 	// Create the file
 	out, err := os.Create(src)
 	if err != nil {
