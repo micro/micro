7c57580ab5d243e3f153e29e0025ad850a20bd8e
AV <62926341+a0v0@users.noreply.github.com>
2022-06-08 14:48:37 +0530

    Fix: Gitlab private repository deployment (#1943)
    * Update git.go
    
    * Update git.go
    
    fixed personal access token issue with gitlab private repos

--- service/runtime/source/git/git.go
+++ service/runtime/source/git/git.go
@@ -30,7 +30,6 @@ import (
 	"strings"
 
 	"github.com/teris-io/shortid"
-	"github.com/xanzy/go-gitlab"
 )
 
 const credentialsKey = "GIT_CREDENTIALS"
@@ -221,30 +220,17 @@ func (g *binaryGitter) checkoutGitLabPublic(repo, branchOrCommit string) error {
 }
 
 func (g *binaryGitter) checkoutGitLabPrivate(repo, branchOrCommit string) error {
-	git, err := gitlab.NewClient(g.secrets[credentialsKey])
-	if err != nil {
-		return err
-	}
-	owned := true
-	projects, _, err := git.Projects.ListProjects(&gitlab.ListProjectsOptions{
-		Owned: &owned,
-	})
-	if err != nil {
-		return err
-	}
-	projectID := ""
-	for _, project := range projects {
-		if strings.Contains(repo, project.Name) {
-			projectID = fmt.Sprintf("%v", project.ID)
-		}
-	}
-	if len(projectID) == 0 {
-		return fmt.Errorf("Project id not found for repo %v", repo)
-	}
-	// Example URL:
-	// https://gitlab.com/api/v3/projects/0000000/repository/archive?private_token=XXXXXXXXXXXXXXXXXXXX
-	url := fmt.Sprintf("https://gitlab.com/api/v4/projects/%v/repository/archive?private_token=%v", projectID, g.secrets[credentialsKey])
 
+	repoFolder := strings.ReplaceAll(strings.ReplaceAll(repo, "/", "-"), "https:--", "")
+	g.folder = filepath.Join(os.TempDir(),
+		repoFolder+"-"+shortid.MustGenerate())
+	tarName := strings.ReplaceAll(strings.ReplaceAll(repo, "gitlab.com/", ""), "/", "-")
+
+	url := fmt.Sprintf("%v/-/archive/%v/%v.tar.gz?private_token=%v", repo, branchOrCommit, tarName, g.secrets[credentialsKey])
+
+	if !strings.HasPrefix(url, "https://") {
+		url = "https://" + url
+	}
 	req, _ := http.NewRequest("GET", url, nil)
 	resp, err := g.client.Do(req)
 	if err != nil {
