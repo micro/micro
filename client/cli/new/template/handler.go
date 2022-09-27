d29ce3e72aef8c66d81d999c78f479716d616242
asim <asim@aslam.me>
2022-09-27 14:23:26 +0100

    new blog post

--- client/cli/new/template/handler.go
+++ client/cli/new/template/handler.go
@@ -8,7 +8,7 @@ import (
 
 	log "github.com/micro/micro/v3/service/logger"
 
-	{{dehyphen .Alias}} "{{.Dir}}/proto"
+	pb "{{.Dir}}/proto"
 )
 
 type {{title .Alias}} struct{}
@@ -19,19 +19,19 @@ func New() *{{title .Alias}} {
 }
 
 // Call is a single request handler called via client.Call or the generated client code
-func (e *{{title .Alias}}) Call(ctx context.Context, req *{{dehyphen .Alias}}.Request, rsp *{{dehyphen .Alias}}.Response) error {
+func (e *{{title .Alias}}) Call(ctx context.Context, req *pb.Request, rsp *pb.Response) error {
 	log.Info("Received {{title .Alias}}.Call request")
 	rsp.Msg = "Hello " + req.Name
 	return nil
 }
 
 // Stream is a server side stream handler called via client.Stream or the generated client code
-func (e *{{title .Alias}}) Stream(ctx context.Context, req *{{dehyphen .Alias}}.StreamingRequest, stream {{dehyphen .Alias}}.{{title .Alias}}_StreamStream) error {
+func (e *{{title .Alias}}) Stream(ctx context.Context, req *pb.StreamingRequest, stream pb.{{title .Alias}}_StreamStream) error {
 	log.Infof("Received {{title .Alias}}.Stream request with count: %d", req.Count)
 
 	for i := 0; i < int(req.Count); i++ {
 		log.Infof("Responding: %d", i)
-		if err := stream.Send(&{{dehyphen .Alias}}.StreamingResponse{
+		if err := stream.Send(&pb.StreamingResponse{
 			Count: int64(i),
 		}); err != nil {
 			return err
@@ -42,14 +42,14 @@ func (e *{{title .Alias}}) Stream(ctx context.Context, req *{{dehyphen .Alias}}.
 }
 
 // PingPong is a bidirectional stream handler called via client.Stream or the generated client code
-func (e *{{title .Alias}}) PingPong(ctx context.Context, stream {{dehyphen .Alias}}.{{title .Alias}}_PingPongStream) error {
+func (e *{{title .Alias}}) PingPong(ctx context.Context, stream pb.{{title .Alias}}_PingPongStream) error {
 	for {
 		req, err := stream.Recv()
 		if err != nil {
 			return err
 		}
 		log.Infof("Got ping %v", req.Stroke)
-		if err := stream.Send(&{{dehyphen .Alias}}.Pong{Stroke: req.Stroke}); err != nil {
+		if err := stream.Send(&pb.Pong{Stroke: req.Stroke}); err != nil {
 			return err
 		}
 	}
@@ -62,17 +62,17 @@ import (
 	"context"
 	log "github.com/micro/micro/v3/service/logger"
 
-	{{dehyphen .Alias}} "{{.Dir}}/proto"
+	pb "{{.Dir}}/proto"
 )
 
 type {{title .Alias}} struct{}
 
-func (e *{{title .Alias}}) Handle(ctx context.Context, msg *{{dehyphen .Alias}}.Message) error {
+func (e *{{title .Alias}}) Handle(ctx context.Context, msg *pb.Message) error {
 	log.Info("Handler Received message: ", msg.Say)
 	return nil
 }
 
-func Handler(ctx context.Context, msg *{{dehyphen .Alias}}.Message) error {
+func Handler(ctx context.Context, msg *pb.Message) error {
 	log.Info("Function Received message: ", msg.Say)
 	return nil
 }
