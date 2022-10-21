68512e5991816bfda0bfd71ee07f189debc1e2f4
asim <asim@aslam.me>
2022-10-21 10:50:16 +0100

    checkpoint

--- client/web/html/assets/mu.js
+++ client/web/html/assets/mu.js
@@ -1,60 +1,379 @@
 var namespace = "micro";
-var url = "https://api.m3o.com";
+var url = "http://localhost:8080";
 var cookie = "micro-token";
+var services = {};
+
+Object.defineProperty(String.prototype, 'capitalize', {
+    value: function() {
+        return this.charAt(0).toUpperCase() + this.slice(1);
+    },
+    enumerable: false
+});
 
 function getCookie(name) {
-  const value = `; ${document.cookie}`;
-  const parts = value.split(`; ${name}=`);
-  if (parts.length === 2) return parts.pop().split(';').shift();
+    const value = `; ${document.cookie}`;
+    const parts = value.split(`; ${name}=`);
+    if (parts.length === 2) return parts.pop().split(';').shift();
 }
 
 function setCookie(name, value, expiry) {
-  const d = new Date();
-  d.setTime(d.getTime() + (expiry*1000));
-  let expires = "expires="+ d.toUTCString();
-  document.cookie = name + "=" + value + ";" + expires + ";path=/";
+    const d = new Date();
+    d.setTime(d.getTime() + (expiry * 1000));
+    let expires = "expires=" + d.toUTCString();
+    document.cookie = name + "=" + value + ";" + expires + ";path=/";
 }
 
-async function call(url = '', data = {}) {
-  var token = getCookie(cookie);
+async function call(service = '', endpoint = '', data = {}) {
+    var token = getCookie(cookie);
 
-  // Default options are marked with *
-  const response = await fetch(url, {
-    method: 'POST', // *GET, POST, PUT, DELETE, etc.
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': 'Bearer ' + token,
-      'Micro-Namespace': namespace,
-    },
-    body: JSON.stringify(data) // body data type must match "Content-Type" header
-  });
-  return response.json(); // parses JSON response into native JavaScript objects
+    // Default options are marked with *
+    const response = await fetch(`${url}/${service}/${endpoint}`, {
+        method: 'POST', // *GET, POST, PUT, DELETE, etc.
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + token,
+            'Micro-Namespace': namespace,
+        },
+        body: JSON.stringify(data) // body data type must match "Content-Type" header
+    });
+    return response.json(); // parses JSON response into native JavaScript objects
 }
 
 async function login(username, password) {
-	return call(url + '/auth/Token', {
-		"id": username,
-		"secret": password,
-		"token_expiry": 30 * 86400,
-	}).then(function(rsp) {
-		setCookie(cookie, rsp.token.access_token, rsp.token.expiry);
-	});
+    return call('auth', 'Token', {
+        "id": username,
+        "secret": password,
+        "token_expiry": 30 * 86400,
+    }).then(function(rsp) {
+        setCookie(cookie, rsp.token.access_token, rsp.token.expiry);
+        window.location.href = "/";
+    });
 }
 
 async function logout() {
-	setCookie(cookie, "", 0);
+    setCookie(cookie, "", 0);
+}
+
+async function listServices() {
+    return call('registry', 'ListServices')
+        .then(function(rsp) {
+            return rsp.services;
+        });
+}
+
+async function getService(name) {
+    return call('registry', 'GetService', {
+        "service": name
+    });
 }
 
 async function setURL(u) {
-	url = u;	
+    url = u;
+}
+
+function renderLogin() {
+    var div = document.getElementById("login");
+
+    var h3 = document.createElement("h3");
+    h3.innerText = "Login";
+
+    var form = document.createElement("form")
+    form.onsubmit = submitLogin;
+
+    var label1 = document.createElement("label");
+    label1.innerText = "Username";
+    var label2 = document.createElement("label");
+    label2.innerText = "Password";
+
+    var username = document.createElement("input");
+    username.id = "username";
+    username.type = "username";
+    username.name = "username";
+    username.required = true;
+
+    var password = document.createElement("input");
+    password.id = "password";
+    password.type = "password";
+    password.name = "password";
+    password.required = true;
+
+    var submit = document.createElement("button")
+    submit.setAttribute("class", "btn btn-default");
+    submit.innerText = "Submit";
+
+    div.appendChild(h3);
+    form.appendChild(label1);
+    form.appendChild(username);
+    form.appendChild(label2);
+    form.appendChild(password);
+    form.appendChild(submit);
+    div.appendChild(form);
+}
+
+function renderServices(fn) {
+    var search = function() {
+        var refs = $('a[data-filter]');
+        $('.search').on('keyup', function() {
+            var val = $.trim(this.value);
+            refs.hide();
+            refs.filter(function() {
+                return $(this).data('filter').search(val) >= 0
+            }).show();
+        });
+
+        $('.search').on('keypress', function(e) {
+            if (e.which != 13) {
+                return;
+            }
+            $('.service').each(function() {
+                if ($(this).css('display') == "none") {
+                    return;
+                }
+                window.location.href = $(this).attr('href');
+            })
+        });
+    };
+
+    // load into the #services div
+    var heading = document.getElementById("heading");
+    var content = document.getElementById("content");
+    var service = document.createElement("div");
+    service.id = "services";
+
+    var render = function(rsp) {
+        rsp.forEach(function(srv) {
+            var div = document.createElement("div")
+            div.setAttribute("class", "app-icon");
+            var a = document.createElement("a");
+            a.href = "/" + srv.name;
+            a.setAttribute("data-filter", srv.name);
+            a.setAttribute("class", "service");
+            a.innerText = srv.name;
+            div.appendChild(a);
+            service.appendChild(div);
+        });
+
+        // setup search filtering
+        search();
+
+        // execute user defined function
+        if (fn != undefined) {
+            fn();
+        }
+    }
+
+    // the search box
+    // <h4><input class="input-lg search" type=text placeholder="Search" autofocus></h4>
+    var input = document.createElement("input")
+    input.setAttribute("class", "input-lg search");
+    input.type = "text"
+    input.placeholder = "Search"
+    input.autofocus = true;
+
+    // render from the cache
+    if (services.length > 0) {
+        return render(services);
+    }
+
+    // call the backend
+    listServices().then(function(rsp) {
+        // reset content
+        heading.innerHTML = "";
+        content.innerHTML = "";
+
+        // append the search box
+        heading.appendChild(input);
+
+        // append services to content
+        content.appendChild(service);
+
+        // cache the list for next time
+        rsp.forEach(function(srv) {
+            services[srv.name] = srv;
+        });
+
+        // render the content
+        render(rsp);
+    });
+}
+
+function renderService(service) {
+    getService(service)
+        .then(function(rsp) {
+            console.log("rendering service", service);
+            var heading = document.getElementById("heading");
+            heading.innerText = service;
+            var content = document.getElementById("content");
+            content.innerHTML = "";
+            var div = document.createElement("div");
+            div.id = "service";
+            content.appendChild(div);
+
+            var eps = {};
+
+            rsp.services[0].endpoints.forEach(function(endpoint) {
+                var name = endpoint.name.split(".")[1];
+                // define a new div for the endpoint
+                var ep = document.createElement("div")
+                ep.setAttribute("class", "endpoint");
+
+                // create the endpoint link
+                var a = document.createElement("a");
+                a.href = "/" + service + "/" + name;
+                a.innerText = name;
+
+                // set the content
+                ep.appendChild(a);
+                div.appendChild(ep);
+            })
+
+        });
+}
+
+function renderEndpoint(service, endpoint) {
+    getService(service)
+        .then(function(rsp) {
+            console.log("rendering", service, endpoint);
+            var heading = document.getElementById("heading");
+            heading.innerText = service + " " + endpoint;
+            var content = document.getElementById("content");
+            content.innerHTML = "";
+            var request = document.createElement("div");
+            request.id = "request";
+            var response = document.createElement("response");
+            response.id = "response";
+
+            content.appendChild(request);
+            content.appendChild(response);
+
+            rsp.services[0].endpoints.forEach(function(ep) {
+                var name = ep.name.split(".")[1];
+
+                // render the form
+                if (endpoint == name) {
+                    console.log("rending endpoint", ep.name);
+                    // get request info
+                    // render a form
+                    // render output
+                    var form = document.createElement("form");
+
+                    form.onsubmit = function(ev) {
+                        ev.preventDefault();
+
+                        // build request
+                        var request = {};
+
+                        for (i = 0; i < form.elements.length; i++) {
+                            var entry = form.elements[i];
+                            if (entry.name.length == 0) {
+                                continue
+                            }
+			    if (entry.value.length == 0) {
+				    continue
+			    }
+                            request[entry.name] = entry.value;
+                        }
+
+                        // renders the output recursively
+                        var render = function(key, val, depth) {
+                            // print the value if it's not an object
+			    var print = function(key, val) {
+				var value = document.createElement("div");
+				value.setAttribute("class", "field");
+				key = key.capitalize();
+				value.innerText = `${key}: ${val}`
+				return value;
+			    }
+			    // not an object, just print it
+			    if (typeof val != "object") {
+				return print(key, val)
+			    }
+			    // if it's an array then check types and print
+			    if (val.constructor == Array) {
+				if (typeof val[0] != "object") {
+				    return print(key, val);
+				}
+			    }
+
+                            var output = document.createElement("div");
+                            output.setAttribute("class", "response");
+
+                            if (depth == 0) {
+                                // set a title
+                                var h4 = document.createElement("h4");
+                                h4.setAttribute("class", "title");
+                                h4.innerText = key;
+                                output.appendChild(h4);
+                            }
+
+                            // iterate the objects as needed
+                            for (const [key, value] of Object.entries(val)) {
+                                // append the next output value
+                                depth++
+				console.log(typeof value, key, value);
+                                output.appendChild(render(key, value, depth));
+                            }
+
+                            // return the entire rendered value
+                            return output;
+                        }
+
+                        call(service, endpoint, request)
+                            .then(function(rsp) {
+                                // render output;
+                                var result = document.createElement("div");
+                                result.appendChild(render("response", rsp, 0));
+				response.innerText = '';
+                                response.prepend(result);
+                            });
+                    };
+
+                    ep.request.values.forEach(function(value) {
+                        var input = document.createElement("input");
+                        input.id = value.name
+                        input.type = "text";
+                        input.name = value.name;
+                        input.placeholder = value.name;
+                        input.autocomplete = "off";
+                        form.appendChild(input);
+                    });
+
+                    var submit = document.createElement("button")
+                    submit.setAttribute("class", "btn btn-default");
+                    submit.innerText = "Submit";
+                    form.appendChild(submit);
+                    request.appendChild(form);
+                }
+            })
+
+        });
 }
 
 function submitLogin(form) {
-	var username = document.getElementById("username").value;
-	var password = document.getElementById("password").value;
-	login(username, password);
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    login(username, password);
+    return false;
 }
 
 function submitLogout(form) {
-	logout()
+    logout()
+}
+
+function main() {
+    // parse the url
+    if (window.location.pathname == "/") {
+        return renderServices();
+    }
+
+    var parts = window.location.pathname.split("/")
+
+    // process service
+    if (parts.length == 2) {
+        renderService(parts[1]);
+    }
+
+    if (parts.length == 3) {
+        renderEndpoint(parts[1], parts[2]);
+    }
 }
--- client/web/web.go
+++ client/web/web.go
@@ -4,8 +4,6 @@ package web
 import (
 	"context"
 	"embed"
-	"encoding/json"
-	"fmt"
 	"html/template"
 	"io/fs"
 	"log"
@@ -13,18 +11,13 @@ import (
 	"net/url"
 	"os"
 	"path/filepath"
-	"sort"
 	"strings"
 	"sync"
 	"time"
 
-	"github.com/fatih/camelcase"
 	"github.com/gorilla/mux"
 	"github.com/micro/micro/v3/client/web/html"
 	"github.com/micro/micro/v3/cmd"
-	"github.com/micro/micro/v3/service/auth"
-	"github.com/micro/micro/v3/service/registry"
-	"github.com/serenize/snaker"
 	"github.com/urfave/cli/v2"
 )
 
@@ -42,27 +35,15 @@ var (
 	TokenCookieName = "micro-token"
 
 	// create a session store
-	mtx sync.RWMutex
+	mtx      sync.RWMutex
 	sessions = map[string]*session{}
 )
 
 type srv struct {
 	*mux.Router
-	// registry we use
-	registry registry.Registry
-}
-
-type reg struct {
-	registry.Registry
-
-	sync.RWMutex
-	lastPull time.Time
-	services []*registry.Service
 }
 
 type session struct {
-	// account related to the session
-	Account *auth.Account
 	// token used for the session
 	Token string
 }
@@ -110,17 +91,9 @@ func (s *srv) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 		// no session, go get the account
 		if !ok {
-			// can't inspect the token
-			acc, err := auth.Inspect(token)
-			if err != nil {
-				http.Error(w, "Unauthorized", 401)
-				return
-			}
-
 			// save the session
 			mtx.Lock()
 			sess = &session{
-				Account: acc,
 				Token: token,
 			}
 			sessions[token] = sess
@@ -145,53 +118,6 @@ func (s *srv) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	s.Router.ServeHTTP(w, r)
 }
 
-func split(v string) string {
-	parts := camelcase.Split(strings.Replace(v, ".", "", 1))
-	return strings.Join(parts, " ")
-}
-
-func format(v *registry.Value) string {
-	if v == nil || len(v.Values) == 0 {
-		return "{}"
-	}
-	var f []string
-	for _, k := range v.Values {
-		f = append(f, formatEndpoint(k, 0))
-	}
-	return fmt.Sprintf("{\n%s}", strings.Join(f, ""))
-}
-
-func formatEndpoint(v *registry.Value, r int) string {
-	// default format is tabbed plus the value plus new line
-	fparts := []string{"", "%s %s", "\n"}
-	for i := 0; i < r+1; i++ {
-		fparts[0] += "\t"
-	}
-	// its just a primitive of sorts so return
-	if len(v.Values) == 0 {
-		return fmt.Sprintf(strings.Join(fparts, ""), snaker.CamelToSnake(v.Name), v.Type)
-	}
-
-	// this thing has more things, it's complex
-	fparts[1] += " {"
-
-	vals := []interface{}{snaker.CamelToSnake(v.Name), v.Type}
-
-	for _, val := range v.Values {
-		fparts = append(fparts, "%s")
-		vals = append(vals, formatEndpoint(val, r+1))
-	}
-
-	// at the end
-	l := len(fparts) - 1
-	for i := 0; i < r+1; i++ {
-		fparts[l] += "\t"
-	}
-	fparts = append(fparts, "}\n")
-
-	return fmt.Sprintf(strings.Join(fparts, ""), vals...)
-}
-
 func faviconHandler(w http.ResponseWriter, r *http.Request) {
 	return
 }
@@ -205,270 +131,22 @@ func (s *srv) indexHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "OPTIONS" {
 		return
 	}
-
-	domain := registry.DefaultDomain
-
-	services, err := s.registry.ListServices(registry.ListDomain(domain))
-	if err != nil {
-		log.Printf("Error listing services: %v", err)
-	}
-
-	type webService struct {
-		Name string
-		Link string
-		Icon string // TODO: lookup icon
-	}
-
-	var webServices []webService
-	for _, srv := range services {
-		name := srv.Name
-		link := fmt.Sprintf("/%v", name)
-
-		if len(srv.Endpoints) == 0 {
-			continue
-		}
-
-		// in the case of 3 letter things e.g m3o convert to M3O
-		if len(name) <= 3 && strings.ContainsAny(name, "012345789") {
-			name = strings.ToUpper(name)
-		}
-
-		webServices = append(webServices, webService{Name: name, Link: link})
-	}
-
-	sort.Slice(webServices, func(i, j int) bool { return webServices[i].Name < webServices[j].Name })
-
-	type templateData struct {
-		HasWebServices bool
-		WebServices    []webService
-	}
-
-	data := templateData{len(webServices) > 0, webServices}
-	s.render(w, r, html.IndexTemplate, data)
+	s.render(w, r, html.IndexTemplate, struct{}{})
 }
 
 func (s *srv) loginHandler(w http.ResponseWriter, req *http.Request) {
-	if req.Method == "POST" {
-		s.generateTokenHandler(w, req)
-		return
-	}
-
 	s.render(w, req, html.LoginTemplate, struct{}{})
 }
 
 func (s *srv) logoutHandler(w http.ResponseWriter, req *http.Request) {
-	var domain string
-	if arr := strings.Split(req.Host, ":"); len(arr) > 0 {
-		domain = arr[0]
-	}
-
 	http.SetCookie(w, &http.Cookie{
 		Name:    TokenCookieName,
 		Value:   "",
 		Expires: time.Unix(0, 0),
-		Domain:  domain,
 		Secure:  true,
 	})
 
 	http.Redirect(w, req, "/", 302)
-}
-
-func (s *srv) generateTokenHandler(w http.ResponseWriter, req *http.Request) {
-	renderError := func(errMsg string) {
-		t, err := template.New("template").Parse(html.LoginTemplate)
-		if err != nil {
-			http.Error(w, "Error occurred:"+err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		if err := t.ExecuteTemplate(w, "basic", map[string]interface{}{
-			"error": errMsg,
-		}); err != nil {
-			http.Error(w, "Error occurred:"+err.Error(), http.StatusInternalServerError)
-		}
-	}
-
-	user := req.PostFormValue("username")
-	if len(user) == 0 {
-		renderError("Missing Username")
-		return
-	}
-
-	pass := req.PostFormValue("password")
-	if len(pass) == 0 {
-		renderError("Missing Password")
-		return
-	}
-
-	acc, err := auth.Token(
-		auth.WithCredentials(user, pass),
-		auth.WithTokenIssuer(Namespace),
-		auth.WithExpiry(time.Hour*24*7),
-	)
-	if err != nil {
-		renderError("Authentication failed: " + err.Error())
-		return
-	}
-
-	var domain string
-	if arr := strings.Split(req.Host, ":"); len(arr) > 0 {
-		domain = arr[0]
-	}
-
-	http.SetCookie(w, &http.Cookie{
-		Name:    TokenCookieName,
-		Value:   acc.AccessToken,
-		Expires: acc.Expiry,
-		Domain:  domain,
-		Secure:  true,
-	})
-
-	http.Redirect(w, req, "/", http.StatusFound)
-}
-
-func (s *srv) registryHandler(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	svc := vars["name"]
-
-	domain := registry.DefaultDomain
-
-	if len(svc) > 0 {
-		sv, err := s.registry.GetService(svc, registry.GetDomain(domain))
-		if err != nil {
-			http.Error(w, "Error occurred:"+err.Error(), 500)
-			return
-		}
-
-		if len(sv) == 0 {
-			http.Error(w, "Not found", 404)
-			return
-		}
-
-		if r.Header.Get("Content-Type") == "application/json" {
-			b, err := json.Marshal(map[string]interface{}{
-				"services": s,
-			})
-			if err != nil {
-				http.Error(w, "Error occurred:"+err.Error(), 500)
-				return
-			}
-			w.Header().Set("Content-Type", "application/json")
-			w.Write(b)
-			return
-		}
-
-		s.render(w, r, html.ServiceTemplate, sv)
-		return
-	}
-
-	services, err := s.registry.ListServices(registry.ListDomain(domain))
-	if err != nil {
-		log.Printf("Error listing services: %v", err)
-	}
-
-	sort.Sort(sortedServices{services})
-
-	if r.Header.Get("Content-Type") == "application/json" {
-		b, err := json.Marshal(map[string]interface{}{
-			"services": services,
-		})
-		if err != nil {
-			http.Error(w, "Error occurred:"+err.Error(), 500)
-			return
-		}
-		w.Header().Set("Content-Type", "application/json")
-		w.Write(b)
-		return
-	}
-
-	s.render(w, r, html.RegistryTemplate, services)
-}
-
-func (s *srv) callHandler(w http.ResponseWriter, r *http.Request) {
-	domain := registry.DefaultDomain
-
-	services, err := s.registry.ListServices(registry.ListDomain(domain))
-	if err != nil {
-		log.Printf("Error listing services: %v", err)
-	}
-
-	sort.Sort(sortedServices{services})
-
-	serviceMap := make(map[string][]*registry.Endpoint)
-	for _, service := range services {
-		if len(service.Endpoints) > 0 {
-			serviceMap[service.Name] = service.Endpoints
-			continue
-		}
-		// lookup the endpoints otherwise
-		s, err := s.registry.GetService(service.Name, registry.GetDomain(domain))
-		if err != nil {
-			continue
-		}
-		if len(s) == 0 {
-			continue
-		}
-		serviceMap[service.Name] = s[0].Endpoints
-	}
-
-	if r.Header.Get("Content-Type") == "application/json" {
-		b, err := json.Marshal(map[string]interface{}{
-			"services": services,
-		})
-		if err != nil {
-			http.Error(w, "Error occurred:"+err.Error(), 500)
-			return
-		}
-		w.Header().Set("Content-Type", "application/json")
-		w.Write(b)
-		return
-	}
-
-	s.render(w, r, html.CallTemplate, serviceMap)
-}
-
-func (s *srv) serviceHandler(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	name := vars["service"]
-	if len(name) == 0 {
-		return
-	}
-
-	domain := registry.DefaultDomain
-
-	services, err := s.registry.GetService(name, registry.GetDomain(domain))
-	if err != nil {
-		log.Printf("Error getting service %s: %v", name, err)
-	}
-
-	sort.Sort(sortedServices{services})
-
-	serviceMap := make(map[string][]*registry.Endpoint)
-
-	for _, service := range services {
-		if len(service.Endpoints) > 0 {
-			serviceMap[service.Name] = service.Endpoints
-			continue
-		}
-	}
-
-	if r.Header.Get("Content-Type") == "application/json" {
-		b, err := json.Marshal(map[string]interface{}{
-			"services": services,
-		})
-		if err != nil {
-			http.Error(w, "Error occurred:"+err.Error(), 500)
-			return
-		}
-		w.Header().Set("Content-Type", "application/json")
-		w.Write(b)
-		return
-	}
-
-	s.render(w, r, html.WebTemplate, serviceMap, templateValue{
-		Key:   "Name",
-		Value: name,
-	})
 }
 
 type templateValue struct {
@@ -478,9 +156,7 @@ type templateValue struct {
 
 func (s *srv) render(w http.ResponseWriter, r *http.Request, tmpl string, data interface{}, vals ...templateValue) {
 	t, err := template.New("template").Funcs(template.FuncMap{
-		"Split":  split,
-		"format": format,
-		"Title":  strings.Title,
+		"Title": strings.Title,
 		"First": func(s string) string {
 			if len(s) == 0 {
 				return s
@@ -510,28 +186,18 @@ func (s *srv) render(w http.ResponseWriter, r *http.Request, tmpl string, data i
 
 	filepath.Join(u.Path, r.URL.Path)
 
-	// If the user is logged in, render Account instead of Login
-	loginTitle := "Login"
-	loginLink := LoginURL
-	user := ""
-	token := ""
+	var token string
 
 	sess, ok := r.Context().Value(session{}).(*session)
 	if ok {
 		token = sess.Token
-		user = sess.Account.ID
-		loginTitle = "Logout"
-		loginLink = "/logout"
 	}
 
 	templateData := map[string]interface{}{
-		"ApiURL":     apiURL,
-		"LoginTitle": loginTitle,
-		"LoginURL":   loginLink,
-		"Results":    data,
-		"User":       user,
-		"Token":      token,
-		"Namespace":  Namespace,
+		"ApiURL":    apiURL,
+		"Token":     token,
+		"Results":   data,
+		"Namespace": Namespace,
 	}
 
 	// add extra values
@@ -574,9 +240,6 @@ func Run(ctx *cli.Context) error {
 
 	srv := &srv{
 		Router: mux.NewRouter(),
-		registry: &reg{
-			Registry: registry.DefaultRegistry,
-		},
 	}
 
 	htmlContent, err := fs.Sub(content, "html")
@@ -589,12 +252,10 @@ func Run(ctx *cli.Context) error {
 	srv.HandleFunc("/404", srv.notFoundHandler)
 	srv.HandleFunc("/login", srv.loginHandler)
 	srv.HandleFunc("/logout", srv.logoutHandler)
-	srv.HandleFunc("/client", srv.callHandler)
-	srv.HandleFunc("/services", srv.registryHandler)
-	srv.HandleFunc("/service/{name}", srv.registryHandler)
 	srv.PathPrefix("/assets/").Handler(http.FileServer(http.FS(htmlContent)))
-	srv.HandleFunc("/{service}", srv.serviceHandler)
 	srv.HandleFunc("/", srv.indexHandler)
+	srv.HandleFunc("/{service}", srv.indexHandler)
+	srv.HandleFunc("/{service}/{endpoint}", srv.indexHandler)
 
 	// create new http server
 	server := &http.Server{
@@ -633,25 +294,3 @@ var (
 		},
 	}
 )
-
-func reverse(s []string) {
-	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
-		s[i], s[j] = s[j], s[i]
-	}
-}
-
-type sortedServices struct {
-	services []*registry.Service
-}
-
-func (s sortedServices) Len() int {
-	return len(s.services)
-}
-
-func (s sortedServices) Less(i, j int) bool {
-	return s.services[i].Name < s.services[j].Name
-}
-
-func (s sortedServices) Swap(i, j int) {
-	s.services[i], s.services[j] = s.services[j], s.services[i]
-}
