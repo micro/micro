46e72289c431e74591694d08a1364bff5f5a8ff3
Alex Serheyev <alex@dna.tech>
2024-04-20 11:38:08 +0300

    ⏪️fix(ci): revert dependency version go: downloading github.com/jackc/pgservicefile v0.0.0-20231201235250-de7065d80cb9 gcc: error: unrecognized command-line option ‘-mthreads’; did you mean ‘-pthread’? Error: ../../../go/pkg/mod/golang.org/x/net@v0.23.0/http2/transport.go:1096:13: tc.NetConn undefined (type *tls.Conn has no field or method NetConn) note: module requires Go 1.18 Error: Process completed with exit code 2.

--- scripts/build_all_platforms.sh
+++ scripts/build_all_platforms.sh
@@ -5,5 +5,6 @@ builds=("windows,amd64" "windows,386" "linux,arm64" "linux,386" "linux,amd64" "l
 for build in ${builds[@]}; do
   IFS=',' read -r -a array <<< "$build"
   echo "building ${array[0]} ${array[1]}"
-  GOOS=${array[0]} GOARCH=${array[1]} CGO_ENABLED=1 go build cmd/micro/main.go
+  # TODO: CGO_ENABLED=1 prevent building windows amd64
+  GOOS=${array[0]} GOARCH=${array[1]} CGO_ENABLED=0 go build cmd/micro/main.go
 done
