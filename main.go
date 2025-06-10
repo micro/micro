ee96d61acc52bdc1c482c82270975a6be71dc52d
Asim Aslam <asim@aslam.me>
2025-06-10 10:31:55 +0100

    move server to micro server (#2161)

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 
 	_ "github.com/micro/micro/v5/cmd/micro-cli"
 	_ "github.com/micro/micro/v5/cmd/micro-run"
+	_ "github.com/micro/micro/v5/cmd/micro-server"
 )
 
 var version = "5.0.0-dev"
