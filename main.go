78e50a3c2daa422254a1fd15eb67126c7dbc9da3
Asim Aslam <asim@aslam.me>
2025-06-11 09:28:27 +0000

    x

--- main.go
+++ main.go
@@ -1,14 +1,22 @@
 package main
 
 import (
+	"embed"
 	"go-micro.dev/v5/cmd"
 
 	_ "github.com/micro/micro/v5/cmd/micro-cli"
 	_ "github.com/micro/micro/v5/cmd/micro-run"
-	_ "github.com/micro/micro/v5/cmd/micro-server"
+	"github.com/micro/micro/v5/cmd/micro-server"
 )
 
+//go:embed html/*
+var htmlFS embed.FS
+
 var version = "5.0.0-dev"
+
+func init() {
+	server.HTML = htmlFS
+}
 
 func main() {
 	cmd.Init(
